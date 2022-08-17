cf964c0da5cf07c09d41abbe5c07bce3b565b3ea
Tobias <tobias@fixmycity.de>
2022-08-17 17:52:31 +0200

    Next: Sourcemaps on production
    Which should be fine for us and useful for others to learn

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  productionBrowserSourceMaps: true, // build source maps in production – https://nextjs.org/docs/advanced-features/source-maps
 }
 
 module.exports = nextConfig
