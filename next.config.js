45f5307a522ff4fd7be88fbb51eed655bb2b254d
Tobias <tobias@fixmycity.de>
2022-08-17 17:52:45 +0200

    Next: Some image plugin base config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: true, // build source maps in production – https://nextjs.org/docs/advanced-features/source-maps
+  images: {
+    domains: [], // allow fetching images from those domains – https://nextjs.org/docs/api-reference/next/image#domains
+  },
 }
 
 module.exports = nextConfig
