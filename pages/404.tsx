9a40b50f3d69056b96cf6757cf6d22d06a204682
Tobias <tobias@fixmycity.de>
2022-08-17 17:52:06 +0200

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Layout } from '@/components/Layout'
+import { NextPage } from 'next'
+
+const Custom404: NextPage = () => {
+  return <>Hallo 404</>
+}
+
+export default Custom404
--- pages_TODO/404.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Layout } from '@/components/Layout'
-
-const NotFound: React.FC = () => {
-  return <Layout />
-}
-
-export default NotFound
