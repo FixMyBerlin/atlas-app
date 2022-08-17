e924cb682e92d09d8103e9c667b7af3dbd8dd4fa
Tobias <tobias@fixmycity.de>
2022-08-17 17:51:59 +0200

    Next: Add jest

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,21 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = {
-  cacheDirectory: '<rootDir>/.cache/unit',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.spec.json',
-    },
-    __PATH_PREFIX__: ``,
-  },
-  moduleDirectories: ['node_modules', 'src'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
-    '~/(.*)': '<rootDir>/src/$1',
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
-  preset: 'ts-jest',
-  rootDir: '.',
-  setupFiles: [`<rootDir>/__helpers__/loadershim.js`],
-  setupFilesAfterEnv: ['<rootDir>/__helpers__/setup.ts'],
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: [
-    '<rootDir>/__mocks__/*',
-    'node_modules',
-    '\\.cache',
-    '<rootDir>.*/public',
-  ],
-  testEnvironmentOptions: { url: `http://localhost` },
-  transform: { '^.+\\.[tj]sx?$': 'ts-jest' },
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  testEnvironment: 'jest-environment-jsdom',
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
