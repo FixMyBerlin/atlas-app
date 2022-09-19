ba3407aec03a09c57b5b7f955c4b6fd69f6bf60b
Tobias <tobias@fixmycity.de>
2022-09-19 15:45:42 +0200

    Small improvements to AddUrl

--- atlas-geo/atlas-geo/app/process/helper/AddUrl.test.lua
+++ atlas-geo/atlas-geo/app/process/helper/AddUrl.test.lua
@@ -3,9 +3,9 @@ require("AddUrl")
 
 print('=== Test AddUrl ===')
 
-local osm_type = "Z"
+local osm_type = "way"
 local object = { ["id"] = 33, ["tags"] = {} }
 
 AddUrl(osm_type, object)
 
-assert(object.tags.osm_url == "https://osm.org/Z/33")
+assert(object.tags.osm_url == "https://osm.org/way/33")
