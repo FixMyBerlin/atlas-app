983fddb76301e0d408c6372cbb6b7931383c6141
Tobias <tobias@fixmycity.de>
2022-09-19 16:00:39 +0200

    Update lua help texts

--- atlas-geo/app/process/helper/MergeArray.lua
+++ atlas-geo/app/process/helper/MergeArray.lua
@@ -1,6 +1,7 @@
 -- Demo http://tpcg.io/_HFSD73
 -- Source https://stackoverflow.com/a/65600458/729221
--- * @docs `MergeArray({ 'array1' }, { 'array2' })`
+
+-- * @desc `MergeArray({ 'array1' }, { 'array2' })`
 -- * @returns `{ 'array1', 'array2' }`
 function MergeArray(array1, array2)
   for _, v in pairs(array2) do table.insert(array1, v) end
--- atlas-geo/app/process/helper/Set.lua
+++ atlas-geo/app/process/helper/Set.lua
@@ -1,7 +1,10 @@
 -- https://www.lua.org/pil/11.5.html
-function Set(list)
+
+-- * @desc `Set({ 'item' })''
+-- * @returns `{ ["item"]: true }`
+function Set(array)
   local set = {}
-  -- Reminder: ipairs will make sure 'list' is in it's strucutre an array by skipping all object like keys (string, out of order-keys).
-  for _, l in ipairs(list) do set[l] = true end
+  -- Reminder: ipairs will make sure 'array' is in it's strucutre an array by skipping all object like keys (string, out of order-keys).
+  for _, l in ipairs(array) do set[l] = true end
   return set
 end
