6edcdc52e806fe4069c7b5962751206ea8933571
Tobias <tobias@fixmycity.de>
2022-09-20 17:52:31 +0200

    Extract "AddSkipInfoToHighways"
    We use this as a general highway filter, which we specify even more per data layer.

--- atlas-geo/atlas-geo/app/process/roadtypesOsm.lua
+++ atlas-geo/atlas-geo/app/process/roadtypesOsm.lua
@@ -8,6 +8,7 @@ require("MergeArray")
 require("AddMetadata")
 require("AddUrl")
 require("HighwayClasses")
+require("AddSkipInfoToHighways")
 
 local table = osm2pgsql.define_table({
   name = 'roadtypesOsm',
@@ -39,58 +40,7 @@ function osm2pgsql.process_way(object)
   object.tags._skipNotes = "init"
   object.tags._skip = false
 
-  -- Skip all non standard access values
-  local allowed_access_values = Set({ "private", "no", "destination", "delivery", "permit" })
-  if object.tags.access and allowed_access_values[object.tags.access] then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `allowed_access_values`"
-    object.tags._skip = true
-  end
-
-  if object.tags.operator == 'private' then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `operator=private`"
-    object.tags._skip = true
-  end
-
-  if object.tags.foot == 'private' then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `foot=private`"
-    object.tags._skip = true
-  end
-
-  if object.tags.indoor == 'yes' then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `indoor=yes`"
-    object.tags._skip = true
-  end
-
-  if object.tags.informal == 'yes' then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `informal=yes`"
-    object.tags._skip = true
-  end
-
-  if object.tags['mtb:scale'] then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped since `mtb:scale` indicates a special interest path"
-    object.tags._skip = true
-  end
-
-  if object.tags.tracktype == "grade5" then
-    object.tags._skipNotes = object.tags._skipNotes ..
-        ";Skipped since `tracktype=grade5` indicates a special interest path"
-    object.tags._skip = true
-  end
-
-  ToNumber(object.tags, Set({ 'width' }))
-  if object.tags.width and object.tags.width < 2.1 then
-    object.tags._skipNotes = object.tags._skipNotes ..
-        ";Skipped since `width<2.1m` indicates a special interest path"
-    object.tags._skip = true
-  end
-
-  -- Skip all unwanted `highway=service + service=<value>` values
-  -- The key can have random values, we maintly want to skip "driveway", "parking_aisle".
-  local skip_service_values = Set({ "alley", "drive-through", "emergency_access" })
-  if object.tags.service and not skip_service_values[object.tags.service] then
-    object.tags._skipNotes = object.tags._skipNotes .. ";Skipped by `skip_service_values`"
-    object.tags._skip = true
-  end
+  AddSkipInfoToHighways(object)
 
   -- Skip sidewalk `(highway=footway) + footway=sidewalk`
   -- Including "Fahrrad frei" https://wiki.openstreetmap.org/wiki/DE:Tag:traffic_sign%3DDE:1022-10
