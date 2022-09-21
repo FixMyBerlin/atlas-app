f2e4b895e23d4d709a778768d7aadb7fccafac17
Tobias <tobias@fixmycity.de>
2022-09-21 09:46:49 +0200

    Add metadata table with DateTime info on the data

---
+++ atlas-geo/app/process/metadata.lua
@@ -0,0 +1,39 @@
+package.path = package.path .. ";/app/process/helper/?.lua"
+require("Set")
+require("FilterTags")
+require("ToNumber")
+-- require("PrintTable")
+require("AddAddress")
+require("MergeArray")
+require("AddMetadata")
+require("AddUrl")
+
+local table = osm2pgsql.define_table({
+  name = 'metadata',
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+  columns = {
+    { column = 'tags', type = 'jsonb' },
+    { column = 'geom', type = 'point' },
+  }
+})
+
+-- Meta data table.
+-- The content (the changing cities office) is just a placeholder.
+-- We use this table to show metadata.
+-- Right now that is only the processed_at date. See "run-3-process" for more.
+
+function osm2pgsql.process_node(object)
+  if not object.tags.office then return end
+
+  if object.tags.name == "Changing Cities" then
+    local allowed_tags = Set({ "name", "office", "operator" })
+    FilterTags(object.tags, allowed_tags)
+    AddMetadata(object)
+    AddUrl("node", object)
+
+    table:insert({
+      tags = object.tags,
+      geom = object:as_point()
+    })
+  end
+end
