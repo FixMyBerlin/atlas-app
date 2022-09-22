aeabc51ee8b8a588dd8681df4180a4fb480bff5a
Tobias <tobias@fixmycity.de>
2022-09-22 11:26:09 +0200

    BackgroundLegend: Improve hover
    Clarify that links open in new window on hover

--- components/MapInterface/SelectBackgroundWithLegend/BackgroundLegend.tsx
+++ components/MapInterface/SelectBackgroundWithLegend/BackgroundLegend.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@/components/Link'
-import { MapIcon } from '@heroicons/react/24/outline'
+import { MapIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import { mapDataConfig } from '../Map/mapData'
 import { useQuery } from '../store'
@@ -27,10 +27,19 @@ export const BackgroundLegend: React.FC = () => {
       to={enhancedLink}
       blank
       external
-      classNameOverwrite="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-md hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+      classNameOverwrite="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-md hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-yellow-500 group"
       title={`Für die gewählte Hintergrundkarte ${selectedBackground.name} gibt es eine Legene auf einer externen Webseite.`}
     >
-      <MapIcon className="-ml-1 mr-1.5 h-5 w-5" aria-hidden="true" />
+      <div className="relative -ml-1 mr-1.5 h-5 w-5">
+        <ArrowTopRightOnSquareIcon
+          className="absolute opacity-0 transition-opacity group-hover:opacity-100"
+          aria-hidden="true"
+        />
+        <MapIcon
+          className="absolute opacity-100 transition-opacity group-hover:opacity-0"
+          aria-hidden="true"
+        />
+      </div>
       Legende
     </Link>
   )
