597d80b0db5fed76736caba5fb3343916cb6944e
Tobias <tobias@fixmycity.de>
2022-09-22 12:01:48 +0200

    Styling Header

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -3,9 +3,9 @@ import { SecondaryNavigationDesktop } from './SecondaryNavigationDesktop'
 
 export const Header = () => {
   return (
-    <nav className="bg-gray-800 z-20 shadow-xl">
+    <nav className="z-20 bg-gray-800 shadow-xl">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex sm:items-center sm:justify-between sm:flex-row flex-col">
+        <div className="relative flex flex-col sm:flex-row sm:items-center sm:justify-between">
           <PrimaryNavigation />
 
           <SecondaryNavigationDesktop />
