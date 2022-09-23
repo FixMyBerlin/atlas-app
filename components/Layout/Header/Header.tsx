7b7dd2fbd51cd80d1be242049c8a07ccb6a251ff
Tobias <tobias@fixmycity.de>
2022-09-23 17:03:25 +0200

    Header: Align padding left/right with app

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -4,7 +4,7 @@ import { SecondaryNavigationDesktop } from './SecondaryNavigationDesktop'
 export const Header = () => {
   return (
     <nav className="z-20 bg-gray-800 shadow-xl">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto px-2 sm:px-6 lg:pl-5 lg:pr-2.5">
         <div className="relative flex flex-col sm:flex-row sm:items-center sm:justify-between">
           <PrimaryNavigation />
 
--- components/Layout/Header/SecondaryNavigationDesktop/SecondaryNavigationDesktop.tsx
+++ components/Layout/Header/SecondaryNavigationDesktop/SecondaryNavigationDesktop.tsx
@@ -10,7 +10,7 @@ export const SecondaryNavigationDesktop: React.FC = () => {
         <>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="relative ml-3">
-              <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <Disclosure.Button className="inline-flex items-center justify-center rounded-md border border-gray-700 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="sr-only">Sekundärmenü öffnen</span>
                 {open ? (
                   <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
