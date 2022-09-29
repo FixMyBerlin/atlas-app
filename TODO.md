b35ef425d1d4c5c766ec9c677402cce6891dc74c
Tobias <tobias@fixmycity.de>
2022-09-29 12:36:44 +0200

    Migrate: Add tanstack/react-location and devtools

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODOs
+
+## `@tanstack/react-location-devtools`: Cleanup package.json overwrite
+
+This needs a kind of ugly override in package.json, see https://github.com/TanStack/router/issues/257#issuecomment-1154737874
+Once that issue is resolved, we should cleanup the overwrite.
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Outlet, Router,ReactLocation,MakeGenerics,Route } from "@tanstack/react-location";
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export type LocationGenerics = MakeGenerics<{
+  LoaderData: {
+    expensiveTimestamp: number;
+    reallyExpensiveTimestamp: number;
+  };
+  Params: {
+    invoiceId: string;
+    userId: string;
+  };
+  Search: {
+    showNotes: boolean;
+    notes: string;
+    usersView: {
+      sortBy?: "name" | "id" | "email";
+      filterBy?: string;
+    };
+  };
+}>;
 
+const location = new ReactLocation<LocationGenerics>();
+
+
+// Build our routes. We could do this in our component, too.
+const routes: Route<LocationGenerics>[] = [
+  { path: "/", element: <h1>Home</h1> },
+  {
+    path: "dashboard",
+    element: <h1>dashboard</h1>,
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     <Router
+          location={location}
+          routes={routes}>
+       <Outlet />
+       <ReactLocationDevtools initialIsOpen={false} />
+     </Router>
   )
 }
 
