f3d58cc0a1a2e2e1d0f044b6eae2a4c84dd46645
Tobias <tobias@fixmycity.de>
2022-09-29 13:18:30 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import { Outlet, Router,ReactLocation,MakeGenerics,Route } from "@tanstack/react-location";
-import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
+import {
+  Outlet,
+  Router,
+  ReactLocation,
+  MakeGenerics,
+  Route,
+} from '@tanstack/react-location'
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools'
 
 export type LocationGenerics = MakeGenerics<{
   LoaderData: {
-    expensiveTimestamp: number;
-    reallyExpensiveTimestamp: number;
-  };
+    expensiveTimestamp: number
+    reallyExpensiveTimestamp: number
+  }
   Params: {
-    invoiceId: string;
-    userId: string;
-  };
+    invoiceId: string
+    userId: string
+  }
   Search: {
-    showNotes: boolean;
-    notes: string;
+    showNotes: boolean
+    notes: string
     usersView: {
-      sortBy?: "name" | "id" | "email";
-      filterBy?: string;
-    };
-  };
-}>;
+      sortBy?: 'name' | 'id' | 'email'
+      filterBy?: string
+    }
+  }
+}>
 
-const location = new ReactLocation<LocationGenerics>();
-
+const location = new ReactLocation<LocationGenerics>()
 
 // Build our routes. We could do this in our component, too.
 const routes: Route<LocationGenerics>[] = [
-  { path: "/", element: <h1>Home</h1> },
+  { path: '/', element: <h1>Home</h1> },
   {
-    path: "dashboard",
+    path: 'dashboard',
     element: <h1>dashboard</h1>,
   },
-];
+]
 
 const App = () => {
   return (
-     <Router
-          location={location}
-          routes={routes}>
-       <Outlet />
-       <ReactLocationDevtools initialIsOpen={false} />
-     </Router>
+    <Router location={location} routes={routes}>
+      <Outlet />
+      <ReactLocationDevtools initialIsOpen={false} />
+    </Router>
   )
 }
 
