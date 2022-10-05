b6e9d08ef7b9aec2ed091ff08aa7003f8266487a
Tobias <tobias@fixmycity.de>
2022-10-05 09:09:23 +0200

    Add briefing on barriers data set

---
+++ atlas-geo/app/process/TODO_barriers.lua
@@ -0,0 +1,9 @@
+-- Goal:
+-- ======
+-- Data on things that would prevent a bike network like water, highways, airports
+
+-- TODOs:
+-- ======
+-- LUA https://github.com/FixMyBerlin/osm-scripts/blob/main/utils/poiBarriers/downloadPoiBarriers.ts
+-- Remove small lakes. So closed ways that are of a certain area (LUA area function should work)
+-- Cleanup tags and add custom category for easy visualization
