4613e3b46919d67f2ffe4d9d33681fefa1b55688
Henri <henri@fixmycity.de>
2022-10-07 17:07:24 +0200

    Add verification endpoint

--- atlas-geo/atlas-geo/VERIFICATION.md
+++ atlas-geo/atlas-geo/VERIFICATION.md
@@ -2,10 +2,11 @@
 
 ## Create table verifications
 ```postgresql
-CREATE TABLE public.footway_verification (
+CREATE TABLE public.footways_verification (
 	osm_type varchar NOT NULL,
 	osm_id bigint NOT NULL,
 	verified_at timestamp NOT NULL,
+	verified_by bigint NOT NULL,
 	verified varchar NULL
 );
 ```
@@ -16,6 +17,15 @@ CREATE TABLE public.footway_verification (
 create materialized view highways_verified
 as
 select * from highways h join  highway_verification hv on hv.osm_id = h.way_id ;
+
+# OR
+
+create view lit_verified
+as
+select h.osm_type , h.osm_id , lv.verified_at , lv.verified_by , lv.verified, h.tags  from lit h
+join  lit_verification lv
+on lv.osm_id = h.osm_id
+and lv.osm_type = h.osm_type  ;
 
 GRANT SELECT ON TABLE public.highways_verified TO api_read;
 ```
--- atlas-geo/atlas-geo/exports/api.py
+++ atlas-geo/atlas-geo/exports/api.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Header, Response, Request, HTTPException
 from fastapi.responses import FileResponse
 from typing import Union
-from osgeo import gdal
+from psycopg2 import sql
 import psycopg2
-import tempfile
 import json
 
 app = FastAPI(
@@ -21,15 +20,29 @@ connString = "host=%s dbname=%s user=%s password=%s port=6000" %(databaseServer,
 
 conn = psycopg2.connect(connString)
 
-availableExportTables = {
+available_export_functions = {
   "bike_infrastructure": "export_geojson_bikeinfra",
   "lit": "export_geojson_lit",
   "highways": "export_geojson_bbox"
 }
 
+available_verification_tables = {
+  "bike_infrastructure": "bike_infrastrucutre_verification",
+  "lit": "lit_verification",
+  "highways": "highways_verification"
+}
+
+available_data_tables = {
+  "bike_infrastructure": "bicycleRoadInfrastrucutre",
+  "lit": "lit",
+  "highways": "highways"
+}
+
+verified_status = ['correct', 'false', 'undefined']
+
 @app.get("/export/{type_name}")
 def export_region(response: Response, type_name: str, minlon: float= 13.3, minlat : float=52.2, maxlon: float=13.7, maxlat: float=52.3):
-    if type_name not in availableExportTables.keys():
+    if type_name not in available_export_functions.keys():
       raise HTTPException(status_code=404, detail="export type unknown")
 
     cur = conn.cursor()
@@ -38,39 +51,55 @@ def export_region(response: Response, type_name: str, minlon: float= 13.3, minla
     response.headers["Content-Disposition"] = 'attachment; filename="export.geojson"'
     response.headers["Content-Type"] = 'application/geo+json'
 
-    sql = "select * from " + availableExportTables[type_name] + "(%s, %s, %s, %s);"
-    cur.execute(sql, ( minlon, minlat, maxlon, maxlat) )
+    statement = sql.SQL("SELECT * FROM {table_name} (%s, %s, %s, %s);").format(table_name=sql.Identifier(available_export_functions[type_name]))
+    cur.execute(statement, ( minlon, minlat, maxlon, maxlat) )
     return cur.fetchone()[0]
-
-
-@app.get("/export/{type_name}/shapefile")
-def export_shapefile_region(response: Response, type_name: str, minlon: float= 13.3, minlat : float=52.2, maxlon: float=13.7, maxlat: float=52.3):
-    if type_name not in availableExportTables.keys():
-      raise HTTPException(status_code=404, detail="export type unknown")
-
-    cur = conn.cursor()
-
-    sql = "select * from " + availableExportTables[type_name] + "(%s, %s, %s, %s);"
-    cur.execute(sql, ( minlon, minlat, maxlon, maxlat) )
-    geojson = cur.fetchone()[0]
-
-    with tempfile.NamedTemporaryFile() as geojsonTmp:
-      print(geojsonTmp.name)
-      json.dump(geojson, geojsonTmp)
-      srcDS = gdal.OpenEx(geojsonTmp.name+'.json')
-
-      with tempfile.NamedTemporaryFile() as shapeTmp:
-        ds = gdal.VectorTranslate(shapeTmp.name+'.shp', srcDS, format='ESRI Shapefile')
-
-        response.headers["Content-Type"] = 'x-gis/x-shapefile'
-
-        return FileResponse(shapeTmp.name)
 
 
 @app.get("/verify/{type_name}/{osm_id}")
-def export_region(response: Response, type_name: str, osm_id: int):
+def verify_osm_object(response: Response, type_name: str, osm_type: str, osm_id: int, verified_at: str, verified_by: int, verified: str):
+    if type_name not in available_verification_tables.keys():
+      raise HTTPException(status_code=404, detail="export type unknown")
+
+    # Check if verified string is valid
+    if verified not in verified_status:
+      raise HTTPException(status_code=400, detail="verified property is not valid")
+
     cur = conn.cursor()
 
-    sql = """insert into """
-    cur.execute(sql, (minlon, minlat, maxlon, maxlat) )
-    return cur.fetchone()[0]
+    # Check if osm_id is available
+    statement = sql.SQL("SELECT osm_id FROM {table_name} l WHERE l.osm_id = %s").format(table_name=sql.Identifier(available_data_tables[type_name]))
+    cur.execute(statement, (osm_id,))
+
+    results = cur.fetchone()
+    if results != None and len(cur.fetchone()) < 1:
+      raise HTTPException(status_code=404, detail="osm_id not found")
+
+    statement = sql.SQL("INSERT INTO {table_name} (osm_type, osm_id, verified_at, verified_by, verified) VALUES (%s, %s, %s, %s, %s)").format(table_name=sql.Identifier(available_verification_tables[type_name]))
+    cur.execute(statement, (osm_type, osm_id, verified_at, verified_by, verified ))
+    conn.commit()
+    return 'OK'
+
+
+# @app.get("/export/{type_name}/shapefile")
+# def export_shapefile_region(response: Response, type_name: str, minlon: float= 13.3, minlat : float=52.2, maxlon: float=13.7, maxlat: float=52.3):
+#     if type_name not in availableExportTables.keys():
+#       raise HTTPException(status_code=404, detail="export type unknown")
+
+#     cur = conn.cursor()
+
+#     sql = "select * from " + availableExportTables[type_name] + "(%s, %s, %s, %s);"
+#     cur.execute(sql, ( minlon, minlat, maxlon, maxlat) )
+#     geojson = cur.fetchone()[0]
+
+#     with tempfile.NamedTemporaryFile() as geojsonTmp:
+#       print(geojsonTmp.name)
+#       json.dump(geojson, geojsonTmp)
+#       srcDS = gdal.OpenEx(geojsonTmp.name+'.json')
+
+#       with tempfile.NamedTemporaryFile() as shapeTmp:
+#         ds = gdal.VectorTranslate(shapeTmp.name+'.shp', srcDS, format='ESRI Shapefile')
+
+#         response.headers["Content-Type"] = 'x-gis/x-shapefile'
+
+#         return FileResponse(shapeTmp.name)
