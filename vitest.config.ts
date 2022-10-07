cdab76d62c620202026d862acd7273e09b0f4965
Tobias <tobias@fixmycity.de>
2022-10-07 19:49:25 +0200

    Setup vitest (alternative to Jest) for unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    // https://vitest.dev/config/#globals
+    // globals: true,
+    // https://vitest.dev/config/#environment
+    // environment: 'happy-dom',
+  },
+})
