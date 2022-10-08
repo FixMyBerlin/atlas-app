74fe2ba49177c6e0a42bef1f7612aac305ddb374
Tobias <tobias@fixmycity.de>
2022-10-08 06:51:01 +0200

    Fix api access token
    I moved them out of the .env file, which did not work on production. They are meant to be used publicly, so we should just keep them in the same file.

--- src/components/MapInterface/mapData/sourcesMapDataConfig/sources.const.ts
+++ src/components/MapInterface/mapData/sourcesMapDataConfig/sources.const.ts
@@ -11,6 +11,14 @@ export type MapDataConfigSourcesIds =
   | 'mapillaryMapfeatures'
   | 'mapillaryTrafficSigns'
 
+// https://account.mapbox.com/access-tokens
+// "Default public token"
+const apiKeyMapbox =
+  'pk.eyJ1IjoiaGVqY28iLCJhIjoiY2piZjd2bzk2MnVsMjJybGxwOWhkbWxpNCJ9.L1UNUPutVJHWjSmqoN4h7Q'
+// https://www.mapillary.com/dashboard/developers
+// "Tarmac OSM Viewer", they call it "Client Token"
+const apiKeyMapillary = 'MLY|5337311709720950|61508fdcc416406fd8dfb79748463852'
+
 export const sources: MapDataConfigSource<MapDataConfigSourcesIds>[] = [
   {
     id: 'parkraumParking',
@@ -26,31 +34,23 @@ export const sources: MapDataConfigSource<MapDataConfigSourcesIds>[] = [
   },
   {
     id: 'unfallatlas',
-    tiles: `https://api.mapbox.com/v4/hejco.5oexnrgf/{z}/{x}/{y}.vector.pbf?sku=101bSz70Afq22&access_token=${
-      import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
-    }`,
+    tiles: `https://api.mapbox.com/v4/hejco.5oexnrgf/{z}/{x}/{y}.vector.pbf?sku=101bSz70Afq22&access_token=${apiKeyMapbox}`,
     attributionHtml: 'todo', // TODO
   },
   {
     id: 'tarmacHighways',
-    tiles: `https://api.mapbox.com/v4/hejco.d7mywzd3/{z}/{x}/{y}.vector.pbf?access_token=${
-      import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
-    }`,
+    tiles: `https://api.mapbox.com/v4/hejco.d7mywzd3/{z}/{x}/{y}.vector.pbf?access_token=${apiKeyMapbox}`,
     attributionHtml: 'todo', // TODO
   },
   {
     id: 'tarmacPois',
-    tiles: `https://api.mapbox.com/v4/hejco.3hccfujx/{z}/{x}/{y}.vector.pbf?access_token=${
-      import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
-    }`,
+    tiles: `https://api.mapbox.com/v4/hejco.3hccfujx/{z}/{x}/{y}.vector.pbf?access_token=${apiKeyMapbox}`,
     attributionHtml: 'todo', // TODO
   },
   {
     // https://www.mapillary.com/developer/api-documentation/#coverage-tiles
     id: 'mapillaryCoverage',
-    tiles: `https://tiles.mapillary.com/maps/vtp/mly1_public/2/{z}/{x}/{y}?access_token=${
-      import.meta.env.VITE_MAPILLARY_ACCESS_TOKEN
-    }`,
+    tiles: `https://tiles.mapillary.com/maps/vtp/mly1_public/2/{z}/{x}/{y}?access_token=${apiKeyMapillary}`,
     minzoom: 0,
     maxzoom: 14,
     attributionHtml: 'Daten von Mapillary', // TODO – could not find anything specific; they don't attribute on their own page.
@@ -58,9 +58,7 @@ export const sources: MapDataConfigSource<MapDataConfigSourcesIds>[] = [
   {
     // https://www.mapillary.com/developer/api-documentation/#point-tiles
     id: 'mapillaryMapfeatures',
-    tiles: `https://tiles.mapillary.com/maps/vtp/mly_map_feature_point/2/{z}/{x}/{y}?access_token=${
-      import.meta.env.VITE_MAPILLARY_ACCESS_TOKEN
-    }`,
+    tiles: `https://tiles.mapillary.com/maps/vtp/mly_map_feature_point/2/{z}/{x}/{y}?access_token=${apiKeyMapillary}`,
     minzoom: 14,
     maxzoom: 14,
     attributionHtml: 'Daten von Mapillary', // TODO – could not find anything specific; they don't attribute on their own page.
@@ -68,9 +66,7 @@ export const sources: MapDataConfigSource<MapDataConfigSourcesIds>[] = [
   {
     // https://www.mapillary.com/developer/api-documentation/#traffic-sign-tiles
     id: 'mapillaryTrafficSigns',
-    tiles: `https://tiles.mapillary.com/maps/vtp/mly_map_feature_traffic_sign/2/{z}/{x}/{y}?access_token=${
-      import.meta.env.VITE_MAPILLARY_ACCESS_TOKEN
-    }`,
+    tiles: `https://tiles.mapillary.com/maps/vtp/mly_map_feature_traffic_sign/2/{z}/{x}/{y}?access_token=${apiKeyMapillary}`,
     minzoom: 14,
     maxzoom: 14,
     attributionHtml: 'Daten von Mapillary', // TODO – could not find anything specific; they don't attribute on their own page.
