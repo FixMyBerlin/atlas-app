2a95e8c225cff613f5d074c3a81d4a48d0053b35
Tobias <tobias@fixmycity.de>
2022-10-10 19:39:07 +0200

    Regionen: Fix primary navigation
    React location has to have a cleaner way of solving this…

--- src/components/Layout/Header/components/PrimaryNavigation.tsx
+++ src/components/Layout/Header/components/PrimaryNavigation.tsx
@@ -1,4 +1,5 @@
-import { useMatchRoute, Link } from '@tanstack/react-location'
+import { LocationGenerics } from '@routes/index'
+import { useMatchRoute, Link, useMatch } from '@tanstack/react-location'
 import classNames from 'classnames'
 import React from 'react'
 import { PrimaryNavigationProps } from './types'
@@ -7,25 +8,35 @@ type Props = PrimaryNavigationProps
 
 export const PrimaryNavigation: React.FC<Props> = ({ primaryNavigation }) => {
   const matchRoute = useMatchRoute()
+  const {
+    data: { region },
+  } = useMatch<LocationGenerics>()
 
   return (
     <div className="hidden sm:ml-6 sm:block">
       <div className="flex space-x-4">
-        {primaryNavigation.map((item) => (
-          <Link
-            key={item.name}
-            to={item.href}
-            className={classNames(
-              matchRoute({ to: item.href })
-                ? 'cursor-default bg-gray-900 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-              'rounded-md px-3 py-2 text-sm font-medium'
-            )}
-            aria-current={matchRoute({ to: item.href }) ? 'page' : undefined}
-          >
-            {item.name}
-          </Link>
-        ))}
+        {primaryNavigation.map((item) => {
+          const routeWithRegion = item.href.replaceAll(
+            ':regionPath',
+            region?.path || ''
+          )
+
+          return (
+            <Link
+              key={item.name}
+              to={routeWithRegion}
+              className={classNames(
+                matchRoute({ to: item.href })
+                  ? 'cursor-default bg-gray-900 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'rounded-md px-3 py-2 text-sm font-medium'
+              )}
+              aria-current={matchRoute({ to: item.href }) ? 'page' : undefined}
+            >
+              {item.name}
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
