e049862d45a48b66a0a6994e024460e77e66bb4f
Tobias <tobias@fixmycity.de>
2022-10-10 19:20:46 +0200

    Deactivate unused primary navigation items
    We dont need those for now

--- src/components/Layout/Header/HeaderRegionen/primaryNavigation.const.ts
+++ src/components/Layout/Header/HeaderRegionen/primaryNavigation.const.ts
@@ -3,6 +3,6 @@
 export const primaryNavigation = [
   { name: 'Atlas', href: '/regionen/:regionPath/' },
   { name: 'Mitmachen', href: '/regionen/:regionPath/mitmachen' },
-  { name: 'Planungen', href: '#todo-planungen' },
-  { name: 'Analysen', href: '#tood-analysen' },
+  // { name: 'Planungen', href: '#todo-planungen' },
+  // { name: 'Analysen', href: '#tood-analysen' },
 ]
--- src/components/Layout/Header/secondaryNavigation.const.ts
+++ src/components/Layout/Header/secondaryNavigation.const.ts
@@ -1,12 +1,12 @@
 // On Mobile, both Navigationlists are combined in <MobileMenu>
 
 export const secondaryNavigationGrouped = [
+  // [
+  //   { name: 'Einstellung', href: '#todo-einstellung' },
+  //   { name: 'Abmelden', href: '#todo-abmelden' },
+  // ],
   [
-    { name: 'Einstellung', href: '#todo-einstellung' },
-    { name: 'Abmelden', href: '#todo-abmelden' },
-  ],
-  [
-    { name: 'Feedback', href: '#feedback' },
+    { name: 'Feedback', href: '/kontakt#feedback' },
     { name: 'Datenschutz', href: '/datenschutz' },
     { name: 'Kontakt & Impressum', href: '/kontakt' },
   ],
