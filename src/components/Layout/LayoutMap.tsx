ec5afd23fdf0043e78e28a372d412354d49569ea
Tobias <tobias@fixmycity.de>
2022-10-13 17:56:39 +0200

    Fix layout for content pages

--- src/components/Layout/LayoutMap.tsx
+++ src/components/Layout/LayoutMap.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { HeaderRegionen } from './Header'
 
 type Props = {
@@ -6,11 +7,18 @@ type Props = {
 }
 
 // About h-screen-layout with Header: https://stackoverflow.com/a/64260554/729221
+// About the custom Helmet classes see index.css. We set them here so they do not break our regular content pages.
 export const LayoutMap: React.FC<Props> = ({ children }) => {
   return (
-    <div className="flex h-screen flex-col">
-      <HeaderRegionen />
-      <main className="flex-grow">{children}</main>
-    </div>
+    <>
+      <Helmet>
+        <html lang="de" className="fixed overflow-hidden" />
+        <body className="fixed overflow-hidden" />
+      </Helmet>
+      <div className="flex h-screen flex-col">
+        <HeaderRegionen />
+        <main className="flex-grow">{children}</main>
+      </div>
+    </>
   )
 }
