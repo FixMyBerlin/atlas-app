1d7aa7ad696e73fa1f9f0c5f12ecf41b16999311
Henri <henri@fixmycity.de>
2022-10-21 12:21:50 +0200

    Add automatic creation of functions and tables via Python

--- atlas-geo/api/init_db.py
+++ atlas-geo/api/init_db.py
@@ -1,27 +1,53 @@
 from typing import Union
 from psycopg2 import sql
-from constants import available_datasets
+from constants import available_datasets, function_table_mapping, verification_tables
 import psycopg2
 import json
 import os
 
+databaseServer = os.environ.get('PGHOST', "localhost")
+databaseName = os.environ.get('PGDATABASE', "postgres")
+databaseUser = os.environ.get('PGUSER', "postgres")
+databasePW = os.environ.get('PGPASSWORD', "mysecretpassword")
+databasePort = os.environ.get('PGPORT', "5432")
 
-# TODO does not work yet
-def init_db_exports():
-  databaseServer = os.environ.get('PGHOST', "localhost")
-  databaseName = os.environ.get('PGDATABASE', "postgres")
-  databaseUser = os.environ.get('PGUSER', "postgres")
-  databasePW = os.environ.get('PGPASSWORD', "mysecretpassword")
-  databasePort = os.environ.get('PGPORT', "5432")
+connString = "host=%s dbname=%s user=%s password=%s port=%s" %(databaseServer, databaseName, databaseUser, databasePW, databasePort)
 
-  connString = "host=%s dbname=%s user=%s password=%s port=%s" %(databaseServer, databaseName, databaseUser, databasePW, databasePort)
-
+def create_or_replace_export_functions():
   conn = psycopg2.connect(connString)
 
-  with conn as cursor:
-      print("Starting INIT of database for exports")
-      cursor.execute(open("INIT.sql", "r").read())
-      cursor.close()
-      conn.close()
-      print("Finished INIT of database for exports")
+  with conn.cursor() as cursor:
+    print("Starting INIT of database for exports")
+    with open("INIT_FUNCTIONS.sql", "r") as f:
+      sql = f.read()
+    for table_name, func_name in function_table_mapping.items():
+      print('Create function', func_name,' for table ', table_name)
+      processed_sql = sql.replace('{func_name}', func_name).replace('{table_name}', table_name)
+      cursor.execute(processed_sql)
 
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('=' * 80)
+    print("Finished INIT of database")
+
+def create_verification_tables():
+  conn = psycopg2.connect(connString)
+
+  with conn.cursor() as cursor:
+    print("Starting creating tables for verification")
+    with open("INIT_VERIFICATION_TABLES.sql", "r") as f:
+      sql = f.read()
+    for original_table, table_name in verification_tables.items():
+      print('Create verification table ', table_name)
+      processed_sql = sql.replace('{table_name}', table_name)
+      cursor.execute(processed_sql)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('=' * 80)
+    print("Finished creating tables for verification")
+
+create_or_replace_export_functions()
+create_verification_tables()
