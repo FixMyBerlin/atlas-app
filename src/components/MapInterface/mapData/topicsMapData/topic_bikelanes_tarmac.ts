91dbf23290b13188e3ef986fe69dd7f9482be962
Tobias <tobias@fixmycity.de>
2022-11-08 10:02:09 +0100

    Use Mapbox styles fro bikelanes topic

--- src/components/MapInterface/mapData/topicsMapData/topic_bikelanes_tarmac.ts
+++ src/components/MapInterface/mapData/topicsMapData/topic_bikelanes_tarmac.ts
@@ -1,8 +1,12 @@
 import { MapDataTopic } from '../types'
+import { MapboxStyleLayerGroupBikelanesIds } from './mapboxStyles'
+import { mapboxStyleLayers } from './mapboxStyles/mapboxStyleLayers'
 
 const topic = 'bikelanes_tarmac'
 export type TopicBikelanesId_Tarmac = typeof topic
-export type TopicBikelanesStyleIds_Tarmac = 'default' | 'detailed'
+export type TopicBikelanesStyleIds_Tarmac =
+  | 'default'
+  | MapboxStyleLayerGroupBikelanesIds
 export type TopicBikelanesStyleFilterIds_Tarmac = '_nofilter'
 
 export const topic_bikelanes_tarmac: MapDataTopic = {
@@ -15,20 +19,55 @@ export const topic_bikelanes_tarmac: MapDataTopic = {
       id: 'default',
       name: 'Standard',
       desc: null,
-      layers: [
-        {
-          id: 'default',
-          type: 'line',
-          source: 'tarmac_bikelanes',
-          'source-layer': 'public.bicycleRoadInfrastructure',
-          paint: {
-            'line-color': 'HotPink',
-            'line-width': 10,
-          },
-          enableInspector: true,
-          enableCalculator: false,
-        },
-      ],
+      layers: mapboxStyleLayers({
+        group: 'atlas_bikelanes',
+        source: 'tarmac_bikelanes',
+        sourceLayer: 'public.bicycleRoadInfrastructure',
+      }),
+      interactiveFilters: null,
+    },
+    {
+      id: 'atlas_bikelanes_complete',
+      name: 'Vollständigkeit',
+      desc: null,
+      layers: mapboxStyleLayers({
+        group: 'atlas_bikelanes_complete',
+        source: 'tarmac_bikelanes',
+        sourceLayer: 'public.bicycleRoadInfrastructure',
+      }),
+      interactiveFilters: null,
+    },
+    {
+      id: 'atlas_bikelanes_fresh',
+      name: 'Aktualität',
+      desc: null,
+      layers: mapboxStyleLayers({
+        group: 'atlas_bikelanes_fresh',
+        source: 'tarmac_bikelanes',
+        sourceLayer: 'public.bicycleRoadInfrastructure',
+      }),
+      interactiveFilters: null,
+    },
+    {
+      id: 'atlas_bikelanes_verified',
+      name: 'Prüf-Status',
+      desc: null,
+      layers: mapboxStyleLayers({
+        group: 'atlas_bikelanes_verified',
+        source: 'tarmac_bikelanes',
+        sourceLayer: 'public.bicycleRoadInfrastructure',
+      }),
+      interactiveFilters: null,
+    },
+    {
+      id: 'atlas_bikelanes_complete_fresh_verified',
+      name: 'Vollansicht',
+      desc: null,
+      layers: mapboxStyleLayers({
+        group: 'atlas_bikelanes_complete_fresh_verified',
+        source: 'tarmac_bikelanes',
+        sourceLayer: 'public.bicycleRoadInfrastructure',
+      }),
       interactiveFilters: null,
     },
   ],
