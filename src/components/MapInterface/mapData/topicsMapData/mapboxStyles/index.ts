2df32dc7897818c275acb9542f71da57dc31d91f
Tobias <tobias@fixmycity.de>
2022-11-08 17:10:36 +0100

    Fix type name

--- src/components/MapInterface/mapData/topicsMapData/mapboxStyles/index.ts
+++ src/components/MapInterface/mapData/topicsMapData/mapboxStyles/index.ts
@@ -1,8 +1,8 @@
 export * from './types'
 
 import style from './mapbox-layer-styles-by-group.json'
-import { MapboxStylesByLayerGroup } from './types'
+import { MapboxStylesByLayerGroupIds } from './types'
 
-type StylesByLayerGroup = { group: MapboxStylesByLayerGroup; layers: any }[]
+type StylesByLayerGroup = { group: MapboxStylesByLayerGroupIds; layers: any }[]
 
 export const mapboxStylesByLayerGroup = style as unknown as StylesByLayerGroup
