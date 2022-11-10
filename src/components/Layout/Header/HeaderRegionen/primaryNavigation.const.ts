990bcb3337222fe583be4722105037a68ccf0109
Tobias <tobias@fixmycity.de>
2022-11-10 20:59:25 +0100

    Hide primary navigation since no "mitmachen" yet

--- src/components/Layout/Header/HeaderRegionen/primaryNavigation.const.ts
+++ src/components/Layout/Header/HeaderRegionen/primaryNavigation.const.ts
@@ -1,8 +1,8 @@
 // On Mobile, both Navigationlists are combined in <MobileMenu>
 
 export const primaryNavigation = [
-  { name: 'Atlas', href: '/regionen/:regionPath/' },
-  { name: 'Mitmachen', href: '/regionen/:regionPath/mitmachen' },
+  // { name: 'Atlas', href: '/regionen/:regionPath/' },
+  // { name: 'Mitmachen', href: '/regionen/:regionPath/mitmachen' },
   // { name: 'Planungen', href: '#todo-planungen' },
   // { name: 'Analysen', href: '#tood-analysen' },
 ]
