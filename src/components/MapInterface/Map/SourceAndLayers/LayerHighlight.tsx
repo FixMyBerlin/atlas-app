1e8b1776eb3dd21b9ebaaa22c5d1f5bc19bb377a
Sven König <sven@fixmycity.de>
2022-11-11 13:01:43 +0100

    display selection for fills, circle and symbol (text)

--- src/components/MapInterface/Map/SourceAndLayers/LayerHighlight.tsx
+++ src/components/MapInterface/Map/SourceAndLayers/LayerHighlight.tsx
@@ -31,10 +31,27 @@ export const LayerHighlight: React.FC<Props> = (parentLayerProps) => {
     paint: JSON.parse(JSON.stringify(parentLayerProps.paint)),
   }
 
-  if (!props.paint) props.paint = {}
-  props.paint['line-color'] = 'red'
-  delete props.paint['line-blur']
-  delete props.paint['line-opacity']
+  if (props.type === 'line') {
+    if (!props.paint) props.paint = {}
+    props.paint['line-color'] = 'red'
+    delete props.paint['line-blur']
+    delete props.paint['line-opacity']
+  } else if (props.type === 'fill') {
+    props.paint['fill-color'] = 'red'
+  } else if (props.type === 'circle') {
+    props.paint = {
+      ...props.paint,
+      'circle-color': 'red',
+      'circle-stroke-color': 'black',
+      'circle-stroke-width': 2,
+    }
+  } else if (props.type === 'symbol') {
+    props.paint = {
+      ...props.paint,
+      'text-halo-color': 'red',
+      'text-halo-width': 3,
+    }
+  }
 
   props.filter = ['in', 'osm_id', ...osmIds]
 
