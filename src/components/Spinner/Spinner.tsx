036258092dcb41517ea2514c45454c9adc1e5525
Tobias <tobias@fixmycity.de>
2022-11-14 09:07:07 +0100

    Slow down spinnen
    Ping https://github.com/FixMyBerlin/private-issues/issues/145

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 export const SmallSpinner: React.FC = () => {
   return (
-    <span className="box-border inline-block h-4 w-4 animate-[spin_0.4s_linear_infinite] rounded-full border-2 border-slate-500 border-b-transparent"></span>
+    <span className="box-border inline-block h-4 w-4 animate-[spin_0.75s_linear_infinite] rounded-full border-2 border-slate-500 border-b-transparent"></span>
   )
 }
