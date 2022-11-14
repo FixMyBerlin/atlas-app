b9752ec0b7146c93dd5a0b87965acf73d6daa1a6
Tobias <tobias@fixmycity.de>
2022-11-14 17:07:05 +0100

    Styling: Update link styles for imprint page and such

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -17,7 +17,7 @@ type Props = {
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
 const linkStyles =
-  'text-emerald-500 hover:text-brand-yellow hover:underline active:underline'
+  'underline decoration-yellow-600 underline-offset-4 hover:decoration-2 active:decoration-2 hover:text-yellow-700 active:text-yellow-700'
 export const buttonStyles =
   'inline-flex items-center px-4 py-2 border border-transparent font-semibold rounded-md shadow-sm text-gray-800 bg-brand-yellow hover:bg-yellow-400 group-hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-yellow'
 
