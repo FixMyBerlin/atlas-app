bea669bb8941db53873a957c5d45f21e2bf8f6c2
Tobias <tobias@fixmycity.de>
2022-11-14 17:07:43 +0100

    Lint- & TS-Messages in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       typography: {
