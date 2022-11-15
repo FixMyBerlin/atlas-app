01985fc9db661ef7cff20cfa2fbbd48f727aea8d
Tobias <tobias@fixmycity.de>
2022-11-15 17:14:35 +0100

    Inspector: Extract OtherProperties Component

--- src/components/MapInterface/Inspector/Inspector.tsx
+++ src/components/MapInterface/Inspector/Inspector.tsx
@@ -8,6 +8,7 @@ import { getSourceData, getTopicData } from '../mapData'
 import { useMapStateInteraction } from '../mapStateInteraction/useMapStateInteraction'
 import { Disclosure } from './Disclosure'
 import { InspectorHeader } from './InspectorHeader'
+import { OtherProperties } from './OtherProperties'
 import { ConditionalFormattedMessage, translations } from './translations'
 import { Verification } from './Verification'
 
@@ -61,37 +62,10 @@ export const Inspector: React.FC = () => {
         const sourceId = extractSourceIdIdFromSourceKey(sourceKey.toString())
         const sourceData = getSourceData(sourceId)
 
-        const systemKeys = [
-          '_skip',
-          '_skipNotes',
-          'osm_id',
-          'osm_type',
-          'osm_url',
-          'update_at',
-          'version',
-        ]
         const documentedProperties = Object.fromEntries(
           Object.entries(properties)
             .sort((a, b) => a[0].localeCompare(b[0]))
             .filter(([key, _v]) => sourceData?.documentedKeys?.includes(key))
-        )
-        const otherOsmProperties = Object.fromEntries(
-          Object.entries(properties)
-            .sort((a, b) => a[0].localeCompare(b[0]))
-            .filter(
-              ([key, _v]) =>
-                !sourceData?.documentedKeys?.includes(key) &&
-                !systemKeys.includes(key)
-            )
-        )
-        const systemProperties = Object.fromEntries(
-          Object.entries(properties)
-            .sort((a, b) => a[0].localeCompare(b[0]))
-            .filter(
-              ([key, _v]) =>
-                systemKeys.includes(key) &&
-                !sourceData?.documentedKeys?.includes(key)
-            )
         )
 
         return (
@@ -164,61 +138,15 @@ export const Inspector: React.FC = () => {
                   </tbody>
                 </table>
 
-                <div className="grid grid-cols-2">
-                  <div className="border-t bg-white px-4 py-2.5 text-xs">
-                    {!!Object.keys(otherOsmProperties).length && (
-                      <details className="break-all [&_summary]:open:mb-1 [&_summary]:open:font-semibold">
-                        <summary>Weitere OSM Werte</summary>
-                        {Object.entries(otherOsmProperties).map(
-                          ([key, value]) => {
-                            return (
-                              <p
-                                key={key}
-                                className="mb-0.5 border-b border-gray-200 pb-0.5"
-                              >
-                                <code>
-                                  {key}:{' '}
-                                  {typeof value === 'boolean'
-                                    ? JSON.stringify(value)
-                                    : value}
-                                </code>
-                              </p>
-                            )
-                          }
-                        )}
-                      </details>
-                    )}
-                  </div>
-                  <div className="border-t bg-white px-4 py-2.5 text-xs">
-                    {!!Object.keys(systemProperties).length && (
-                      <details className="break-all [&_summary]:open:mb-1 [&_summary]:open:font-semibold">
-                        <summary>System-Werte</summary>
-                        {Object.entries(systemProperties).map(
-                          ([key, value]) => {
-                            return (
-                              <p
-                                key={key}
-                                className="mb-0.5 border-b border-gray-200 pb-0.5"
-                              >
-                                <code>
-                                  {key}:{' '}
-                                  {typeof value === 'boolean'
-                                    ? JSON.stringify(value)
-                                    : value}
-                                </code>
-                              </p>
-                            )
-                          }
-                        )}
-                      </details>
-                    )}
-                  </div>
-                </div>
+                <OtherProperties
+                  properties={properties}
+                  documentedKeys={sourceData?.documentedKeys}
+                />
 
                 <Verification
                   visible={allowVerify}
                   sourceKey={sourceKey.toString()}
-                  objectId={systemProperties['osm_id']}
+                  objectId={properties.osm_id}
                 />
               </Disclosure>
             </IntlProvider>
