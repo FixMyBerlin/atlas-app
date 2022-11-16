d55d6efdcafc7ec705b62017308740f1d64ec0c8
Tobias <tobias@fixmycity.de>
2022-11-16 17:39:23 +0100

    Inspector: Extract WIP StatusTable

--- src/components/MapInterface/Inspector/Inspector.tsx
+++ src/components/MapInterface/Inspector/Inspector.tsx
@@ -1,4 +1,3 @@
-import { CheckBadgeIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import { FormattedMessage, IntlProvider } from 'react-intl'
 import {
@@ -11,6 +10,7 @@ import { Disclosure } from './Disclosure'
 import { InspectorHeader } from './InspectorHeader'
 import { Links } from './Links'
 import { OtherProperties } from './OtherProperties'
+import { StatusTable } from './StatusTable'
 import { ConditionalFormattedMessage, translations } from './translations'
 import { Verification } from './Verification'
 
@@ -138,70 +138,10 @@ export const Inspector: React.FC = () => {
                   </tbody>
                 </table>
 
-                <div className="border-t bg-gray-50 px-4 py-2.5 text-xs">
-                  <h4 className="mb-2 font-semibold">
-                    Bewertung der Datenqualität
-                  </h4>
-                  <div className="grid grid-cols-3">
-                    <div>
-                      <h5 className="font-semibold">Vollständigkeit</h5>
-                      {properties.is_present === true && (
-                        <span title="Die Haupt-Dimension dieses Datensatzes wurde erfasst.">
-                          <CheckBadgeIcon className="h-5 w-5" />
-                        </span>
-                      )}
-                      {properties.is_present === false && (
-                        <span title="Es fehlen Angaben zur Haupt-Dimension dieses Datensatzes.">
-                          TODO
-                        </span>
-                      )}
-                      {properties.is_present === undefined && (
-                        <span title="">Keine Angabe</span>
-                      )}
-                    </div>
-                    <div>
-                      <h5 className="font-semibold">Aktualität</h5>
-                      {properties.is_fresh === true && (
-                        <span title="">
-                          <CheckBadgeIcon className="h-5 w-5" />
-                          <br />
-                          {properties.fresh_age_days} Tage alt
-                          <br />
-                          {sourceData?.freshnessDateKey &&
-                            properties[sourceData?.freshnessDateKey] && (
-                              <>
-                                Prüfdatum:{' '}
-                                {new Date(
-                                  properties[sourceData?.freshnessDateKey]
-                                ).toLocaleString('de-DE')}
-                              </>
-                            )}
-                        </span>
-                      )}
-                      {properties.is_fresh === false && (
-                        <span title="">Nicht Aktuell</span>
-                      )}
-                      {properties.is_fresh === undefined && (
-                        <span title="">Keine Angabe</span>
-                      )}
-                    </div>
-                    <div>
-                      <h5 className="font-semibold">Überprüfung (intern)</h5>
-                      {properties.verified === 'approfed' && (
-                        <span title="">
-                          <CheckBadgeIcon className="h-5 w-5" />
-                        </span>
-                      )}
-                      {properties.verified === 'rejected' && (
-                        <span title="">Überarbeitung nötig</span>
-                      )}
-                      {properties.verified === undefined && (
-                        <span title="">Keine Angabe</span>
-                      )}
-                    </div>
-                  </div>
-                </div>
-
+                <StatusTable
+                  properties={properties}
+                  freshnessDateKey={sourceData?.freshnessDateKey}
+                />
                 <Links properties={properties} />
                 <OtherProperties
                   properties={properties}
