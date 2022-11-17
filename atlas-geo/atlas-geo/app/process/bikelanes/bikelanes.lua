04a913b1230a465122f9bd6316fb2ea80aac2cff
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-11-17 18:10:31 +0100

    disable logic in `roadWidth()` and always return 8 until string parsing is stable

--- atlas-geo/atlas-geo/app/process/bikelanes/bikelanes.lua
+++ atlas-geo/atlas-geo/app/process/bikelanes/bikelanes.lua
@@ -43,17 +43,17 @@ local translateTable = osm2pgsql.define_table({
 })
 
 local function roadWidth(tags)
-  if tags["width"] ~= nil then
-    return tonumber(string.gmatch(tags["width"], "[^%s;]+")())
-  end
-  if tags["est_width"] ~= nil then
-    return tonumber(string.gmatch(tags["est_width"], "[^%s;]+")())
-  end
-  local streetWidths = {primary=10, secondary=8, tertiary=6, residential=6}
-  if streetWidths[tags["highway"]] ~= nil then
-    return streetWidths[tags["highway"]]
-  end
-  return 6
+  -- if tags["width"] ~= nil then
+  --   return tonumber(string.gmatch(tags["width"], "[^%s;]+")())
+  -- end
+  -- if tags["est_width"] ~= nil then
+  --   return tonumber(string.gmatch(tags["est_width"], "[^%s;]+")())
+  -- end
+  -- local streetWidths = {primary=10, secondary=8, tertiary=6, residential=6}
+  -- if streetWidths[tags["highway"]] ~= nil then
+  --   return streetWidths[tags["highway"]]
+  -- end
+  return 8
 end
 
 -- PREDICATES FOR EACH CATEGORY:
