b267f247c27f4f83aa83d0a9c20709155c427bb6
Sven König <sven@fixmycity.de>
2022-11-23 12:15:21 +0100

    get distinct features

--- atlas-geo/atlas-geo/api/init_db.py
+++ atlas-geo/atlas-geo/api/init_db.py
@@ -1,9 +1,7 @@
-from psycopg2 import sql
-from constants import available_datasets, export_geojson_function_from_type, verification_tables, verification_views
+from constants import export_geojson_function_from_type, verification_tables, verification_views
 from db import conn_string
 import psycopg2
-import json
-import os
+
 
 def create_or_replace_export_functions():
   conn = psycopg2.connect(conn_string)
@@ -13,7 +11,7 @@ def create_or_replace_export_functions():
     with open("INIT_FUNCTIONS.sql", "r") as f:
       sql = f.read()
     for table_name, func_name in export_geojson_function_from_type.items():
-      print('Create function', func_name,' for table ', table_name)
+      print('Create function', func_name, ' for table ', table_name)
       processed_sql = sql.replace('{func_name}', func_name).replace('{table_name}', table_name)
       cursor.execute(processed_sql)
 
@@ -32,7 +30,11 @@ def create_verification_tables():
       sql = f.read()
     for original_table, table_name in verification_tables.items():
       print('Create verification table and view ', table_name)
-      processed_sql = sql.replace('{table_name}', table_name).replace('{original_table}', original_table).replace('{view_name}', verification_views[table_name])
+      processed_sql = sql.format(
+        table_name=table_name,
+        original_table=original_table,
+        view_name=verification_views[table_name],
+      )
       cursor.execute(processed_sql)
 
     conn.commit()
