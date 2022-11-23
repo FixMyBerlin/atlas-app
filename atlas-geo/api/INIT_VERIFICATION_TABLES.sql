b0dc2f071d39e8ae27e7e605860bd20da6cc3db5
Sven König <sven@fixmycity.de>
2022-11-23 12:15:21 +0100

    get distinct features

--- atlas-geo/api/INIT_VERIFICATION_TABLES.sql
+++ atlas-geo/api/INIT_VERIFICATION_TABLES.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS public.{table_name} (
 
 -- JOIN original data table with verification table
 CREATE OR REPLACE VIEW {view_name} AS
-SELECT o.osm_type, o.osm_id, v.verified_at, v.verified_by , v.verified, o.tags, o.geom FROM "{original_table}" o
+SELECT DISTINCT ON (o.osm_id) o.osm_type, o.osm_id, v.verified_at, v.verified_by , v.verified, o.tags, o.geom
+FROM "{original_table}" o
 LEFT JOIN  {table_name} v
 ON v.osm_id = o.osm_id
-AND v.osm_type = o.osm_type;
+AND v.osm_type = o.osm_type
+ORDER BY o.osm_id, v.verified_at DESC;
