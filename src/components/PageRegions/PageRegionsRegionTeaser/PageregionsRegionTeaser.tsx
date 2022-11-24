f93c5e37e5e64c659d3e64acd50adde423d6a1da
Tobias <tobias@fixmycity.de>
2022-11-24 09:07:28 +0100

    Remove unused content on region teaser page

--- src/components/PageRegions/PageRegionsRegionTeaser/PageregionsRegionTeaser.tsx
+++ src/components/PageRegions/PageRegionsRegionTeaser/PageregionsRegionTeaser.tsx
@@ -21,18 +21,10 @@ export const PageRegionsRegionTeaser: React.FC<Props> = ({ region }) => {
           </span>
         </div>
         <div className="pt-10 pb-4 text-center">
-          <h3 className="text-sm font-medium text-gray-900">
-            <span aria-hidden="true" className="absolute inset-0" />
-            {region.name}
+          <h3 className="text-xl font-medium text-gray-900">
+            Radverkehrsatlas <br />
+            {region.fullName}
           </h3>
-          <div className="mt-3 flex flex-col items-center">
-            <p className="sr-only">{region.path} out of 5 stars</p>
-            <div className="flex items-center">Icons</div>
-            <p className="mt-1 text-sm text-gray-500">{region.path} reviews</p>
-          </div>
-          <p className="mt-4 text-base font-medium text-gray-900">
-            {region.path}
-          </p>
         </div>
       </div>
     </Link>
