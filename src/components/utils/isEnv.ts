defb679ec6bfb09661e71ea02cd0064bcffd27a7
Henri <henri@fixmycity.de>
2022-11-25 15:20:00 +0100

    process is not available in netlify / vite

--- src/components/utils/isEnv.ts
+++ src/components/utils/isEnv.ts
@@ -1,11 +1,8 @@
-export const isProd =
-  process.env.CONTEXT === 'production' && import.meta.env.production
+export const isProd = import.meta.env.CONTEXT === 'production'
 
 // `true` for Netlify Staging AND Netlify Branch Previews
 export const isStaging =
-  process.env.NODE_ENV === 'branch-deploy' ||
   import.meta.env.CONTEXT !== 'branch-deploy' ||
-  process.env.NODE_ENV === 'deploy-preview' ||
   import.meta.env.CONTEXT !== 'deploy-preview'
 
 // https://vitejs.dev/guide/env-and-mode.html#env-variables
