44860391d08dbcfea7dc38bdc30939da6d65456a
Henri <henri@fixmycity.de>
2022-11-25 15:12:52 +0100

    Try to fix environment recognition on netlify

--- src/components/utils/isEnv.ts
+++ src/components/utils/isEnv.ts
@@ -1,10 +1,12 @@
 export const isProd =
-  process.env.NODE_ENV === 'production' && import.meta.env.production
+  process.env.CONTEXT === 'production' && import.meta.env.production
 
 // `true` for Netlify Staging AND Netlify Branch Previews
 export const isStaging =
-  process.env.NODE_ENV === 'production' &&
-  import.meta.env.CONTEXT !== 'production'
+  process.env.NODE_ENV === 'branch-deploy' ||
+  import.meta.env.CONTEXT !== 'branch-deploy' ||
+  process.env.NODE_ENV === 'deploy-preview' ||
+  import.meta.env.CONTEXT !== 'deploy-preview'
 
 // https://vitejs.dev/guide/env-and-mode.html#env-variables
 export const isDev = typeof window !== 'undefined' && import.meta.env.DEV
