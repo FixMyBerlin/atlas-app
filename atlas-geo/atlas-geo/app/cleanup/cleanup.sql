f67ca62ee9fb4cdfbc662c1961cadf4cfbdeab55
Tobias <tobias@fixmycity.de>
2022-11-28 16:27:21 +0100

    Cleanup: Remove `bikelanesCenterline` (its *New now)

--- atlas-geo/atlas-geo/app/cleanup/cleanup.sql
+++ atlas-geo/atlas-geo/app/cleanup/cleanup.sql
@@ -1,4 +1,4 @@
 -- Learn more about this file in README.md
 
-DROP TABLE IF EXISTS "bike_infrastructure_verified";
+DROP TABLE IF EXISTS "bikelanesCenterline";
 -- DROP FUNCTION IF EXISTS export_geojson_landuse;
