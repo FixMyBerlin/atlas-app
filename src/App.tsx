da7841f5ca84c07a66f158d7672c6a95c94b7492
Tobias <tobias@fixmycity.de>
2022-11-30 15:37:38 +0100

    Fix staging/production check (maybe)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { isDev, isProd, isStaging } from '@components/utils'
 import { Outlet, Router } from '@tanstack/react-location'
 import { location, routes } from './routes'
 
 const App = () => {
   return (
     <Router location={location} routes={routes}>
-      <div className="relative flex-auto">
+      <div
+        className="relative flex-auto"
+        data-env={JSON.stringify({
+          isProd,
+          isStaging,
+          isDev,
+          vite_mode: import.meta.env.MODE,
+          custom: import.meta.env.VITE_NETLIFY_CONTEXT,
+        })}
+      >
         <Outlet />
       </div>
     </Router>
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,14 @@
 
 interface ImportMetaEnv {
   readonly VITE_MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
+
   readonly VITE_TILES_ENV: 'staging' | 'production' | 'development'
   readonly VITE_API_ENV: 'staging' | 'production' | 'development'
+
+  /** @desc Custom env variables to select the environment.
+   * Specified at https://app.netlify.com/sites/radverkehrsatlas/settings/env
+   * Use with `import.meta.env.VITE_NETLIFY_CONTEXT` */
+  readonly VITE_NETLIFY_CONTEXT: 'production' | 'staging' | 'development'
 }
 
 interface ImportMeta {
