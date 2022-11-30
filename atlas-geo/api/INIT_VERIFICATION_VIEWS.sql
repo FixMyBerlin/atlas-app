41b07cab23e277b6e3e2286d18aea29f3a034820
Sven König <sven@fixmycity.de>
2022-11-30 18:28:05 +0100

    refactor init_db

--- atlas-geo/api/INIT_VERIFICATION_VIEWS.sql
+++ atlas-geo/api/INIT_VERIFICATION_VIEWS.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS public.{table_name} (
+CREATE TABLE IF NOT EXISTS public.{verification_table} (
 	osm_type varchar NOT NULL,
+  -- osm_type bpchar(1) NOT NULL,
 	osm_id bigint NOT NULL,
+	-- osm_id int8 NOT NULL,
 	verified_at timestamp NOT NULL,
 	verified_by bigint,
 	verified varchar NULL
@@ -9,8 +11,8 @@ CREATE TABLE IF NOT EXISTS public.{table_name} (
 -- JOIN original data table with verification table
 CREATE OR REPLACE VIEW {view_name} AS
 SELECT DISTINCT ON (o.osm_id) o.osm_type, o.osm_id, v.verified_at, v.verified_by , v.verified, o.tags, o.geom
-FROM "{original_table}" o
-LEFT JOIN  {table_name} v
+FROM {geometry_table} o
+LEFT JOIN  {verification_table} v
 ON v.osm_id = o.osm_id
 AND v.osm_type = o.osm_type
 ORDER BY o.osm_id, v.verified_at DESC;
