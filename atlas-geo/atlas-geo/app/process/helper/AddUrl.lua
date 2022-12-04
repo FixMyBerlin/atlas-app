4404af1f110f133e60e2f16d78173fb06087f8f3
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-12-04 17:44:43 +0100

    add metaData to center line objs

--- atlas-geo/atlas-geo/app/process/helper/AddUrl.lua
+++ atlas-geo/atlas-geo/app/process/helper/AddUrl.lua
@@ -1,4 +1,8 @@
 -- * @param `osm_type` can be 'way', 'node', 'relation' (valid osm.org URL parts)
+function OsmUrl(osm_type, object)
+  return "https://osm.org/" .. osm_type .. "/" .. object.id
+end
+
 function AddUrl(osm_type, object)
-  object.tags.osm_url = "https://osm.org/" .. osm_type .. "/" .. object.id
+  object.tags.osm_url = OsmUrl(osm_type, object)
 end
