ca596d0dd665f887e83a6f55c07a69ac9a7dfda1
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-12-07 14:49:35 +0100

    refactor: rename variable `tagsObject` -> `tags`

--- atlas-geo/atlas-geo/app/process/helper/FilterTags.lua
+++ atlas-geo/atlas-geo/app/process/helper/FilterTags.lua
@@ -1,20 +1,21 @@
 -- Playground http://tpcg.io/_5LRFVV
 
-function FilterTags(tagsObject, allowedTags)
-  for key, _ in pairs(tagsObject) do
+function FilterTags(tags, allowedTags)
+  for key, _ in pairs(tags) do
     if not allowedTags[key] then
-      tagsObject[key] = nil
+      tags[key] = nil
     end
   end
 end
 
-function FilterTags2(tags, allowedTags)
-  local tagsFiltered = {}
-  for key, _ in pairs(allowedTags) do
-    if tags[key] ~= nil then
-      tagsFiltered[key] = tags[key]
-    end
-  end
-  return tagsFiltered
-end
+-- this function is faster iff |tags| > |allowedTags|
+-- function FilterTags2(tags, allowedTags)
+--   local tagsFiltered = {}
+--   for key, _ in pairs(allowedTags) do
+--     if tags[key] ~= nil then
+--       tagsFiltered[key] = tags[key]
+--     end
+--   end
+--   return tagsFiltered
+-- end
 
