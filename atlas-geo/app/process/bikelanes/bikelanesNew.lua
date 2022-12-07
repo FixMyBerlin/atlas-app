fadf559dd4bdc637942cfd787750a990d5825a32
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-12-07 16:36:26 +0100

    extract transformations / make categorizeBikelane non mutating

--- atlas-geo/app/process/bikelanes/bikelanesNew.lua
+++ atlas-geo/app/process/bikelanes/bikelanesNew.lua
@@ -14,13 +14,15 @@ require("AddSkipInfoByWidth")
 require("CheckDataWithinYears")
 require("StartsWith")
 require("categories")
+require("transformations")
 
 local table = osm2pgsql.define_table({
   name = 'bikelanesNew',
   ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
   columns = {
     { column = 'tags', type = 'jsonb' },
-    { column  = 'meta', type = 'jsonb'},
+    { column = 'category', type = 'text'},
+    { column = 'meta', type = 'jsonb'},
     { column = 'geom', type = 'linestring' },
   }
 })
@@ -30,6 +32,7 @@ local translateTable = osm2pgsql.define_table({
   ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
   columns = {
     { column = 'tags', type = 'jsonb' },
+    { column = 'category', type = 'text'},
     { column = 'meta', type = 'jsonb'},
     { column = 'geom', type = 'linestring' },
     { column = 'offset', type = 'real' }
@@ -53,7 +56,6 @@ local allowed_tags = Set({
   "_skip",
   "_skipNotes",
   "_direction",
-  "_offset",
   "access",
   "bicycle_road",
   "bicycle",
@@ -74,6 +76,7 @@ local allowed_tags = Set({
 })
 
 
+
 local function timeValidation(tags)
     -- IMO this whole logic should be implemented on the database
     -- e.g. only safe the last date of modification and then define computated properties
@@ -85,12 +88,6 @@ local function timeValidation(tags)
     tags.fresh_age_days = withinYears.diffDays
 end
 
--- local function normalizeTags(tags)
---   FilterTags(tags, allowed_tags)
---   timeValidation(tags)
---   return tags
--- end
-
 local function intoSkipList(object)
   -- normalizeTags(object.tags)
   skipTable:insert({
@@ -101,30 +98,27 @@ local function intoSkipList(object)
 end
 
 -- projects all tags prefix:subtag=val -> subtag=val
-local function projectTags(tags, prefix)
-  local projectedTags = {}
+local function transformTags(tags, prefix)
+  local transformedTags = {name = tags.name}
   for prefixedKey,val  in pairs(tags) do
     if prefixedKey ~= prefix and StartsWith(prefixedKey, prefix) then
       -- offset of 2 due to 1-indexing and for removing the ':'
       local key = string.sub(prefixedKey, string.len(prefix) + 2)
-      projectedTags[key] = val
+      transformedTags[key] = val
     end
   end
-  return projectedTags
+  return transformedTags
 end
 
 function osm2pgsql.process_way(object)
   if not object.tags.highway then return end
-
-  local allowed_values = HighwayClasses
-
   -- Skip `highway=steps`
   -- We don't look at ramps on steps ATM. That is not good bicycleInfrastructure anyways
-  allowed_values["steps"] = nil
+  HighwayClasses["steps"] = nil
   -- values that we would allow, but skip here:
   -- "construction", "planned", "proposed", "platform" (Haltestellen),
   -- "rest_area" (https://wiki.openstreetmap.org/wiki/DE:Tag:highway=rest%20area)
-  if not allowed_values[object.tags.highway] then return end
+  if not HighwayClasses[object.tags.highway] then return end
 
   AddSkipInfoToHighways(object)
 
@@ -134,11 +128,13 @@ function osm2pgsql.process_way(object)
   end
 
   -- apply predicates
-  if BikelaneCategory(object.tags) then
+  local category = CategorizeBikelane(object.tags)
+  if category ~= nil then
     object.tags._skipNotes = nil
     FilterTags(object.tags, allowed_tags)
     table:insert({
       tags = object.tags,
+      category = category,
       meta = Metadata(object),
       geom = object:as_linestring()
     })
@@ -146,71 +142,42 @@ function osm2pgsql.process_way(object)
   end
 
   -- apply predicates nested
-  -- for that we define "projections" as:
-  -- `highway` is the new highway type for the generated object
-  -- `prefix` will be concatinated wiht one of the sides `':left' | ':right' | ':both' | '' `
-  -- all tags wich have the concatination as a preix get projected -> w\o prefix
-  -- tagsCC is a list of tags which get copied from the parent object if they don't exist yet on the generated one
-  local footwayProjection = {
-    highway = "footway",
-    prefix = "sidewalk",
-  }
-  local function laneSurface(cycleway, parent)
-    -- TODO: check with category instead
-    if cycleway.bicycle=='lane' then
-      cycleway["surface"] = parent["surface"]
-      cycleway["smoothness"] = parent["smoothness"]
-    end
-  end
-  local function offsetLanes(cycleway)
-  end
-
-  local function cyclewayPP(cycleway, parent)
-    laneSurface(cycleway, parent)
-    offsetLanes(cycleway)
-    return true
-  end
-
-  local cyclewayProjection = {
-    highway = "cycleway",
-    prefix = "cycleway",
-    postProcessing = cyclewayPP,-- for cycleways on streets surface and smoothness should match
-  }
-  local projections = { footwayProjection, cyclewayProjection }
-
-  local projSides = {
+  local sides = {
     [":right"] = { -1 },
     [":left"] = { 1 },
     [":both"] = { -1, 1 },
     [""] = { -1, 1 }
   }
-
-  for _, projection in pairs(projections) do
-    -- NOTE: the category/projection should also influence the offset
+  for _, transformation in pairs(Transformations) do
+    -- NOTE: the category/transformation should also influence the offset
     -- e.g. a street with bike lane should have offset=streetWidth/2 - bikelaneWidth/2
     -- where a sidewalk with bicycle=yes should have offset=streetWidth/2 + bikelaneWidth/2
     local offset = RoadWidth(object.tags) / 2
-    for side, signs in pairs(projSides) do
-      local prefixedSide = projection.prefix .. side
-      if object.tags[prefixedSide] ~= "no" and object.tags[prefixedSide] ~="separate" then
-        local cycleway = projectTags(object.tags, prefixedSide)
-        cycleway["highway"] = projection.highway
-        cycleway["name"] = cycleway["name"]
-        cycleway[projection.prefix] = object.tags[prefixedSide] -- project the `side` to the `prefix`
-        if BikelaneCategory(cycleway) then
+    for side, signs in pairs(sides) do
+      local prefixedSide = transformation.prefix .. side
+      if object.tags[prefixedSide] ~= "no" and object.tags[prefixedSide] ~= "separate" then
+        -- this is the transformation:
+        local cycleway = transformTags(object.tags, prefixedSide)
+        cycleway.highway = transformation.highway
+        cycleway[transformation.prefix] = object.tags[prefixedSide] -- project `prefix:side` to `prefix`
+        local offset = RoadWidth(object.tags) / 2
+        category = CategorizeBikelane(cycleway)
+        if category ~= nil then
           cycleway._centerline = "projected tag=" .. prefixedSide
           for _, sign in pairs(signs) do
             local isOneway = object.tags['oneway'] == 'yes' and object.tags['oneway:bicycle'] ~= 'no'
             if not (side == "" and sign > 0 and isOneway)  then -- skips implicit case for oneways
-              if projection.postProcessing ~= nil or projection.postProcessing(cycleway, object.tags) then
-                  FilterTags(cycleway, allowed_tags)
-                  translateTable:insert({
-                    tags = cycleway,
-                    meta = Metadata(object),
-                    geom = object:as_linestring(),
-                    offset = sign * offset
-                  })
+              if transformation.postProcessing ~= nil then
+                offset = offset + transformation.postProcessing(category, cycleway, object.tags)
               end
+                FilterTags(cycleway, allowed_tags)
+                translateTable:insert({
+                  tags = cycleway,
+                  category = category,
+                  meta = Metadata(object),
+                  geom = object:as_linestring(),
+                  offset = sign * offset
+                })
             end
           end
         end
@@ -221,8 +188,6 @@ function osm2pgsql.process_way(object)
   -- TODO SKIPLIST: For ZES, we skip "Verbindungsstücke", especially for the "cyclewayAlone" case
   -- We would have to do this in a separate processing step or wait for length() data to be available in LUA
   -- MORE: osm-scripts-Repo => utils/Highways-BicycleWayData/filter/radwegVerbindungsstueck.ts
-  if object.tags.category == nil then
-    intoSkipList(object)
-  end
+  intoSkipList(object)
 end
 
