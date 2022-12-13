a68a45810cfd9702ae5e8589e33623b62a93aafc
Tobias <tobias@fixmycity.de>
2022-12-13 17:44:33 +0100

    Cleanup bikelanes_v1 aka bikelanes_old
    This did not work like before, anymore; not worth fixing.

--- atlas-geo/atlas-geo/app/run-4-process.sh
+++ atlas-geo/atlas-geo/app/run-4-process.sh
@@ -23,9 +23,6 @@ echo "\e[1m\e[7m PROCESS – Topic: bikelanes \e[27m\e[21m"
 ${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}bikelanes/bikelanes.lua ${OSM_FILTERED_FILE}
 psql -q -f "${PROCESS_DIR}bikelanes/bikelanesCenterline.sql"
 
-echo "\e[1m\e[7m PROCESS – Topic: bikelanes_v1 \e[27m\e[21m"
-${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}bikelanes/bikelanes_v1.lua ${OSM_FILTERED_FILE}
-
 echo "\e[1m\e[7m PROCESS – Topic: boundaries \e[27m\e[21m"
 ${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}boundaries.lua ${OSM_FILTERED_FILE}
 
