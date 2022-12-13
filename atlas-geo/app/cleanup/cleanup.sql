a36823ed5737655707add64ce68dbc2b960fb56b
Tobias <tobias@fixmycity.de>
2022-12-13 17:44:33 +0100

    Cleanup bikelanes_v1 aka bikelanes_old
    This did not work like before, anymore; not worth fixing.

--- atlas-geo/app/cleanup/cleanup.sql
+++ atlas-geo/app/cleanup/cleanup.sql
@@ -13,5 +13,6 @@ DROP TABLE IF EXISTS "bikelanesCenterlineNew";
 DROP TABLE IF EXISTS "bikelanes_skipList";
 DROP TABLE IF EXISTS "testTable";
 DROP TABLE IF EXISTS "bikelanes_skipListNew";
+DROP TABLE IF EXISTS "bikelanes_v1";
 DROP FUNCTION IF EXISTS "update_lit_verified" CASCADE;
 DROP FUNCTION IF EXISTS "update_bikelanes_verified" CASCADE;
