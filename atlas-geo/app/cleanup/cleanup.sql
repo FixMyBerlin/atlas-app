2f6d7b9f963939bacd088a2256a364253744fd9d
Tobias <tobias@fixmycity.de>
2022-12-13 15:27:00 +0100

    cleanup: Remove all those non needed tables

--- atlas-geo/app/cleanup/cleanup.sql
+++ atlas-geo/app/cleanup/cleanup.sql
@@ -1,4 +1,16 @@
 -- Learn more about this file in README.md
 
+DROP TABLE IF EXISTS "bike_infrastructure_verification";
+DROP TABLE IF EXISTS "lit_new";
+DROP TABLE IF EXISTS "bikelanes_new";
+DROP TABLE IF EXISTS "lit_TEMP";
+DROP TABLE IF EXISTS "bikelanes_old";
+DROP TABLE IF EXISTS "bikelanes_skipList_old";
+DROP TABLE IF EXISTS "bikelanes_TEMP";
+DROP TABLE IF EXISTS "bikelanesNew";
 DROP TABLE IF EXISTS "bikelanesCenterline";
+DROP TABLE IF EXISTS "bikelanesCenterlineNew";
+DROP TABLE IF EXISTS "bikelanes_skipList";
+DROP TABLE IF EXISTS "testTable";
+DROP TABLE IF EXISTS "bikelanes_skipListNew";
 -- DROP FUNCTION IF EXISTS export_geojson_landuse;
