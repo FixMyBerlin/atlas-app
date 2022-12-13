b34d193fd718d31603ac9ab553f07e7743bd8346
Tobias <tobias@fixmycity.de>
2022-12-13 15:03:04 +0100

    Rename postfix `_new` to `_<NAME>_temp`
    That communicates a bit better the intended usage.
    
    See also https://github.com/FixMyBerlin/private-issues/issues/186#issuecomment-1327614550

--- atlas-geo/app/POSTPROCESS.sql
+++ atlas-geo/app/POSTPROCESS.sql
@@ -2,8 +2,8 @@ BEGIN;
 DROP TABLE IF EXISTS lit CASCADE;
 DROP TABLE IF EXISTS bikelanes CASCADE;
 
-ALTER TABLE lit_new RENAME TO lit;
-ALTER TABLE bikelanes_new RENAME TO bikelanes;
+ALTER TABLE _lit_temp RENAME TO lit;
+ALTER TABLE _bikelanes_temp RENAME TO bikelanes;
 COMMIT;
 
 --- Views are newly created in init_db.py because they are deleted via the DROP TABLE CASCADE
