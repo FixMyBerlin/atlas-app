0a7ee512fb9e8638ac1f121a9c2605625bbf047c
Tobias <tobias@fixmycity.de>
2022-12-13 17:05:58 +0100

    Document (and format) Freshness helper

--- atlas-geo/atlas-geo/app/process/shared/InferAddress.lua
+++ atlas-geo/atlas-geo/app/process/shared/InferAddress.lua
@@ -1,14 +1,16 @@
 -- * @desc Handle different address tagging schema.
 --    Docs <https://wiki.openstreetmap.org/wiki/Key:addr:*>
 -- * @returns
---   `dest` if provided with `AddressKeys` are added to it.
---   Otherwise a new object of format `AddressKeys` ist returned.
+--   `dest` if provided with `AddressKeys` added to it.
+--   Otherwise a new object of format `AddressKeys` is returned.
 function InferAddress(tags, dest)
   dest = dest or {}
+
   dest.addr_street = tags.street or tags["addr:street"]
   dest.addr_zip = tags.postcode or tags["addr:postcode"]
   dest.addr_city = tags.city or tags["addr:city"]
   dest.addr_number = tags.housenumber or tags["addr:housenumber"]
+
   return dest
 end
 
