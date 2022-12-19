f278030ca06e0c3e807ed5a83939486c746aba83
Tobias <tobias@fixmycity.de>
2022-12-19 16:49:50 +0100

    Add debugging colors for styles
    This needs to be activated manually. But it is useful to debug style layer conditions like data filter and style filter etc. Set `useDebugStyle = true` to paint line geometries in purple.

--- src/components/MapInterface/mapData/topicsMapData/mapboxStyles/mapboxStyleLayers.ts
+++ src/components/MapInterface/mapData/topicsMapData/mapboxStyles/mapboxStyleLayers.ts
@@ -7,7 +7,35 @@ type Props = {
   sourceLayer: string
 }
 
+const debugStyleLayer = ({
+  source,
+  sourceLayer,
+}: Pick<Props, 'source' | 'sourceLayer'>) => {
+  // TODO: Right now this will only work for type=line; it needs refactoring to work with points and areas.
+  return [
+    {
+      id: 'debugStyleLayer',
+      type: 'line',
+      source: source,
+      'source-layer': sourceLayer,
+      paint: {
+        'line-width': 20,
+        'line-color': '#a21caf',
+        'line-opacity': 0.6,
+      },
+      layout: {},
+    },
+  ]
+}
+
+// This is for debugging. We might want to put this into a state, later.
+const useDebugStyle = false
+
 export const mapboxStyleLayers = ({ group, source, sourceLayer }: Props) => {
+  if (useDebugStyle) {
+    return debugStyleLayer({ source, sourceLayer })
+  }
+
   const mapboxLayers = mapboxStylesByLayerGroup.find(
     (g: any) => g.group === group
   )?.layers
