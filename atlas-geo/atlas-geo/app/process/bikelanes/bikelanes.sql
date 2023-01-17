c1003f4dea4a44c6a205feae388d25c461f48e38
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-01-17 10:58:07 +0100

    update comments

--- atlas-geo/atlas-geo/app/process/bikelanes/bikelanes.sql
+++ atlas-geo/atlas-geo/app/process/bikelanes/bikelanes.sql
@@ -1,11 +1,12 @@
--- What happens:
+-- remove categories which are only used for checking the presence of data
+DELETE FROM "_bikelanes_temp" WHERE category='only_data' OR category='not_expected';
+
+-- What happens here:
 -- we project to cartesian coordinates
 -- move the geometry by `_offset` (+ left / - right)
 -- because negative offsets reverse the order and we want the right side to be aligned we reverse the order again
 -- additionally we check wether the geometry is `simple` because otherwise we might get a MLString
--- remove categories which are only used for checking the presence of data
-DELETE FROM "_bikelanes_temp" WHERE category='only_data' OR category='not_expected';
-
+-- for the same reason we simplify the geometries
 -- TODO: check parameters `quad_segs` and  `join`
 UPDATE "_bikelanes_temp"
   SET geom=ST_Reverse(ST_Transform(ST_OffsetCurve(ST_Simplify(ST_Transform(geom, 25833), 0.5), "_offset", 'quad_segs=4 join=round'), 3857))
@@ -20,6 +21,7 @@ ALTER TABLE "_bikelanes_temp" DROP COLUMN "_offset";
 
 --IDEA: maybe we can transform closed geometries with some sort of buffer function:
 -- at least for the cases where we buffer "outside"(side=right) this should always yield a LineString
+-- IDEA 2: scale around center of geom (would require to estimate the scaling factor)
 
 
 -- Query below shows the geometries that would result in MultiLineString
