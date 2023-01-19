6abe378ca0b0f244eca94e0dc5ee51e777a3d7c3
Carolin B <38653703+carobro@users.noreply.github.com>
2023-01-19 10:54:20 +0100

    Buffer korrigiert

--- atlas-geo/atlas-geo/app/process/maxspeed/maxspeed.sql
+++ atlas-geo/atlas-geo/app/process/maxspeed/maxspeed.sql
@@ -1,5 +1,24 @@
+  -- SQL:
+  -- für alle linien die kein maxpseed haben (auch nicht über die source-tags)
+  --  wir nehmen die landuse=residential+industrial+commerical+retail
+  --  buffer von ~10m um die fläche
+  --  dann alle linien die (TODO) vollständig / am meisten / … in der fläche fläche sind
+  --  (tendentizell dafür nicht schneiden, weil wir am liebsten die OSM ways so haben wie in OSM)
+  --  und dann können wir in sql "maxspeed" "maxspeed_source='infereed from landuse'"
+  --  UND dann auch einen "_todo="add 'maxspeed:source=DE:urban' to way"
+  -- hinweis: außerstädtisch extrapolieren wir aber keine daten, da zu wenig "richtig"
+
 -- DELETE * FROM "maxspeed_transformed";
 INSERT INTO "maxspeed_transformed"
   SELECT   maxspeed.osm_type, maxspeed.osm_id, maxspeed.category, maxspeed.tags, maxspeed.meta, maxspeed.geom
   FROM "fromTo_landuse" as landuse, "maxspeed_todoList" as maxspeed
-  WHERE ST_Intersects(maxspeed.geom::geometry , ST_Expand(landuse.geom, 10000)::geometry);
+  WHERE ST_Intersects(maxspeed.geom::geometry , ST_Expand(landuse.geom, 10)::geometry);
+
+-- UPDATE "maxspeed_transformed" SET "_maxspeed_source" = 'infereed from landuse';
+
+update "maxspeed_transformed"  
+set  tags = jsonb_set(tags, '{_maxspeed_source}','"infereed from landuse"');
+
+update "maxspeed_transformed"  
+set  tags = jsonb_insert(tags, '{maxspeed}','"add maxspeed:source=DE:urban to way"');
+
