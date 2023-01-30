d5fc426c48992bd23eb10a5a247da665adbff680
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-01-30 18:23:41 +0100

    first draft

--- atlas-geo/atlas-geo/app/process/bikelanes/transformations.lua
+++ atlas-geo/atlas-geo/app/process/bikelanes/transformations.lua
@@ -18,45 +18,47 @@ CENTER_SIGN = 0
 RIGHT_SIGN = -1
 function GetTransformedObjects(tags, transformations)
   local sides = {
-    [":left"] = { LEFT_SIGN },
-    [":right"] = { RIGHT_SIGN },
-    [":both"] = { LEFT_SIGN, RIGHT_SIGN },
-    [""] = { LEFT_SIGN, RIGHT_SIGN },
+    [":left"] = LEFT_SIGN,
+    [":right"] = RIGHT_SIGN
   }
-  local transformedObjects = {}
+  local results = {}
   for _, transformation in pairs(transformations) do
-    for side, signs in pairs(sides) do
-      -- TODO: take :both as scelleton and overwrite with specific tags from :left or :right (maybe also from implicit both)
-      for _, sign in pairs(signs) do
-        -- this is the transformation:
-        local prefix = transformation.prefix
-        local prefixedSide = prefix .. side
-        if tags[prefixedSide] ~= nil and tags.highway ~= transformation.highway then -- check if tag exist
-          local transformedObj = unnestTags(tags, prefixedSide)
-          transformedObj.highway = transformation.highway
-          transformedObj.name = tags.name -- copy name
-          transformedObj[prefix] = tags[prefixedSide] -- self projection
-
-          -- transformation meta data (some categories rely on this e.g. implicit oneway)
-          transformedObj._projected_from = prefixedSide
-          transformedObj._projected_to = prefix
-          if not transformation.filter or transformation.filter(transformedObj) then
-            transformedObj.sign = sign
-            table.insert(transformedObjects, transformedObj)
-          end
-          if transformedObj.lanes == nil and false then
-            -- TODO: this assumes right hand traffic (would be nice to have this as an option)
-            if sign == RIGHT_SIGN then
-              transformedObj['cycleway:lanes'] = transformedObj['cycleway:lanes:forward']
-              transformedObj['bicycle:lanes'] = transformedObj['bicycle:lanes:forward']
-            elseif sign == LEFT_SIGN then
-              transformedObj['cycleway:lanes'] = transformedObj['cycleway:lanes:backward']
-              transformedObj['bicycle:lanes'] = transformedObj['bicycle:lanes:backward']
-            end
-          end
+    for side, sign in pairs(sides) do
+      local prefix = transformation.prefix
+      local newObj = {highway = transformation.highway, name = tags.name}
+      unnestTags(tags, prefix, newObj)
+      unnestTags(tags, prefix .. ":both", newObj)
+      -- this is the transformation:
+      local prefixedSide = prefix .. side
+      if tags.highway ~= transformation.highway then -- check if tag exist
+        unnestTags(tags, prefixedSide, newObj)
+        if tags[prefixedSide] ~= nil then
+          newObj[prefix] = tags[prefixedSide] -- self projection
         end
+        -- transformation meta data (some categories rely on this e.g. implicit oneway)
+        -- TODO: make this conditional on the result of unnestTags
+        newObj._projected_from = prefixedSide
+        newObj._projected_to = prefix
+
+        if not transformation.filter or transformation.filter(newObj) then
+          newObj.sign = sign
+          table.insert(results, newObj)
+        end
+
+
+        -- DRAFT FOR LANES:
+        -- if newObj.lanes == nil and false then
+        --   -- TODO: this assumes right hand traffic (would be nice to have this as an option)
+        --   if sign == RIGHT_SIGN then
+        --     transformedObj['cycleway:lanes'] = transformedObj['cycleway:lanes:forward']
+        --     transformedObj['bicycle:lanes'] = transformedObj['bicycle:lanes:forward']
+        --   elseif sign == LEFT_SIGN then
+        --     transformedObj['cycleway:lanes'] = transformedObj['cycleway:lanes:backward']
+        --     transformedObj['bicycle:lanes'] = transformedObj['bicycle:lanes:backward']
+        --   end
+        -- end
       end
     end
   end
-  return transformedObjects
+  return results
 end
