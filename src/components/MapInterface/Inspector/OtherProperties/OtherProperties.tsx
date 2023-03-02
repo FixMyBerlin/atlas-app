99d9a76b7ece9d488340852ff2b3c35a1eb095b4
Tobias <tobias@fixmycity.de>
2023-03-02 07:15:45 +0100

    Inspector: Add OSM Wiki Link
    This link will not work for some tags, but is useful for others

--- src/components/MapInterface/Inspector/OtherProperties/OtherProperties.tsx
+++ src/components/MapInterface/Inspector/OtherProperties/OtherProperties.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@components/Link'
 import React from 'react'
 
 type Props = {
@@ -23,33 +24,39 @@ export const OtherProperties: React.FC<Props> = ({
     'verified_at',
     'verified',
     'version',
+    'offset',
+    'side',
   ]
-  const otherOsmProperties = Object.fromEntries(
-    Object.entries(properties)
-      .sort((a, b) => a[0].localeCompare(b[0]))
-      .filter(([key, _v]) => !systemKeys.includes(key))
-  )
-  const systemProperties = Object.fromEntries(
-    Object.entries(properties)
-      .sort((a, b) => a[0].localeCompare(b[0]))
-      .filter(
-        ([key, _v]) =>
-          systemKeys.includes(key) && !documentedKeys?.includes(key)
-      )
-  )
+  const otherOsmProperties = Object.entries(properties)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .filter(([key, _v]) => !systemKeys.includes(key))
+
+  const systemProperties = Object.entries(properties)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .filter(
+      ([key, _v]) => systemKeys.includes(key) && !documentedKeys?.includes(key)
+    )
+
   return (
     <details className="border-t bg-white px-4 py-2.5 text-xs [&_summary]:open:mb-1 [&_summary]:open:font-semibold">
       <summary className="cursor-pointer text-right">Weitere Werte</summary>
       <div className="grid grid-cols-2 gap-4 break-all">
         <div>
           <h5 className="mb-2 font-semibold">Weitere OSM Werte:</h5>
-          {Object.keys(otherOsmProperties).length ? (
-            Object.entries(otherOsmProperties).map(([key, value]) => {
+          {otherOsmProperties.length ? (
+            otherOsmProperties.map(([key, value]) => {
               return (
                 <p key={key} className="mb-0.5 border-b border-gray-200 pb-0.5">
                   <code>
                     {key}:{' '}
-                    {typeof value === 'boolean' ? JSON.stringify(value) : value}
+                    {typeof value === 'boolean' ? JSON.stringify(value) : value}{' '}
+                    <Link
+                      blank
+                      to={`https://wiki.openstreetmap.org/wiki/Tag:${key}=${value}`}
+                      title="OpenStreetMap Wiki"
+                    >
+                      W
+                    </Link>
                   </code>
                 </p>
               )
@@ -60,8 +67,8 @@ export const OtherProperties: React.FC<Props> = ({
         </div>
         <div>
           <h5 className="mb-2 font-semibold">System-Werte:</h5>
-          {Object.keys(systemProperties).length ? (
-            Object.entries(systemProperties).map(([key, value]) => {
+          {systemProperties.length ? (
+            systemProperties.map(([key, value]) => {
               return (
                 <p key={key} className="mb-0.5 border-b border-gray-200 pb-0.5">
                   <code>
