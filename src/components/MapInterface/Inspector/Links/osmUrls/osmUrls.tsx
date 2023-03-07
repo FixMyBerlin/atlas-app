0e7e3788e3a64a10500ddf8d3160016b4e2dbc5c
Tobias <tobias@fixmycity.de>
2023-03-07 08:54:20 +0100

    Inspector Links: Extract pointFromGeometry

--- src/components/MapInterface/Inspector/Links/osmUrls/osmUrls.tsx
+++ src/components/MapInterface/Inspector/Links/osmUrls/osmUrls.tsx
@@ -1,3 +1,5 @@
+import { pointFromGeometry } from './pointFromGeometry'
+
 const osmType = { W: 'way', N: 'node', R: 'relation' }
 
 export const osmUrl = (type: 'W' | 'N' | 'R', id: number) => {
@@ -15,24 +17,8 @@ export const historyUrl = (type: 'W' | 'N' | 'R', id: number) => {
 export const mapillaryUrl = (
   geometry: maplibregl.GeoJSONFeature['geometry']
 ) => {
-  let [lat, lng] = [0, 0]
-  switch (geometry.type) {
-    case 'Point': {
-      lng = geometry.coordinates[0]
-      lat = geometry.coordinates[1]
-      break
-    }
-    case 'LineString': {
-      const middle =
-        geometry.coordinates[Math.floor(geometry.coordinates.length / 2) || 0]
-      lng = middle[0]
-      lat = middle[1]
-      break
-    }
-    default: {
-      return undefined
-      break
-    }
-  }
+  const [lng, lat] = pointFromGeometry(geometry)
+  if (!lng || !lat) return undefined
+
   return `https://www.mapillary.com/app/?lat=${lat}&lng=${lng}&z=15`
 }
