2adc934cd822201507010d85c2769a39e9403a6c
Henri <henri@fixmycity.de>
2023-03-08 09:15:23 +0100

    Use context managers for cursors

--- atlas-geo/api/main.py
+++ atlas-geo/api/main.py
@@ -13,7 +13,7 @@ import os
 app = FastAPI(
     title="RadverkehrsatlasTools",
     description="Use and interact with OSM data from FixMyCity",
-    version="0.0.1",
+    version="0.1.0",
 )
 
 origins = [
@@ -39,17 +39,16 @@ def export_region(response: Response, type_name: str, minlon: float= 13.3, minla
     if type_name not in export_geojson_function_from_type.keys():
       raise HTTPException(status_code=404, detail="export type unknown")
 
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+      # Download file directly
+      response.headers["Content-Disposition"] = 'attachment; filename="'+type_name+'.geojson"'
+      response.headers["Content-Type"] = 'application/geo+json'
 
-    # Download file directly
-    response.headers["Content-Disposition"] = 'attachment; filename="'+type_name+'.geojson"'
-    response.headers["Content-Type"] = 'application/geo+json'
+      statement = sql.SQL("SELECT * FROM {table_name} (%s, %s, %s, %s);").format(table_name=sql.Identifier(export_geojson_function_from_type[type_name]))
+      cur.execute(statement, ( minlon, minlat, maxlon, maxlat) )
+      result = cur.fetchone()[0]
 
-    statement = sql.SQL("SELECT * FROM {table_name} (%s, %s, %s, %s);").format(table_name=sql.Identifier(export_geojson_function_from_type[type_name]))
-    cur.execute(statement, ( minlon, minlat, maxlon, maxlat) )
-    result = cur.fetchone()[0]
-    cur.close()
-    return result
+      return result
 
 
 @app.get("/verify/{type_name}/{osm_id}")
@@ -57,42 +56,38 @@ def retrieve_verify_status(response: Response, type_name: str, osm_id: int):
     if type_name not in available_datasets:
       raise HTTPException(status_code=404, detail="verification type unknown")
 
-    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-
-    # Check if osm_id is available
-    statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s ORDER BY verified_at DESC LIMIT 1").format(table_name=sql.Identifier(verification_tables[type_name]))
-    cur.execute(statement, (osm_id,))
-
-    results = cur.fetchone()
-    if results == None:
-      statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s;").format(table_name=sql.Identifier(type_name))
+    with conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as cur:
+      # Check if osm_id is available
+      statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s ORDER BY verified_at DESC LIMIT 1").format(table_name=sql.Identifier(verification_tables[type_name]))
       cur.execute(statement, (osm_id,))
-      results = cur.fetchone()
-      return {
-        "osm_id": osm_id,
-        "verified": "none"
-      }
-    cur.close()
 
-    return results
+      results = cur.fetchone()
+      if results == None:
+        statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s;").format(table_name=sql.Identifier(type_name))
+        cur.execute(statement, (osm_id,))
+        results = cur.fetchone()
+        return {
+          "osm_id": osm_id,
+          "verified": "none"
+        }
+      return results
+
 
 @app.get("/verify/{type_name}/{osm_id}/history")
 def retrieve_verify_history(response: Response, type_name: str, osm_id: int):
     if type_name not in available_datasets:
       raise HTTPException(status_code=404, detail="verification type unknown")
 
-    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    with conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor) as cur:
+      # Check if osm_id is available
+      statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s ORDER BY verified_at DESC").format(table_name=sql.Identifier(verification_tables[type_name]))
+      cur.execute(statement, (osm_id,))
 
-    # Check if osm_id is available
-    statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s ORDER BY verified_at DESC").format(table_name=sql.Identifier(verification_tables[type_name]))
-    cur.execute(statement, (osm_id,))
+      results = cur.fetchall()
+      if results == None:
+        raise HTTPException(status_code=404, detail="osm_id not found")
 
-    results = cur.fetchall()
-    if results == None:
-      raise HTTPException(status_code=404, detail="osm_id not found")
-
-    cur.close()
-    return results
+      return results
 
 
 @app.post("/verify/{type_name}/{osm_id}")
@@ -103,22 +98,20 @@ def verify_osm_object(response: Response, type_name: str, osm_type: str, osm_id:
     if verified_status not in valid_verified_status:
       raise HTTPException(status_code=400, detail="verified_status property is not valid")
 
-    cur = conn.cursor()
+    with conn.cursor() as cur:
+      # Check if osm_id is available
+      statement = sql.SQL("SELECT osm_id FROM {table_name} l WHERE l.osm_id = %s").format(table_name=sql.Identifier(type_name))
+      cur.execute(statement, (osm_id,))
 
-    # Check if osm_id is available
-    statement = sql.SQL("SELECT osm_id FROM {table_name} l WHERE l.osm_id = %s").format(table_name=sql.Identifier(type_name))
-    cur.execute(statement, (osm_id,))
+      results = cur.fetchone()
+      if results == None:
+        raise HTTPException(status_code=404, detail="osm_id not found")
 
-    results = cur.fetchone()
-    if results == None:
-      raise HTTPException(status_code=404, detail="osm_id not found")
+      statement = sql.SQL("INSERT INTO {table_name} (osm_type, osm_id, verified_at, verified_by, verified) VALUES (%s, %s, %s, %s, %s)").format(table_name=sql.Identifier(verification_tables[type_name]))
+      cur.execute(statement, (osm_type, osm_id, verified_at, verified_by, verified_status ))
+      conn.commit()
 
-    statement = sql.SQL("INSERT INTO {table_name} (osm_type, osm_id, verified_at, verified_by, verified) VALUES (%s, %s, %s, %s, %s)").format(table_name=sql.Identifier(verification_tables[type_name]))
-    cur.execute(statement, (osm_type, osm_id, verified_at, verified_by, verified_status ))
-    conn.commit()
-
-    cur.close()
-    return 'OK'
+      return 'OK'
 
 
 @app.on_event("shutdown")
