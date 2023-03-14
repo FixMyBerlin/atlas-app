b7c1d0558095f5ab5d0e00e222a5211bd2d1c6e1
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-03-14 13:03:30 +0100

    add barriers to processing pipeline

--- atlas-geo/atlas-geo/app/run-4-process.sh
+++ atlas-geo/atlas-geo/app/run-4-process.sh
@@ -60,6 +60,8 @@ echo "\e[1m\e[7m PROCESS – Topic: maxspeed \e[27m\e[21m"
 ${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}maxspeed/maxspeed.lua ${OSM_FILTERED_FILE}
 # psql -q -f "${PROCESS_DIR}maxspeed/maxspeed.sql"
 
+echo "\e[1m\e[7m PROCESS – Topic: barriers \e[27m\e[21m"
+${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}barriers.lua ${OSM_FILTERED_FILE}
 
 # echo "\e[1m\e[7m PROCESS – Topic: parking \e[27m\e[21m"
 # ${OSM2PGSQL_BIN} --create --output=flex --extra-attributes --style=${PROCESS_DIR}parking.lua ${OSM_FILTERED_FILE}
