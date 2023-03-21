adaf2186711eb063433ad7fce4912ea332e83e43
Tobias <tobias@fixmycity.de>
2023-03-21 17:21:16 +0100

    Inspector: Rework Verification – Fix spacing

--- src/components/MapInterface/Inspector/Verification/VerificationHistory/VerificationHistoryEntry.tsx
+++ src/components/MapInterface/Inspector/Verification/VerificationHistory/VerificationHistoryEntry.tsx
@@ -27,7 +27,7 @@ export const VerificationHistoryEntry: React.FC<Props> = ({ history }) => {
                   <BoltIcon className="h-4 w-4" aria-hidden="true" />
                 )}
               </span>
-              <div className="w-full">
+              <div className="mb-3 w-full">
                 <div className="flex min-w-0 flex-1 justify-between space-x-4">
                   <div className="text-sm text-gray-500">{verifiedBy}</div>
                   <div className="whitespace-nowrap text-right text-sm text-gray-500">
@@ -37,7 +37,7 @@ export const VerificationHistoryEntry: React.FC<Props> = ({ history }) => {
                 {event.comment && (
                   <Markdown
                     markdown={event.comment}
-                    className="prose-sm mr-1 mt-2 mb-3 border-l border-gray-400 pl-2 prose-p:leading-tight prose-p:text-gray-500 prose-ol:pl-3 prose-ol:leading-tight prose-ul:pl-3 prose-ul:leading-tight prose-li:m-0 prose-li:p-0 prose-li:marker:text-gray-500"
+                    className="prose-sm mr-1 mt-2 border-l border-gray-400 pl-2 prose-p:leading-tight prose-p:text-gray-500 prose-ol:pl-3 prose-ol:leading-tight prose-ul:pl-3 prose-ul:leading-tight prose-li:m-0 prose-li:p-0 prose-li:marker:text-gray-500"
                   />
                 )}
               </div>
