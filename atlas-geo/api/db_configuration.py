d6be75936bce889bbf1e902eea54ac5b4ca9af72
Henri <henri@fixmycity.de>
2023-03-23 18:05:43 +0100

    Add more tables for exporting and improve simplify db_configuration

--- atlas-geo/api/db_configuration.py
+++ atlas-geo/api/db_configuration.py
@@ -1,16 +1,5 @@
 valid_verified_status = ['approved', 'rejected', 'undefined']
 
-available_datasets = [
-  "shops_tarmac",
-  "education_tarmac",
-  "publicTransport_tarmac",
-  "places",
-  "roadClassification_tarmac",
-  "landuse",
-  "bikelanes",
-  "lit"
-]
-
 export_geojson_function_from_type = {
   "bikelanes": "atlas_export_geojson_bikelanes",
   "bikelanes_verified": "atlas_export_geojson_bikelanes_verified",
@@ -22,6 +11,8 @@ export_geojson_function_from_type = {
   "places": "atlas_export_geojson_places",
   "roadClassification": "atlas_export_geojson_roadtypes",
   "landuse": "atlas_export_geojson_landuse",
+  "maxspeed": "atlas_export_geojson_maxspeed",
+  "buildings": "atlas_export_geojson_buildings",
 }
 
 verification_tables = {
