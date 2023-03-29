e41d64a5ff6860799679a3e50799730b4cb732f6
Tobias <tobias@fixmycity.de>
2023-03-29 20:58:44 +0200

    IsFresh: Add postfix feature, WIP test file

--- atlas-geo/atlas-geo/app/process/shared/IsFresh.lua
+++ atlas-geo/atlas-geo/app/process/shared/IsFresh.lua
@@ -1,3 +1,4 @@
+package.path = package.path .. ";/app/process/helper/?.lua"
 require("CheckDataWithinYears")
 
 -- * @desc Categoize the fresshnews of data.
@@ -6,7 +7,8 @@ require("CheckDataWithinYears")
 -- * @returns
 --   `dest` with freshness-tags added to it.
 --   If no parameter was provided a new object gets created.
-function IsFresh(object, date_tag, dest)
+--   `isFreshKey` a table of keys to use for FilterTags
+function IsFresh(object, date_tag, dest, postfix)
   dest = dest or {}
 
   local date = os.date('!%Y-%m-%d', object.timestamp)
@@ -20,15 +22,17 @@ function IsFresh(object, date_tag, dest)
   -- Freshness of data, see documentation
   local withinYears = CheckDataWithinYears(date, 2)
 
+  local fresh_key = table.concat({"fresh", postfix}, "_")
   if withinYears.result then
-    dest.fresh = "fresh_" .. source
+    dest[fresh_key] = "fresh_" .. source
   else
-    dest.fresh = "outdated_" .. source
+    dest[fresh_key] = "outdated_" .. source
   end
 
-  dest.fresh_age_days = withinYears.diffDays
+  local fresh_age_key = table.concat({"fresh_age_days", postfix}, "_")
+  dest[fresh_age_key] = withinYears.diffDays
 
-  return dest
+  local isFreshKeys = { fresh_key, fresh_age_key }
+
+  return dest, isFreshKeys
 end
-
-IsFreshKeys = { 'fresh', 'fresh_age_days' }
