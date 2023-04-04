187ac55cc4e37d784a59baf24f0380341e6e39ed
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-04-04 17:36:30 +0200

    bugfix: refine "opposite" projection

--- atlas-geo/app/process/bikelanes/bikelanes.lua
+++ atlas-geo/app/process/bikelanes/bikelanes.lua
@@ -94,12 +94,16 @@ function osm2pgsql.process_way(object)
   local tags = object.tags
   local meta = Metadata(object)
 
-  --legacy one way scheme
+  -- legacy one way scheme
   -- doesn't handle opposite tagging scheme on nested tags!
   if osm2pgsql.has_prefix(tags.cycleway, 'opposite') then
     if tags.oneway == 'yes' then
-      tags['oneway:bicycle'] = 'no'
-      tags.cycleway = string.sub(tags.cycleway, 10)
+      tags['oneway:bicycle'] = tags['oneway:bicycle'] or 'no'
+      local opposite_type = string.sub(tags.cycleway, 10)
+      if opposite_type ~= '' then
+        tags['cycleway:left'] = opposite_type
+      end
+      tags.cycleway = nil
     end
   end
 
