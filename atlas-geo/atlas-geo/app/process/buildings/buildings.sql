d77850a8daf934527549621828bcbcee99f3095d
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-04-18 14:38:35 +0200

    add comments

--- atlas-geo/atlas-geo/app/process/buildings/buildings.sql
+++ atlas-geo/atlas-geo/app/process/buildings/buildings.sql
@@ -1,4 +1,7 @@
 DROP TABLE buildings;
+-- cluster all intersecting buildings
 SELECT (unnest(st_clusterintersecting(geom))) AS geom INTO buildings FROM _buildings_temp;
+-- join clusters and convert to multi polygons
 UPDATE buildings SET geom = st_multi(st_unaryunion(geom));
+-- copy results into final table and set CRS (couldn't find a way to infer the CRS)
 ALTER TABLE buildings ALTER COLUMN geom TYPE geometry (MultiPolygon, 3857);
