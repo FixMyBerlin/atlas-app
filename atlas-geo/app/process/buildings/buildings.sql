fb5671769b8d91e9d4500e69f983137eafe8b379
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-04-18 16:36:49 +0200

    delete small buildings

--- atlas-geo/app/process/buildings/buildings.sql
+++ atlas-geo/app/process/buildings/buildings.sql
@@ -5,3 +5,5 @@ SELECT (unnest(st_clusterintersecting(geom))) AS geom INTO buildings FROM _build
 UPDATE buildings SET geom = st_multi(st_unaryunion(geom));
 -- copy results into final table and set CRS (couldn't find a way to infer the CRS)
 ALTER TABLE buildings ALTER COLUMN geom TYPE geometry (MultiPolygon, 3857);
+-- delete small buildings
+DELETE FROM buildings WHERE ST_Area(geom) < 20;
