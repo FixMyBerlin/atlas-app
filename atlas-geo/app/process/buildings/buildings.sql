0e506e892da1915c11081828ad7a3dd81f83cd4d
Tobias <tobias@fixmycity.de>
2023-04-25 15:01:59 +0200

    Buildings: Delete more buildings

--- atlas-geo/app/process/buildings/buildings.sql
+++ atlas-geo/app/process/buildings/buildings.sql
@@ -6,8 +6,9 @@ SELECT (unnest(st_clusterintersecting(geom))) AS geom INTO buildings FROM _build
 -- join clusters and convert to multi polygons
 UPDATE buildings SET geom = st_multi(st_unaryunion(geom));
 
--- copy results into final table and set CRS (couldn't find a way to infer the CRS)
+-- set CRS (couldn't find a way to infer the CRS)
 ALTER TABLE buildings ALTER COLUMN geom TYPE geometry (MultiPolygon, 3857);
 
--- delete small buildings
-DELETE FROM buildings WHERE ST_Area(geom) < 20;
+-- delete small buildings < 20qm
+-- TODO: figure out why "20" does not work as a value; I picked the 100 by checking what looked reasonable for http://localhost:7800/public.buildings.html#15.18/52.441297/13.557327
+DELETE FROM buildings WHERE ST_Area(geom) < 100;
