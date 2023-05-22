7a11243316621f72dfac3400d9f401f95d0278e9
Tobias <tobias@fixmycity.de>
2023-05-22 16:55:20 +0200

    Dataset Parking: Remove non needed *_public files

--- src/components/MapInterface/mapData/sourcesMapData/datasets/datasets.const.ts
+++ src/components/MapInterface/mapData/sourcesMapData/datasets/datasets.const.ts
@@ -12,10 +12,6 @@ export const datasets = {
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-zones-fisbroker.pmtiles',
   'bietigheim-bissingen_on_street_parking_lines':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines.pmtiles',
-  'bietigheim-bissingen_on_street_parking_lines_public':
-    'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines_public.pmtiles',
   'bietigheim-bissingen_parking_areas':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas.pmtiles',
-  'bietigheim-bissingen_parking_areas_public':
-    'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas_public.pmtiles',
 } as const
--- src/components/MapInterface/mapData/sourcesMapData/datasets/types.ts
+++ src/components/MapInterface/mapData/sourcesMapData/datasets/types.ts
@@ -7,9 +7,7 @@ export type DatasetIds =
   | 'TrtoWunschlinienLocationPoints'
   | 'berlin-parking-zones-fisbroker'
   | 'bietigheim-bissingen_on_street_parking_lines'
-  | 'bietigheim-bissingen_on_street_parking_lines_public'
   | 'bietigheim-bissingen_parking_areas'
-  | 'bietigheim-bissingen_parking_areas_public'
 
 export type DatasetFiles =
   | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoRadnetz.pmtiles'
@@ -18,6 +16,4 @@ export type DatasetFiles =
   | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienLocationPoints.pmtiles'
   | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-zones-fisbroker.pmtiles'
   | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines.pmtiles'
-  | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines_public.pmtiles'
   | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas.pmtiles'
-  | 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas_public.pmtiles'
