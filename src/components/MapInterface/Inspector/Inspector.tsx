62ff6727bcee74efa0ea98452738eb4851a826f8
Tobias <tobias@fixmycity.de>
2023-05-22 16:57:34 +0200

    Inspector: Fix duplicated Features

--- src/components/MapInterface/Inspector/Inspector.tsx
+++ src/components/MapInterface/Inspector/Inspector.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { extractDataIdIdFromDataKey } from '../Map/SourcesAndLayers/utils/extractFromSourceKey'
 import { sourcesDatasets } from '../mapData/sourcesMapData/sourcesDatasets.const'
 import { useMapStateInteraction } from '../mapStateInteraction/useMapStateInteraction'
+import { createInspectorFeatureKey } from '../utils'
 import { InspectorFeatureDataset } from './InspectorFeatureDataset'
 import { InspectorFeatureSource } from './InspectorFeatureSource'
 import { InspectorHeader } from './InspectorHeader'
@@ -15,13 +16,30 @@ export type InspectorFeature = {
 export const Inspector: React.FC = () => {
   const { inspectorFeatures, resetInspector } = useMapStateInteraction()
 
-  if (!inspectorFeatures.length) return null
+  // When we click on the map, MapLibre returns all Features for all layers.
+  // For hidden layers like the hitarea layer, those features are duplicates which we filter out.
+  const uniqueKeys: Record<string, boolean> = {}
+  const uniqueInspectorFeatures = inspectorFeatures.reduce(
+    (result: typeof inspectorFeatures, feature) => {
+      if (!uniqueKeys[createInspectorFeatureKey(feature)]) {
+        uniqueKeys[createInspectorFeatureKey(feature)] = true
+        result.push(feature)
+      }
+      return result
+    },
+    []
+  )
+
+  if (!uniqueInspectorFeatures.length) return null
 
   return (
     <div className="absolute top-0 right-0 bottom-0 z-10 w-[35rem] overflow-y-scroll bg-white p-5 pr-3 shadow-md">
-      <InspectorHeader count={inspectorFeatures.length} handleClose={() => resetInspector()} />
+      <InspectorHeader
+        count={uniqueInspectorFeatures.length}
+        handleClose={() => resetInspector()}
+      />
 
-      {inspectorFeatures.map((inspectObject) => {
+      {uniqueInspectorFeatures.map((inspectObject) => {
         const sourceKey = String(inspectObject.layer.source)
         if (!sourceKey) return null
 
@@ -31,7 +49,7 @@ export const Inspector: React.FC = () => {
         if (isDataset) {
           return (
             <InspectorFeatureDataset
-              key={sourceKey}
+              key={createInspectorFeatureKey(inspectObject)}
               sourceKey={sourceKey}
               properties={inspectObject.properties}
               geometry={inspectObject.geometry}
