220118d21cb3ebd95207dc7aa03b625fa658e157
Tobias <tobias@fixmycity.de>
2023-06-07 16:58:35 +0200

    Datasets: Add preview URL to generated file

--- src/components/MapInterface/mapData/sourcesMapData/datasets/datasets.const.ts
+++ src/components/MapInterface/mapData/sourcesMapData/datasets/datasets.const.ts
@@ -1,21 +1,38 @@
 // Auto-generated file, do not edit
 
 export const datasets = {
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoRadnetz.pmtiles'
   TrtoRadnetz: 'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoRadnetz.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienConnectionLines.pmtiles'
   TrtoWunschlinienConnectionLines:
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienConnectionLines.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienCrossingPoints.pmtiles'
   TrtoWunschlinienCrossingPoints:
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienCrossingPoints.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienLocationPoints.pmtiles'
   TrtoWunschlinienLocationPoints:
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/TrtoWunschlinienLocationPoints.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-polygons-euvm.pmtiles'
   'berlin-parking-polygons-euvm':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-polygons-euvm.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-zones-fisbroker.pmtiles'
   'berlin-parking-zones-fisbroker':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/berlin-parking-zones-fisbroker.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines.pmtiles'
   'bietigheim-bissingen_on_street_parking_lines':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines_label_nodes.pmtiles'
   'bietigheim-bissingen_on_street_parking_lines_label_nodes':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_on_street_parking_lines_label_nodes.pmtiles',
+
+  // Preview: https://protomaps.github.io/PMTiles/?url=https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas.pmtiles'
   'bietigheim-bissingen_parking_areas':
     'https://atlas-tiles.s3.eu-central-1.amazonaws.com/bietigheim-bissingen_parking_areas.pmtiles',
 } as const
