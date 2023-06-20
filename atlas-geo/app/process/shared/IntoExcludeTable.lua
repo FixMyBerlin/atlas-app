0511001c8e3d90ac4dda99052ac02e8e76242401
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-06-20 13:46:08 +0200

    disable exclude tables

--- atlas-geo/app/process/shared/IntoExcludeTable.lua
+++ atlas-geo/app/process/shared/IntoExcludeTable.lua
@@ -1,10 +1,10 @@
 require("Metadata")
 
 function IntoExcludeTable(table, object, reason)
-  table:insert({
-    tags = object.tags,
-    meta = Metadata(object),
-    reason = reason,
-    geom = object:as_linestring()
-  })
+  -- table:insert({
+  --   tags = object.tags,
+  --   meta = Metadata(object),
+  --   reason = reason,
+  --   geom = object:as_linestring()
+  -- })
 end
