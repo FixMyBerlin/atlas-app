c5cdc74d79031df082fb4bad1ddf54fbcf5194dd
rush42 <laurenz.rasche@gmail.com>
2023-06-27 12:24:21 +0200

    remove duplicated variables and check for file when trying to skip

--- atlas-geo/app/run-1-download.sh
+++ atlas-geo/app/run-1-download.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-OSM_DATADIR="/data/" # needs to be root (in docker)
-OSM_LOCAL_FILE=${OSM_DATADIR}openstreetmap-latest.osm.pbf
-
 # OSM_DOWNLOAD_URL=http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf
 # OSM_DOWNLOAD_FILE=berlin-latest.osm.pbf
 OSM_DOWNLOAD_URL=http://download.geofabrik.de/europe/germany-latest.osm.pbf
@@ -12,16 +9,20 @@ OSM_DOWNLOAD_FILE=germany-latest.osm.pbf
 start_time=$(date +%s)
 echo -e "\e[1m\e[7m DOWNLOAD – START \e[27m\e[21m – Start Time: $(date)\e[0m"
 
-if [ "$SKIP_DOWNLOAD" = "skip" ]; then
-  echo "💥 SKIPPED with 'SKIP_DOWNLOAD=skip' in '/docker-compose.yml'"
-else
-  echo "File: ${OSM_DOWNLOAD_URL}"
-  # Note: Showing the progress (locally) does not work, unfortunately
-  if wget --timestamping ${OSM_DOWNLOAD_URL} --directory-prefix=${OSM_DATADIR}; then
-    cp ${OSM_DATADIR}${OSM_DOWNLOAD_FILE} ${OSM_LOCAL_FILE}
+if [ $SKIP_DOWNLOAD == 1 ]; then
+  if [ -f "${OSM_LOCAL_FILE}" ]; then 
+    echo "💥 SKIPPED with 'SKIP_DOWNLOAD=1' in '/docker-compose.yml'"
+    exit 0;
   else
-    echo "Error: Failed to download the file from ${OSM_DOWNLOAD_URL}"
+    echo "Can't skip download, no file was found."
   fi
+fi
+echo "File: ${OSM_DOWNLOAD_URL}"
+# Note: Showing the progress (locally) does not work, unfortunately
+if wget --timestamping ${OSM_DOWNLOAD_URL} --directory-prefix=${OSM_DATADIR}; then
+  cp ${OSM_DATADIR}${OSM_DOWNLOAD_FILE} ${OSM_LOCAL_FILE}
+else
+  echo "Error: Failed to download the file from ${OSM_DOWNLOAD_URL}"
 fi
 
 end_time=$(date +%s)
