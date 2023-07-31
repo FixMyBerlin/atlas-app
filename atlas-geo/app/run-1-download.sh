648b88805d2c2f12f273aa35fd55098bdeebd284
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-07-31 11:16:29 +0200

    move `OSM_DOWNLOAD_URL` to env vars

--- atlas-geo/app/run-1-download.sh
+++ atlas-geo/app/run-1-download.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 
-# OSM_DOWNLOAD_URL=http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf
-# OSM_DOWNLOAD_FILE=berlin-latest.osm.pbf
-OSM_DOWNLOAD_URL=http://download.geofabrik.de/europe/germany-latest.osm.pbf
-OSM_DOWNLOAD_FILE=germany-latest.osm.pbf
-
 start_time=$(date +%s)
 echo -e "\e[1m\e[7m DOWNLOAD – START \e[27m\e[21m – Start Time: $(date)\e[0m"
 
+OSM_DOWNLOAD_FILE="${OSM_DATADIR}$(basename $OSM_DOWNLOAD_URL)"
 if [ $SKIP_DOWNLOAD == 1 ]; then
-  if [ -f "${OSM_LOCAL_FILE}" ]; then
+  if [ -f "${OSM_DOWNLOAD_FILE}" ]; then
     echo "💥 SKIPPED with 'SKIP_DOWNLOAD=1'"
     exit 0;
   else
     echo "Can't skip download, no file was found."
   fi
 fi
-echo "File: ${OSM_DOWNLOAD_URL}"
+echo "Downloading file: ${OSM_DOWNLOAD_URL}"
 # Note: Showing the progress (locally) does not work, unfortunately
 if wget --timestamping ${OSM_DOWNLOAD_URL} --directory-prefix=${OSM_DATADIR}; then
-  ln -f ${OSM_DATADIR}${OSM_DOWNLOAD_FILE} ${OSM_LOCAL_FILE}
+  ln -f ${OSM_DOWNLOAD_FILE} ${OSM_LOCAL_FILE}
 else
   echo "Error: Failed to download the file from ${OSM_DOWNLOAD_URL}"
 fi
