3244f95407bedb39d54e6de669a2f293e4c57a21
Henri <henri@fixmycity.de>
2023-08-02 12:08:28 +0200

    fix loading data on first activation

--- src/components/MapInterface/Map/SourcesAndLayers/DynamicSources.tsx
+++ src/components/MapInterface/Map/SourcesAndLayers/DynamicSources.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { FeatureCollection } from 'geojson'
 import axios from 'axios'
 import { CircleLayer, Layer, Source, useMap } from 'react-map-gl'
@@ -6,6 +6,7 @@ import { useMapStateInteraction } from '@components/MapInterface/mapStateInterac
 
 export const DynamicSources: React.FC = () => {
   const { mainMap } = useMap()
+  // const [activeRequestEventHandler, setActiveRequestEventHandler] = useState(false)
   const { mapLoaded, setOsmNotesLoaded, osmNotesActive } = useMapStateInteraction()
 
   const [geodata, setGeodata] = useState<FeatureCollection>({
@@ -13,25 +14,42 @@ export const DynamicSources: React.FC = () => {
     features: [],
   })
 
+  // Cache function, otherwise deactivating the event handler will not work
+  const osmNotesApiRequest = useCallback(() => {
+    if (!mainMap) return
+    setOsmNotesLoaded(false)
+    const b = mainMap.getBounds()
+    axios
+      .get(
+        `https://api.openstreetmap.org/api/0.6/notes?bbox=${b.getSouthWest().lng},${
+          b.getSouthWest().lat
+        },${b.getNorthEast().lng},${b.getNorthEast().lat}`
+      )
+      .then((res) => {
+        setGeodata(res.data)
+        setOsmNotesLoaded(true)
+      })
+  }, [])
+
+  // Handle event handler -> only when osm notes layer is active
   useEffect(() => {
     if (!mainMap || !mapLoaded) return
 
-    mainMap.on('moveend', () => {
-      setOsmNotesLoaded(false)
-      const b = mainMap.getBounds()
-      axios
-        .get(
-          `https://api.openstreetmap.org/api/0.6/notes?bbox=${b.getSouthWest().lng},${
-            b.getSouthWest().lat
-          },${b.getNorthEast().lng},${b.getNorthEast().lat}`
-        )
-        .then((res) => {
-          console.log(res.data)
-          setGeodata(res.data)
-          setOsmNotesLoaded(true)
-        })
-    })
-  }, [mapLoaded])
+    console.warn('osmnotes active, fetching data', osmNotesActive)
+
+    if (osmNotesActive) {
+      mainMap.on('moveend', osmNotesApiRequest)
+    }
+
+    if (!osmNotesActive) {
+      mainMap.off('moveend', osmNotesApiRequest)
+    }
+  }, [osmNotesActive])
+
+  // On first activation, fetch data without map interaction
+  useEffect(() => {
+    osmNotesApiRequest()
+  }, [])
 
   const layerProps: CircleLayer = {
     id: 'osmnoteslayer',
