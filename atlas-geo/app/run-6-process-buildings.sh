52c6cc7a4598c5f8d1d550090bf14721ef2725c4
Tobias <tobias@fixmycity.de>
2023-08-02 17:34:36 +0200

    Process/Buildings: Disable processing due to error
    The optimized version is still not good enough to run on the dataset of all Germany. We will look into this later…

--- atlas-geo/app/run-6-process-buildings.sh
+++ atlas-geo/app/run-6-process-buildings.sh
@@ -4,7 +4,8 @@ source ./process-helpers.sh
 
 echo -e "\e[1m\e[7m PROCESS BUILDINGS – START \e[27m\e[21m – Start Time: $(date)\e[0m"
 
-run_lua "buildings/buildings"
-run_psql "buildings/buildings"
+echo "DISABLED"
+# run_lua "buildings/buildings"
+# run_psql "buildings/buildings"
 
 echo -e "\e[1m\e[7m PROCESS BUILDINGS – END \e[27m\e[21m – End Time: $(date)\e[0m"
