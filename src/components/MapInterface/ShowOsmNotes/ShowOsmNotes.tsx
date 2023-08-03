011a40b860e3437c245d2fa8a3cbf1925196245a
Henri <henri@fixmycity.de>
2023-08-03 14:17:45 +0200

    Add Tooltip component

--- src/components/MapInterface/ShowOsmNotes/ShowOsmNotes.tsx
+++ src/components/MapInterface/ShowOsmNotes/ShowOsmNotes.tsx
@@ -4,6 +4,7 @@ import { ChatBubbleLeftRightIcon, PlusIcon } from '@heroicons/react/24/outline'
 import { useMap } from 'react-map-gl'
 import { SmallSpinner } from '@components/Spinner/Spinner'
 import { useMapStateInteraction } from '../mapStateInteraction'
+import Tooltip from '@components/Tooltip/Tooltip'
 
 interface Props {
   name?: string
@@ -15,43 +16,51 @@ export const ShowOsmNotes: React.FC<Props> = () => {
 
   return (
     <div className="ml-2 shadow-lg">
-      <button
-        onClick={() => setOsmNotesActive(!osmNotesActive)}
-        className={clsx(
-          'flex-0 group relative min-w-0 overflow-hidden whitespace-nowrap py-2 px-3 text-center text-sm font-medium',
-          osmNotesActive ? 'rounded-l-lg' : 'rounded-lg',
-          osmNotesActive ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-          osmNotesActive ? 'bg-yellow-400' : 'bg-white hover:bg-yellow-50 focus:z-10'
-        )}
+      <Tooltip
+        text={osmNotesActive ? 'OSM-Notes ausblenden' : 'OSM-Notes anzeigen'}
+        className="inline-flex"
       >
-        <div className="h-5 w-5">
-          {osmNotesLoaded && <ChatBubbleLeftRightIcon />}
-          {!osmNotesLoaded && <SmallSpinner />}
-        </div>
-        <span
-          aria-hidden="true"
+        <button
+          onClick={() => setOsmNotesActive(!osmNotesActive)}
           className={clsx(
-            osmNotesActive ? 'bg-yellow-500' : 'bg-transparent',
-            'absolute inset-x-0 bottom-0 h-0.5'
+            'flex-0 group relative min-w-0 overflow-hidden whitespace-nowrap py-2 px-3 text-center text-sm font-medium',
+            osmNotesActive ? 'rounded-l-lg' : 'rounded-lg',
+            osmNotesActive ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+            osmNotesActive ? 'bg-yellow-400' : 'bg-white hover:bg-yellow-50 focus:z-10'
           )}
-        />
-      </button>
-      <button
-        onClick={() => {
-          mainMap ? console.warn('BBOX', mainMap.getBounds()) : null
-        }}
-        className={clsx(
-          'focus:z-9 rounded-r-lg bg-white transition-transform hover:bg-yellow-50',
-          osmNotesActive ? 'translate-x-0' : '-translate-x-20',
-          'text-gray-500 hover:text-gray-700',
-          'flex-0 group min-w-0 overflow-hidden whitespace-nowrap py-2 px-3 text-center text-sm font-medium',
-          osmNotesActive ? 'inline-flex' : 'hidden'
-        )}
-      >
-        <div className="h-5 w-5">
-          <PlusIcon />
-        </div>
-      </button>
+        >
+          <div className="h-5 w-5">
+            {osmNotesLoaded && <ChatBubbleLeftRightIcon />}
+            {!osmNotesLoaded && <SmallSpinner />}
+          </div>
+          <span
+            aria-hidden="true"
+            className={clsx(
+              osmNotesActive ? 'bg-yellow-500' : 'bg-transparent',
+              'absolute inset-x-0 bottom-0 h-0.5'
+            )}
+          />
+        </button>
+      </Tooltip>
+      <Tooltip text="OSM-Note erstellen" className="inline-flex">
+        <button
+          onClick={() => {
+            mainMap ? console.warn('BBOX', mainMap.getBounds()) : null
+          }}
+          // title="OSM-Note erstellen"
+          className={clsx(
+            'flex-0 group min-w-0 overflow-hidden whitespace-nowrap py-2 px-3 text-center text-sm font-medium',
+            'focus:z-9 rounded-r-lg bg-white hover:bg-yellow-50',
+            osmNotesActive ? 'translate-x-0' : '-translate-x-20',
+            'text-gray-500 hover:text-gray-700',
+            osmNotesActive ? 'inline-flex' : 'hidden'
+          )}
+        >
+          <div className="h-5 w-5">
+            <PlusIcon />
+          </div>
+        </button>
+      </Tooltip>
     </div>
   )
 }
---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,33 @@
+import clsx from 'clsx'
+import React, { useEffect, useRef, useState } from 'react'
+
+interface Props {
+  text: string
+  className?: string
+  children: React.ReactNode
+}
+
+const Tooltip: React.FC<Props> = ({ text, children, className }) => {
+  const [height, setHeight] = useState(0)
+  const parentWrapperRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (parentWrapperRef.current) {
+      setHeight(parentWrapperRef.current.clientHeight)
+    }
+  })
+
+  return (
+    <div className={clsx('has-tooltip', className)} ref={parentWrapperRef}>
+      <span
+        className={`tooltip hyphens-nonte w-max cursor-default select-none break-keep rounded bg-gray-900 p-2 text-white shadow-md`}
+        style={{ top: height + 3 }}
+      >
+        {text}
+      </span>
+      {children}
+    </div>
+  )
+}
+
+export default Tooltip
