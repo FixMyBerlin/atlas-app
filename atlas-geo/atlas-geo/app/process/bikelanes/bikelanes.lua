b323c81860f8d15ac6792639ab698e6d596b654c
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-08-07 15:16:43 +0200

    reactivate old datasets

---
+++ atlas-geo/atlas-geo/app/process/bikelanes/bikelanes.lua
@@ -0,0 +1,182 @@
+package.path = package.path .. ";/app/process/helper/?.lua;/app/process/shared/?.lua;/app/process/bikelanes/?.lua"
+require("Set")
+require("FilterTags")
+require("Metadata")
+require("HighwayClasses")
+require("RoadWidth")
+require("ExcludeHighways")
+require("IsFresh")
+require("categories")
+require("transformations")
+require("JoinSets")
+require("PrintTable")
+require("IntoExcludeTable")
+require("ConvertCyclewayOppositeSchema")
+
+
+local presenceTable = osm2pgsql.define_table({
+  name = 'bikelanesPresence',
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+  columns = {
+    { column = 'tags',  type = 'jsonb' },
+    { column = 'geom',  type = 'linestring' },
+    { column = 'left',  type = 'text' },
+    { column = 'self',  type = 'text' },
+    { column = 'right', type = 'text' },
+    { column = 'meta',  type = 'jsonb' }
+  }
+})
+
+local excludeTable = osm2pgsql.define_table({
+  name = 'bikelanes_excluded',
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+  columns = {
+    { column = 'tags',   type = 'jsonb' },
+    { column = 'meta',   type = 'jsonb' },
+    { column = 'reason', type = 'text' },
+    { column = 'geom',   type = 'linestring' },
+  }
+})
+
+-- whitelist of tags we want to insert intro the DB
+local allowed_tags = Set({
+  '_parent_highway',
+  'access',
+  'bicycle_road',
+  'bicycle',
+  'conditional',
+  'cycleway',
+  'cycleway:lane', -- 'advisory', 'exclusive'
+  'dual_carriageway',
+  'foot',
+  'footway',
+  'highway',
+  'is_sidepath',
+  'name',
+  'oneway', -- we use oneway:bicycle=no (which is transformed to oneway=no) to add a notice in the UI about two way cycleways in one geometry
+  'prefix',
+  'segregated',
+  'side',
+  'smoothness',
+  'surface:colour',
+  'surface',
+  'traffic_sign',
+  'width',
+  'bicycle:lanes',
+  'cycleway:lanes',
+  'separation',
+  'separation:left',
+  'separation:right',
+  'lane', -- 'cycleway:SIDE:lane'
+})
+
+function osm2pgsql.process_way(object)
+  -- filter highway classes
+  local allowed_highways = JoinSets({ HighwayClasses, MajorRoadClasses, MinorRoadClasses, PathClasses })
+  if not object.tags.highway or not allowed_highways[object.tags.highway] then return end
+
+  local exclude, reason = ExcludeHighways(object.tags)
+  if exclude then
+    IntoExcludeTable(excludeTable, object, reason)
+    return
+  end
+
+  local tags = object.tags
+  local meta = Metadata(object)
+
+  ConvertCyclewayOppositeSchema(tags)
+
+  -- Our atlas-app inspector should be explicit about tagging that OSM considers default/implicit
+  if tags.bicycle_road == 'yes' then
+    tags.oneway = tags.oneway or 'implicit_no'
+  end
+
+  -- transformations
+  local footwayTransformation = {
+    highway = "footway",
+    prefix = "sidewalk",
+    filter = function(tags)
+      return not (tags.footway == 'no' or tags.footway == 'separate')
+    end
+  }
+  -- cycleway transformer:
+  local cyclewayTransformation = {
+    highway = "cycleway",
+    prefix = "cycleway",
+  }
+  local transformations = { cyclewayTransformation, footwayTransformation } -- order matters for presence
+
+  -- generate cycleways from center line tagging, also includes the original object with `sign = 0`
+  local cycleways = GetTransformedObjects(tags, transformations);
+  local presence = {} -- table holding the presence (per way object)
+  local width = RoadWidth(tags)
+  for _, cycleway in pairs(cycleways) do
+    local sign = cycleway.sign
+    local onlyPresent = OnlyPresent(cycleway) -- these are categories defining the presence of data
+    if onlyPresent ~= nil then
+      presence[sign] = presence[sign] or onlyPresent
+    else
+      local category = CategorizeBikelane(cycleway)
+      if category ~= nil then
+        FilterTags(cycleway, allowed_tags)
+
+        local freshTag = "check_date"
+        if cycleway.prefix then
+          freshTag = "check_date:" .. cycleway.prefix
+        end
+
+        -- Our atlas-app inspector should be explicit about tagging that OSM considers default/implicit
+        cycleway.oneway = cycleway.oneway or 'implicit_yes'
+
+        presence[sign] = presence[sign] or category
+      end
+    end
+  end
+
+  -- Below is presence logic
+  -- Filter ways where we dont expect bicycle infrastructure
+  -- TODO: filter on surface and traffic zone and maxspeed (maybe wait for maxspeed PR)
+  if (MinorRoadClasses[tags.highway] and tags.highway ~= 'service') or presence[CENTER_SIGN] then
+    -- set the nil values to 'not_expected', for all minor roads and complete data
+    for _, side in pairs(SIDES) do presence[side] = presence[side] or NOT_EXPECTED end
+  elseif not (presence[CENTER_SIGN] or presence[RIGHT_SIGN] or presence[LEFT_SIGN]) then
+    if not MajorRoadClasses[tags.highway] then
+      IntoExcludeTable(excludeTable, object, "no infrastructure expected for highway type: " .. tags.highway)
+      return
+    elseif tags.motorroad or tags.expressway then
+      IntoExcludeTable(excludeTable, object, "no infrastructure expected for motorroad and express way")
+      return
+      -- elseif tags.maxspeed and tags.maxspeed <= 20 then
+      --   intoExcludeTable(object, "no infrastructure expected for max speed <= 20 kmh")
+      --   return
+    end
+  elseif (presence[RIGHT_SIGN] or presence[LEFT_SIGN]) then
+    presence[CENTER_SIGN] = presence[CENTER_SIGN] or NOT_EXPECTED
+  end
+  if tags.oneway == 'yes' and tags['oneway:bicycle'] ~= 'no' then
+    presence[LEFT_SIGN] = presence[LEFT_SIGN] or NOT_EXPECTED
+  end
+
+  -- replace all nil values with 'missing'
+  for _, side in pairs(SIDES) do presence[side] = presence[side] or "missing" end
+
+  local allowed_tags_presence = Set({
+    'left',
+    'right',
+    'self',
+    'name',
+    'highway',
+    'oneway',
+    'dual_carriageway',
+  })
+  FilterTags(tags, allowed_tags_presence)
+
+  presenceTable:insert({
+    tags = tags,
+    geom = object:as_linestring(),
+    left = presence[LEFT_SIGN],
+    self = presence[CENTER_SIGN],
+    right = presence[RIGHT_SIGN],
+    meta = meta,
+  })
+end
---
+++ atlas-geo/atlas-geo/app/process/lit/lit.lua
@@ -0,0 +1,108 @@
+package.path = package.path .. ";/app/process/helper/?.lua;/app/process/shared/?.lua"
+require("Set")
+require("FilterTags")
+require("MergeArray")
+require("JoinSets")
+require("Metadata")
+require("HighwayClasses")
+require("ExcludeHighways")
+require("ExcludeByWidth")
+require("IsFresh")
+
+local table = osm2pgsql.define_table({
+  name = '_lit_temp',
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+  columns = {
+    { column = 'category', type = 'text' },
+    { column = 'tags',     type = 'jsonb' },
+    { column = 'meta',     type = 'jsonb' },
+    { column = 'geom',     type = 'linestring' },
+  }
+})
+
+-- Notes
+-- =====
+-- Which highways do we look at?
+--  We should include sidewalks.
+--  But ideally, we would exclude ways that are not network-relevant like cemetary footways
+
+-- We ignore street lamps:
+--  https://wiki.openstreetmap.org/wiki/Tag:highway%3Dstreet_lamp is not relevant for this data set
+
+-- About nodes and relations:
+--  process_node: We only look at highways
+--  process_relation: We might need to add relations later; but for now ways should be enought
+
+-- TODO: Process sidewalk:*, cycleway:* data othe centerline
+--  Right now, we only look at the primary data.
+--  We need to improve this, to copy the ways for sidewalks and cycleways that are not yet mapped separately. And apply the data on those ways as well.
+--  Could we do this, by adding a _processing_instructions="move left".
+--  We add those ways twice to the data … and post-process the in SQL?
+
+function osm2pgsql.process_way(object)
+  if not object.tags.highway then return end
+
+  local allowed_highways = JoinSets({ HighwayClasses, MajorRoadClasses, MinorRoadClasses, PathClasses })
+  -- values that we would allow, but skip here:
+  -- "construction", "planned", "proposed", "platform" (Haltestellen)
+  if not allowed_highways[object.tags.highway] then return end
+  if ExcludeHighways(object.tags) or ExcludeByWidth(object.tags, 2.1) then
+    return
+  end
+
+  -- https://wiki.openstreetmap.org/wiki/Key:lit
+
+  -- Categorize the data in three groups: "lit", "unlit", "special"
+  local category = nil
+  if object.tags.lit == nil then
+    object.tags.is_present = false
+  else
+    object.tags.is_present = true
+    category = "special"
+    if (object.tags.lit == "yes") then
+      category = "lit"
+    end
+    if (object.tags.lit == "no") then
+      category = "unlit"
+    end
+  end
+
+  -- Normalize name info for sidepath'
+  -- TODO: Extact into helper
+  object.tags.name = object.tags.name or object.tags['is_sidepath:of:name']
+
+  local allowed_tags = Set({
+    "access",
+    "area",
+    "category",
+    "check_date:lit",
+    "footway",
+    "highway",
+    "is_present",
+    "is_sidepath",
+    "lit",
+    "surface",
+    "smoothness",
+    "name",
+    "service",
+    "width",          -- experimental
+    "sidewalk:width", -- experimental
+    "cycleway:width", -- experimental
+  })
+  FilterTags(object.tags, allowed_tags)
+
+  -- Freshness of data (AFTER `FilterTags`!)
+  if (object.tags.is_present == true) then
+    IsFresh(object, 'check_date:lit', object.tags)
+  end
+
+  if object.tags._exclude then
+  else
+    table:insert({
+      category = category,
+      tags = object.tags,
+      meta = Metadata(object),
+      geom = object:as_linestring()
+    })
+  end
+end
