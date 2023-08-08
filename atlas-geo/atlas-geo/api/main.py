e3ba15cf8a84ebff528187efe057df5117b7b825
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-08-08 16:06:30 +0200

    add api endpoint to export by region

--- atlas-geo/atlas-geo/api/main.py
+++ atlas-geo/atlas-geo/api/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
 conn = psycopg2.connect(conn_string)
 
 @app.get("/export/{type_name}")
-def export_region(response: Response, type_name: str, minlon: float= 13.3, minlat : float=52.2, maxlon: float=13.7, maxlat: float=52.3):
+def export_bbox(response: Response, type_name: str, minlon: float= 13.3, minlat : float=52.2, maxlon: float=13.7, maxlat: float=52.3):
     if type_name not in export_geojson_function_from_type:
       raise HTTPException(status_code=404, detail="export type unknown")
 
@@ -49,8 +49,24 @@ def export_region(response: Response, type_name: str, minlon: float= 13.3, minla
       response.headers["Content-Disposition"] = 'attachment; filename="'+type_name+'.geojson"'
       response.headers["Content-Type"] = 'application/geo+json'
 
-      statement = sql.SQL("SELECT * FROM {table_name} (%s, %s, %s, %s);").format(table_name=sql.Identifier(export_geojson_function_from_type[type_name]))
+      statement = sql.SQL("SELECT * FROM {table_name} (( SELECT * FROM ST_MakeEnvelope(%s, %s, %s, %s)));").format(table_name=sql.Identifier(export_geojson_function_from_type[type_name]))
       cur.execute(statement, ( minlon, minlat, maxlon, maxlat) )
+      result = cur.fetchone()[0]
+
+      return result
+
+@app.get("/export/{type_name}/{osm_id}")
+def export_region(response: Response, type_name: str, osm_id: int):
+    if type_name not in export_geojson_function_from_type:
+      raise HTTPException(status_code=404, detail="export type unknown")
+
+    with conn.cursor() as cur:
+      # Download file directly
+      response.headers["Content-Disposition"] = 'attachment; filename="'+type_name+'.geojson"'
+      response.headers["Content-Type"] = 'application/geo+json'
+
+      statement = sql.SQL("SELECT * FROM {table_name} (( SELECT ST_Transform(geom, 4326) FROM boundaries WHERE osm_id=%s));").format(table_name=sql.Identifier(export_geojson_function_from_type[type_name]))
+      cur.execute(statement, (osm_id,) )
       result = cur.fetchone()[0]
 
       return result
