33273277362a3cf3b194fc3922f51f0e0a202b1b
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-08-14 11:00:33 +0200

    refactor smoothnessDirect

--- atlas-geo/atlas-geo/app/process/roads/surfaceQuality/SmoothnessDirect.lua
+++ atlas-geo/atlas-geo/app/process/roads/surfaceQuality/SmoothnessDirect.lua
@@ -1,27 +1,28 @@
+require("Set")
 function SmoothnessDirect(smoothness)
   if smoothness ~= nil then
-    local source = "tag"
-    local confidence = "high"
-
+    local smoothnessDirect = Set({
+      "excellent",
+      "good",
+      "intermediate",
+      "bad",
+      "very_bad"
+    })
     -- We only support some smoothness values to make things easier for the user
     local smoothnessNormalization = {
-      ["excellent"] = "excellent",
       ["very_good"] = "excellent",
-      ["good"] = "good",
-      ["intermediate"] = "intermediate",
-      ["bad"] = "bad",
-      ["very_bad"] = "very_bad",
       ["impassable"] = "very_bad",
       ["horrible"] = "very_bad",
       ["very_horrible"] = "very_bad",
     }
-    local normalized = smoothnessNormalization[smoothness]
-
-    if normalized ~= smoothness then
-      source = "tag_normalized"
+    if smoothnessDirect[smoothness] then
+      return smoothness, "tag", "high"
+    else
+      local normalized = smoothnessNormalization[smoothness]
+      if normalized then
+        return normalized, "tag_normalized", "high"
+      end
     end
-    return normalized, source, confidence
   end
-
   return nil, nil, nil
 end
