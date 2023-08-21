f2476c2d78ad4b2db7b65677ceff955f2fcf8389
Henri <henri@fixmycity.de>
2023-08-21 17:22:44 +0200

    Reverse changes and use PGPORT at 5432

--- atlas-geo/api/db.py
+++ atlas-geo/api/db.py
@@ -4,6 +4,6 @@ database_server = os.environ.get('PGHOST', "localhost")
 database_name = os.environ.get('PGDATABASE', "postgres")
 database_user = os.environ.get('PGUSER', "postgres")
 database_pw = os.environ.get('PGPASSWORD', "mysecretpassword")
-database_port = os.environ.get('DATABASEPORT', "5432")
+database_port = os.environ.get('PGPORT', "5432")
 
 conn_string = "host=%s dbname=%s user=%s password=%s port=%s" %(database_server, database_name, database_user, database_pw, database_port)
