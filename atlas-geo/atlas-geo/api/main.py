d1a032e5dd8d9ea12fdf69b00b103ec78f740cbc
rush42 <laurenz.rasche@gmail.com>
2023-08-28 14:34:59 +0200

    raise exception on invalid oms_id in verify endpoint

--- atlas-geo/atlas-geo/api/main.py
+++ atlas-geo/atlas-geo/api/main.py
@@ -120,6 +120,8 @@ async def retrieve_verify_status(response: Response, type_name: str, osm_id: int
           statement = sql.SQL("SELECT * FROM {table_name} WHERE osm_id = %s;").format(table_name=sql.Identifier(type_name))
           await cur.execute(statement, (osm_id,))
           results = await cur.fetchone()
+          if results == None:
+            raise HTTPException(status_code=404, detail="osm_id not found")
           return {
             "osm_id": osm_id,
             "verified": "none"
