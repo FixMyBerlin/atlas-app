803934fb318af25f24e7611a4f9de5a595281920
Tobias <tobias@fixmycity.de>
2023-09-07 10:13:13 +0200

    Fix tests for extractTopicIdFromSourceKey

--- src/components/MapInterface/Map/SourcesAndLayers/utils/extractFromSourceKey/extractFromKey.test.ts
+++ src/components/MapInterface/Map/SourcesAndLayers/utils/extractFromSourceKey/extractFromKey.test.ts
@@ -4,7 +4,7 @@ import { extractTopicIdFromSourceKey } from './extractFromKey'
 
 describe('extractTopicIdFromSourceKey()', () => {
   test('work when used with the right input', () => {
-    const input = createSourceKey('fooTheme', 'fooTopic', 'fooLayer')
+    const input = createSourceKey('fooTheme', 'fooSource', 'fooTopic')
     const result = extractTopicIdFromSourceKey(input)
     expect(result).toMatch('fooTopic')
   })
