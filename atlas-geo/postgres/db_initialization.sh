f7d5dbf844270d34444c4b17506c62932df9771b
rush42 <laurenz.rasche@gmail.com>
2023-09-19 18:35:13 +0200

    update databse search path to include `geo` schema

--- atlas-geo/postgres/db_initialization.sh
+++ atlas-geo/postgres/db_initialization.sh
@@ -5,6 +5,7 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 # Create schema 'geo'
-"${psql[@]}" <<- 'EOSQL'
+"${psql[@]}" <<- EOSQL
     CREATE SCHEMA geo;
+    ALTER DATABASE $POSTGRES_DB SET search_path = "\$user",public,topology,tiger,postgis,geo;
 EOSQL
