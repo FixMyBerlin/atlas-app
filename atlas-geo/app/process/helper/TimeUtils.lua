56296b43757ec1b1896480522c4b6ae973daad4d
rush42 <laurenz.rasche@gmail.com>
2023-09-22 17:09:09 +0200

    floor AgeInDays

--- atlas-geo/app/process/helper/TimeUtils.lua
+++ atlas-geo/app/process/helper/TimeUtils.lua
@@ -1,7 +1,7 @@
 -- return the number of days that passed since a given timestamp in unix format
 function AgeInDays(timestamp) 
     local diff = os.clock() - timestamp
-    return diff / (3600 * 24)
+    return math.floor(diff / (3600 * 24))
 end
 
 -- given a date in YYYY-MM-DD return the respective unix timestamp
