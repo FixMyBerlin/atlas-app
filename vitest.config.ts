f9e735377f354a6d66e40ef96c272573f04be054
Tobias <tobias@fixmycity.de>
2023-09-25 13:35:52 +0200

    Blitz: Merge vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite'
+import { loadEnvConfig } from "@next/env"
+import react from "@vitejs/plugin-react"
 import path from 'path'
+import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components/'),
@@ -16,8 +22,11 @@ export default defineConfig({
   },
   test: {
     // https://vitest.dev/config/#globals
-    // globals: true,
-    // https://vitest.dev/config/#environment
-    // environment: 'happy-dom',
+    dir: "./",
+    globals: true,
+    setupFiles: "./test/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
   },
 })
