36877f0193dcf83639a1d653f343b787c78337e4
Tobias <tobias@fixmycity.de>
2023-09-25 13:42:47 +0200

    Sentry: Install with NextJS wizard, remove previous integration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-// @ts-check
 const { withBlitz } = require("@blitzjs/next")
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
@@ -7,3 +7,35 @@ const { withBlitz } = require("@blitzjs/next")
 const config = {}
 
 module.exports = withBlitz(config)
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "fixmycity-gmbh",
+    project: "radverkehrsatlas",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as Sentry from '@sentry/react'
 import App from './App'
 import './index.css'
 import { isDev } from '@components/utils'
 
 const queryClient = new QueryClient()
-
-Sentry.init({
-  dsn: 'https://6c4fd261ed854a0e93cf3df248b94c08@o1174824.ingest.sentry.io/4504378162937856',
-  // Disable on development
-  enabled: !isDev,
-  // https://docs.sentry.io/platforms/javascript/guides/react/configuration/environments/
-  environment: import.meta.env.VITE_NETLIFY_CONTEXT,
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
