13ff791541636538c97ad1bd8acbadd6f748dfef
Tobias <tobias@fixmycity.de>
2023-09-26 08:35:50 +0200

    Migration: Update <Link/> to use NextLink

--- src/core/components/links/Link.tsx
+++ src/core/components/links/Link.tsx
@@ -1,6 +1,6 @@
 import { clsx } from 'clsx'
-import React from 'react'
-import { Link as ReactLocationLink } from '@tanstack/react-location'
+import React, { forwardRef } from 'react'
+import NextLink from 'next/link'
 
 type Props = {
   /** @desc Internal Link, external Link, e-mail-address (will add the `mailto:` automatically) */
@@ -21,51 +21,70 @@ export const linkStyles =
 export const buttonStyles =
   'inline-flex items-center px-4 py-2 border border-transparent font-semibold rounded-md shadow-sm text-gray-800 bg-yellow-50 hover:bg-yellow-400 group-hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-50 select-none'
 
-export const Link: React.FC<Props> = ({
-  to,
-  classNameOverwrite,
-  className,
-  blank = false,
-  external = false,
-  button = false,
-  mailSubject,
-  mailBody,
-  children,
-  ...props
-}) => {
-  const classes = clsx(className, classNameOverwrite || (button ? buttonStyles : linkStyles))
+export const Link: React.FC<Props> = forwardRef<HTMLAnchorElement, Props>(
+  (
+    {
+      to,
+      classNameOverwrite,
+      className,
+      blank = false,
+      external = false,
+      button = false,
+      mailSubject,
+      mailBody,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    const classes = clsx(className, classNameOverwrite || (button ? buttonStyles : linkStyles))
 
-  let mailto: string | undefined = undefined
-  if (to.includes('@') && !to.startsWith('http')) {
-    const mail = `mailto:${to}`
-    const params = [
-      mailSubject && `subject=${encodeURIComponent(mailSubject)}`,
-      mailBody && `body=${encodeURIComponent(mailBody)}`,
-    ].filter(Boolean)
-    mailto = `${mail}${params && `?${params.join('&')}`}`
-  }
+    let mailto: string | undefined = undefined
+    if (to.includes('@') && !to.startsWith('http')) {
+      const mail = `mailto:${to}`
+      const params = [
+        mailSubject && `subject=${encodeURIComponent(mailSubject)}`,
+        mailBody && `body=${encodeURIComponent(mailBody)}`,
+      ].filter(Boolean)
+      mailto = `${mail}${params && `?${params.join('&')}`}`
+    }
 
-  type NewWindowProps = {
-    target?: string
-    rel?: string
-  }
+    type NewWindowProps = {
+      target?: string
+      rel?: string
+    }
 
-  const newWindowProps: NewWindowProps = {
-    target: blank ? '_blank' : undefined,
-    rel: external ? 'noopener noreferrer' : undefined,
-  }
+    const newWindowProps: NewWindowProps = {
+      target: blank ? '_blank' : undefined,
+      rel: external ? 'noopener noreferrer' : undefined,
+    }
 
-  if (external || blank || mailto) {
+    // external link
+    if (typeof to === 'string') {
+      return (
+        <a
+          ref={ref}
+          href={to}
+          className={classes}
+          rel="noopener noreferrer"
+          {...{ target: blank ? '_blank' : undefined }}
+          {...props}
+        >
+          {children}
+        </a>
+      )
+    }
+
     return (
-      <a href={mailto || to} className={classes} {...newWindowProps} {...props}>
+      <NextLink
+        href={to}
+        ref={ref}
+        className={classes}
+        {...props}
+        {...{ target: blank ? '_blank' : undefined }}
+      >
         {children}
-      </a>
+      </NextLink>
     )
-  }
-
-  return (
-    <ReactLocationLink to={to} {...props} className={classes}>
-      {children}
-    </ReactLocationLink>
-  )
-}
+  },
+)
