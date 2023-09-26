8d7e886acffd730c372ddc8112c5bad5df9aea3b
Tobias <tobias@fixmycity.de>
2023-09-26 06:43:48 +0200

    Regions: Add seeds

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,15 +1,10 @@
-// import db from "./index"
+import seedRegions from './seeds/regions'
 
 /*
  * This seed function is executed when you run `blitz db seed`.
- *
- * Probably you want to use a library like https://chancejs.com
- * to easily generate realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  await seedRegions()
 }
 
 export default seed
