58fd859302b2be6f79e6e562beb9d26cb2cc71ca
Tobias <tobias@fixmycity.de>
2023-09-26 08:36:07 +0200

    Migration: Update `isEnv` to use `process.env.APP_ORIGIN`

--- src/core/utils/isEnv.ts
+++ src/core/utils/isEnv.ts
@@ -1,17 +1,11 @@
-// SSR-Note: Once we re-add some kind of SSR, we need to check for `typeof window !== 'undefined'` again.
-//
-// `import.meta.env*` is from vite.
-// https://vitejs.dev/guide/env-and-mode.html#env-variables
-//
-// The process.env-parts did not work out in our current setup (process.env.CONTEXT, process.env.NODE_ENV).
-// Something to do with how vite works.
+export const isProd =
+  process.env.NODE_ENV === 'production' && process.env.APP_ORIGIN === 'http://radverkehrsatlas.de'
 
-export const isProd = import.meta.env.PROD && import.meta.env.VITE_NETLIFY_CONTEXT === 'production'
+export const isStaging =
+  process.env.NODE_ENV === 'production' &&
+  process.env.APP_ORIGIN === 'http://staging.radverkehrsatlas.de'
 
-export const isStaging = import.meta.env.PROD && import.meta.env.VITE_NETLIFY_CONTEXT === 'staging'
-
-// This uses the fallback to .env.DEV in case the .env.local file is not set up.
-export const isDev = import.meta.env.DEV || import.meta.env.VITE_NETLIFY_CONTEXT === 'development'
+export const isDev = process.env.NODE_ENV === 'development'
 
 export const isBrowser = typeof window !== 'undefined'
 
