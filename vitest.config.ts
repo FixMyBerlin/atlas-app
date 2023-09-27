9a38b7fc8cc73bd44f35f778c699a35d6eb388c2
Tobias <tobias@fixmycity.de>
2023-09-27 13:53:54 +0200

    Cleanup v1 import paths from tsconfig, vitestconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { loadEnvConfig } from "@next/env"
-import react from "@vitejs/plugin-react"
+import { loadEnvConfig } from '@next/env'
+import react from '@vitejs/plugin-react'
 import path from 'path'
-import tsconfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vitest/config"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 const projectDir = process.cwd()
 loadEnvConfig(projectDir)
@@ -14,19 +14,15 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, './src/components/'),
       '@pages': path.resolve(__dirname, './src/pages/'),
-      '@routes': path.resolve(__dirname, './src/routes/'),
-      '@assets': path.resolve(__dirname, './src/assets/'),
-      '@fakeServer': path.resolve(__dirname, './src/fakeServer/'),
-      '@api': path.resolve(__dirname, './src/api/'),
     },
   },
   test: {
     // https://vitest.dev/config/#globals
-    dir: "./",
+    dir: './',
     globals: true,
-    setupFiles: "./test/setup.ts",
+    setupFiles: './test/setup.ts',
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ['text', 'json', 'html'],
     },
   },
 })
