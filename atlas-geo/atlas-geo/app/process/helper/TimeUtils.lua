417f04c6771ffd908643ee62b4b94be5a0bf6be7
Tobias <tobias@fixmycity.de>
2023-09-27 13:39:59 +0200

    Fix new time utils for invalid inputs, add test
    Ping https://github.com/FixMyBerlin/private-issues/issues/1039
    
    Right now this fails with …
    
    ```
    app  | 2023-09-27 12:31:29  ERROR: Failed to execute Lua function 'osm2pgsql.process_way': /app/process/helper/TimeUtils.lua:12: field 'day' missing in date table
    app  | stack traceback:
    app  | 	[C]: in function 'os.time'
    app  | 	/app/process/helper/TimeUtils.lua:12: in function 'ParseDate'
    app  | 	...rocess/roads_bikelanes/surfaceQuality/SurfaceQuality.lua:44: in function 'SurfaceQuality'
    app  | 	./process/roads_bikelanes/roads_bikelanes.lua:81: in function <./process/roads_bikelanes/roads_bikelanes.lua:45>.
    ```

--- atlas-geo/atlas-geo/app/process/helper/TimeUtils.lua
+++ atlas-geo/atlas-geo/app/process/helper/TimeUtils.lua
@@ -1,13 +1,27 @@
 -- return the number of days that passed since a given timestamp in unix format
 -- currently everything >730 is considered outdated
-function AgeInDays(timestamp) 
-    local diff = os.time() - timestamp
-    return math.floor(diff / (3600 * 24))
+function AgeInDays(timestamp)
+  -- Return nil for all that is not a number
+  if type(timestamp) ~= "number" or tonumber(timestamp) == nil then
+    return nil
+  end
+
+  local diff = os.time() - timestamp
+  return math.floor(diff / (3600 * 24))
 end
 
 -- given a date in YYYY-MM-DD return the respective unix timestamp
 function ParseDate(date)
-    local format = "(%d+)-(%d+)-(%d+)"
-    local year, month, day = date:match(format)
-    return os.time({year = year, month = month, day = day})
+  local format = "(%d+)-(%d+)-(%d+)"
+  local year, month, day = date:match(format)
+
+  local success, result = pcall(function()
+    return os.time({ year = year, month = month, day = day })
+  end)
+
+  if success then
+    return result
+  else
+    return nil
+  end
 end
