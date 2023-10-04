c0252fb7f86c9e6af3cb53d552752ece619d1ece
Tobias <tobias@fixmycity.de>
2023-10-04 12:06:23 +0200

    Layout/Metatags: Refactor noindex logic; fix social sharing fallback

--- src/core/layouts/MetaTags/MetaTags.tsx
+++ src/core/layouts/MetaTags/MetaTags.tsx
@@ -2,6 +2,7 @@ import { isProd } from 'src/core/utils'
 import React from 'react'
 import SocialSharingImage from './assets/default.png'
 import Head from 'next/head'
+import { StaticImageData } from 'next/image'
 
 const seoDefaultValues = {
   defaultTitle: 'Radverkehrsatlas',
@@ -13,7 +14,7 @@ type Props = {
   noindex?: boolean
   title?: string | null
   description?: string | null
-  image?: string | null
+  image?: StaticImageData | null
   article?: boolean | null
 }
 
@@ -24,18 +25,9 @@ export const MetaTags: React.FC<Props> = ({ noindex, title, description, image, 
   noindex = isProd ? noindex ?? false : true
 
   const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${baseUrl}${image || SocialSharingImage}`,
-  }
-
-  if (noindex === true) {
-    return (
-      <Head>
-        <title>{seo.title}</title>
-        <meta name="robots" content="noindex" />
-      </Head>
-    )
+    title: title ?? defaultTitle,
+    description: description ?? defaultDescription,
+    image: `${baseUrl}${image?.src ?? SocialSharingImage.src}`,
   }
 
   // FYI, we do not inlcude the url meta tags since is work to handle edge cases but let the browser handle this.
@@ -43,6 +35,10 @@ export const MetaTags: React.FC<Props> = ({ noindex, title, description, image, 
   return (
     <Head>
       <title>{seo.title}</title>
+
+      {/* Even when `noindex`, still render the meta tags so Social Sharing still looks nice */}
+      {noindex === true && <meta name="robots" content="noindex" />}
+
       <meta property="og:title" content={seo.title} />
       <meta name="twitter:title" content={seo.title} />
 
@@ -57,7 +53,7 @@ export const MetaTags: React.FC<Props> = ({ noindex, title, description, image, 
       {(article ? true : null) && <meta property="og:type" content="article" />}
       <meta name="twitter:card" content="summary_large_image" />
 
-      <meta name="theme-color" content="#facc15" />
+      <meta name="theme-color" content="#eab308" />
     </Head>
   )
 }
