c703d7d1b4ea06ef05e1b7cc83ac6254552ed72d
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-10-04 11:10:21 +0200

    Derive smoothness from `mtb:scale` and `tracktype` (FixMyBerlin/atlas-geo#59)
    * derive smoothness from `mtb:scale` and `tracktype`
    
    * remove 'mtb:scale` and `tracktype=grade5`from exclude highways
    
    * derive smoothness: Add docs, update derived values
    
    ---------
    
    Co-authored-by: Tobias <tobias@fixmycity.de>,

--- atlas-geo/app/process/roads_bikelanes/surfaceQuality/DeriveSmoothness.lua
+++ atlas-geo/app/process/roads_bikelanes/surfaceQuality/DeriveSmoothness.lua
@@ -99,3 +99,37 @@ function SmoothnessFromSurface(surface)
   end
   return smoothness, source, confidence, todo
 end
+
+-- https://wiki.openstreetmap.org/wiki/Key:mtb:scale
+-- https://taginfo.openstreetmap.org/keys/mtb%3Ascale#values
+function SmoothnessFromMTBScale(scale)
+  if not scale then
+    return nil, nil, nil, "No MTB scale was given"
+  end
+  if Set({"0", "0+", "0-"})[scale] then
+    return "bad", "MTB scale to smoothness", "medium", nil
+  end 
+  return "very_bad", "MTB scale to smoothness", "medium", nil
+end
+
+-- Wiki https://wiki.openstreetmap.org/wiki/Key:tracktype
+-- Categories that cyclosm.org use https://github.com/cyclosm/cyclosm-cartocss-style/wiki/Tag-to-Render
+-- Mapping of Smoothness<>Surface https://wiki.openstreetmap.org/wiki/Key:smoothness/Gallery
+-- Mapping of Smoothness<>Surface (Legacy) https://wiki.openstreetmap.org/wiki/Berlin/Verkehrswende/smoothness
+function SmoothnessFromTrackType(type)
+  if not type then
+    return nil, nil, nil, "No track type was given"
+  end
+  local trackTypeToSmoothness = {
+    ["grade1"] = "good",
+    ["grade2"] = "intermediate",
+    ["grade3"] = "bad",
+    ["grade4"] = "bad",
+    ["grade5"] = "very_bad"
+  }
+  local smoothness = trackTypeToSmoothness[type]
+  if smoothness then
+    return smoothness, "track type to smoothness", "medium", nil
+  end
+  return nil, nil, nil, nil
+end
