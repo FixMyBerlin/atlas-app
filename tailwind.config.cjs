54691b96d417ce8a73654f76dc08c11daf2f2c10
Tobias <tobias@fixmycity.de>
2023-10-09 16:16:44 +0200

    Remove tailwindcss/line-clamp which is included in this TW version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,9 +31,5 @@ module.exports = {
     //   sans: ['', 'Segoe UI', 'Tahoma', 'sans-serif'],
     // },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
