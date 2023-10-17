d0315ac92d0081a348ade03d4f46db9ae0da600a
rush42 <laurenz.rasche@gmail.com>
2023-10-17 15:02:02 +0200

    bug-fix: us enum `value` and `name` for type guarding

--- atlas-geo/api/db_configuration.py
+++ atlas-geo/api/db_configuration.py
@@ -1,31 +1,4 @@
-# Create export functions for our API.
-# Format: { "<DB Table Name>": "public.<DB Function Name>" }
-# init_db.py: Create a function per entry by calling INIT_FUNCTIONS.sql
-# main.py: Provide the /export/<DB Table Name> entpoint
-export_tables = [
-  "barrierAreas",
-  "barrierLines",
-  "bikelanes_verified",
-  "bikelanes",
-  "boundaries",
-  "roads",
-  "landuse",
-  "places",
-  "poiClassification",
-  "publicTransport",
-]
-
-# def export_function(table_name: str):
-#   return f'atlas_export_geojson_{table_name.lower()}'
-
-# main.py: Used as an allow list to guard the /verify/* API
-# `atlas-app` only uses approved, rejected for now.
-valid_verified_status = ['approved', 'rejected', 'undefined']
-
-
-# The list of DB Tables Names that we support for verification.
-# For each table `a` in the list we create a verification table `a_verification` and a view which joins both
-verification_tables = ["bikelanes"]
+from enum import Enum
 
 # helper to retrieve the name of the verification table
 def verification_table(table_name: str):
@@ -36,3 +9,26 @@ def verified_table(table_name: str):
 
 def export_function(table_name: str):
     return f'atlas_export_geojson_{table_name.lower()}'
+
+# Te list of DB Tables which support exports.
+ExportTable = Enum('Export Table', [(export_function(name), name) for name in [
+  "barrierAreas",
+  "barrierLines",
+  "bikelanes_verified",
+  "bikelanes",
+  "boundaries",
+  "roads",
+  "landuse",
+  "places",
+  "poiClassification",
+  "publicTransport",
+]])
+
+# The list of DB Tables that support verification.
+# For each table `a` in the list we create a verification table `a_verification` and a view which joins both
+VerificationTable = Enum('Verication Table', [(verification_table(name), name) for name in ["bikelanes"]])
+
+# main.py: Used as an allow list to guard the /verify/* API
+# `atlas-app` only uses approved, rejected for now.
+VerifiedState = Enum('Verify State', [(name, name) for name in ['approved', 'rejected', 'undefined']
+])
