b251ae7a1c94edfd912cc2191dd1b98c9bb559c4
rush42 <laurenz.rasche@gmail.com>
2023-10-17 17:16:46 +0200

    add api endpoint for export and view initialization to separate processing and api

--- atlas-geo/atlas-geo/api/db.py
+++ atlas-geo/atlas-geo/api/db.py
@@ -5,5 +5,6 @@ database_name = os.environ.get('PGDATABASE', "postgres")
 database_user = os.environ.get('PGUSER', "postgres")
 database_pw = os.environ.get('PGPASSWORD', "mysecretpassword")
 database_port = os.environ.get('PGPORT', "5432")
+api_secret = os.environ.get('API_SECRET', " ")
 
 conn_string = "host=%s dbname=%s user=%s password=%s port=%s" %(database_server, database_name, database_user, database_pw, database_port)
