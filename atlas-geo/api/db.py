b2fa73e8a96cedcaaee988209dccf9cd2ea12dfe
Henri <henri@fixmycity.de>
2023-10-19 10:02:18 +0200

    Add and remove space/returns

--- atlas-geo/api/db.py
+++ atlas-geo/api/db.py
@@ -1,10 +1,10 @@
 import os
 
-database_server = os.environ.get('PGHOST', "localhost") 
+database_server = os.environ.get('PGHOST', "localhost")
 database_name = os.environ.get('POSTGRES_DB', "postgres")
 database_user = os.environ.get('POSTGRES_USER', "postgres")
 database_pw = os.environ.get('POSTGRES_PASSWORD', "mysecretpassword")
 database_port = os.environ.get('POSTGRES_PORT', "5432")
-api_secret = os.environ.get('API_SECRET', " ")
+api_secret = os.environ.get('API_SECRET', "")
 
 conn_string = "host=%s dbname=%s user=%s password=%s port=%s" %(database_server, database_name, database_user, database_pw, database_port)
