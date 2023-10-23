a91b49b48759ea6371fec237a69321fc381d40d8
rush42 <laurenz.rasche@gmail.com>
2023-10-23 12:04:52 +0200

    isolate container envs

--- atlas-geo/atlas-geo/api/db.py
+++ atlas-geo/atlas-geo/api/db.py
@@ -1,9 +1,9 @@
 import os
 
-database_server = os.environ.get('PGHOST', "localhost")
-database_name = os.environ.get('POSTGRES_DB', "postgres")
-database_user = os.environ.get('POSTGRES_USER', "postgres")
-database_pw = os.environ.get('POSTGRES_PASSWORD', "mysecretpassword")
-database_port = os.environ.get('POSTGRES_PORT', "5432")
+database_server = os.environ.get('PGHOST', "")
+database_name = os.environ.get('PGDATABASE', "postgres")
+database_user = os.environ.get('PGUSER', "postgres")
+database_pw = os.environ.get('PGPASSWORD', "")
+database_port = os.environ.get('PGPORT', "5432")
 
 conn_string = "host=%s dbname=%s user=%s password=%s port=%s" %(database_server, database_name, database_user, database_pw, database_port)
