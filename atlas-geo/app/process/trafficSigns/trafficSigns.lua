1be987778d594e9fd50e89b3ec168e5f8e551f40
rush42 <laurenz.rasche@gmail.com>
2023-10-24 13:33:35 +0200

    just output traffix sign nodes

--- atlas-geo/app/process/trafficSigns/trafficSigns.lua
+++ atlas-geo/app/process/trafficSigns/trafficSigns.lua
@@ -11,29 +11,29 @@ local table = osm2pgsql.define_table({
     { column = 'geom', type = 'point' },
   }
 })
-local trafficSignWays = osm2pgsql.define_table({
-  name = '_trafficSigns_tmp',
-  ids = { type = 'way', id_column = 'osm_id', type_column = 'osm_type' },
-  columns = {
-    { column = 'tags', type = 'jsonb' },
-    { column = 'meta', type = 'jsonb' },
-    { column = 'geom', type = 'linestring' },
-  }
-})
+-- local trafficSignWays = osm2pgsql.define_table({
+--   name = '_trafficSigns_tmp',
+--   ids = { type = 'way', id_column = 'osm_id', type_column = 'osm_type' },
+--   columns = {
+--     { column = 'tags', type = 'jsonb' },
+--     { column = 'meta', type = 'jsonb' },
+--     { column = 'geom', type = 'linestring' },
+--   }
+-- })
 
 -- This table collects information about traffic signs, which are oriented by the way the belong to
 -- assuming that every traffic sign belongs to at most one way this table can hold up to two entries per traffic sign.
 -- That is one for its predecessor and an other for its successor. Traffic signs located at the beginning or end of a way only have one such entry.
 -- A single entry holds the osm id of the traffic sign as `node_id`, and one index from which the angle is to be taken (in way direction)
-local directionTable = osm2pgsql.define_table({
-  name = '_trafficSignDirections',
-  ids = { type = 'way', id_column = 'osm_id', type_column = 'osm_type' },
-  columns = {
-    { column = 'node_id', type = 'bigint' },
-    { column = 'idx', type = 'int' },
-    { column = 'geom', type = 'linestring' },
-  }
-})
+-- local directionTable = osm2pgsql.define_table({
+--   name = '_trafficSignDirections',
+--   ids = { type = 'way', id_column = 'osm_id', type_column = 'osm_type' },
+--   columns = {
+--     { column = 'node_id', type = 'bigint' },
+--     { column = 'idx', type = 'int' },
+--     { column = 'geom', type = 'linestring' },
+--   }
+-- })
 
 local function splitDirections(tags)
   local directionOffsets = { ["forward"] = 0, ["backward"] = 180 }
@@ -94,59 +94,66 @@ function osm2pgsql.process_node(object)
   if ExitProcessing(object) then return end
 
   local tags = object.tags
-  if tags.direction ~= nil then
-    local direction = tonumber(tags.direction)
-    if direction == nil then
-      direction = cardinalDirection2Degree(tags.direction)
-    end
-    if direction ~= nil then
-      tags.direction = tags.direction
-    else
-      to_orient[object.id] = true
-    end
-  end
-  for _, traffic_sign in pairs(splitDirections(tags)) do
-    table:insert({
-      tags = traffic_sign,
-      meta = Metadata(object),
-      geom = object:as_point()
-    })
-  end
+
+  table:insert({
+    tags = tags,
+    meta = Metadata(object),
+    geom = object:as_point()
+  })
+
+  -- if tags.direction ~= nil then
+  --   local direction = tonumber(tags.direction)
+  --   if direction == nil then
+  --     direction = cardinalDirection2Degree(tags.direction)
+  --   end
+  --   if direction ~= nil then
+  --     tags.direction = tags.direction
+  --   else
+  --     to_orient[object.id] = true
+  --   end
+  -- end
+  -- for _, traffic_sign in pairs(splitDirections(tags)) do
+  --   table:insert({
+  --     tags = traffic_sign,
+  --     meta = Metadata(object),
+  --     geom = object:as_point()
+  --   })
+  -- end
 end
 
-function osm2pgsql.process_way(object)
-  local prev_idx = nil
-  local prev_was_sign = false
-  local prev_node_id = nil
-  local is_sign = false
-  for idx, id in pairs(object.nodes) do
-    is_sign = to_orient[id]
-    if is_sign then
-      if prev_idx ~= nil then
-        directionTable:insert({
-          node_id = id,
-          idx = prev_idx,
-          geom = object:as_linestring()
-        })
-      end
-    end
-    if prev_was_sign then
-      directionTable:insert({
-        node_id = prev_node_id,
-        idx = prev_idx,
-        geom = object:as_linestring()
-      })
-    end
-    prev_idx = idx
-    prev_node_id = id
-    prev_was_sign = is_sign
-  end
-  if ExitProcessing(object) then return end
-  if object.is_closed or object.tags.traffic_sing == 'None' then return end
+-- function osm2pgsql.process_way(object)
+--   local prev_idx = nil
+--   local prev_was_sign = false
+--   local prev_node_id = nil
+--   local is_sign = false
+--   for idx, id in pairs(object.nodes) do
+--     is_sign = to_orient[id]
+--     if is_sign then
+--       if prev_idx ~= nil then
+--         directionTable:insert({
+--           node_id = id,
+--           idx = prev_idx,
+--           geom = object:as_linestring()
+--         })
+--       end
+--     end
+--     if prev_was_sign then
+--       directionTable:insert({
+--         node_id = prev_node_id,
+--         idx = prev_idx,
+--         geom = object:as_linestring()
+--       })
+--     end
+--     prev_idx = idx
+--     prev_node_id = id
+--     prev_was_sign = is_sign
+--   end
+--   if ExitProcessing(object) then return end
+--   if object.is_closed or object.tags.traffic_sing == 'None' then return end
   
-  trafficSignWays:insert({
-    tags = object.tags,
-    meta = Metadata(object),
-    geom = object:as_linestring(),
-  })
-end
+--   trafficSignWays:insert({
+--     tags = object.tags,
+--     meta = Metadata(object),
+--     geom = object:as_linestring(),
+--   })
+-- end
