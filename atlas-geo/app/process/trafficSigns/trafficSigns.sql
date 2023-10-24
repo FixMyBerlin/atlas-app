c8c2e9848522e77bfc7bb7e5fd102e8b63b1804d
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-10-24 16:45:46 +0200

    Update app/process/trafficSigns/trafficSigns.sql
    Co-authored-by: Tobias <tobias@fixmycity.de>

--- atlas-geo/app/process/trafficSigns/trafficSigns.sql
+++ atlas-geo/app/process/trafficSigns/trafficSigns.sql
@@ -1,2 +1,2 @@
-select avg(degrees(ST_Azimuth(st_pointn(geom, idx), st_pointn(geom, idx+1)))), node_id from "_trafficSignDirections" group by node_id;
-SELECT *  FROM "trafficSigns" join orientations on "trafficSigns".osm_id = "orientations".node_id;
+-- SELECT avg(degrees(ST_Azimuth(st_pointn(geom, idx), st_pointn(geom, idx+1)))), node_id FROM "_trafficSignDirections" GROUP BY node_id;
+-- SELECT *  FROM "trafficSigns" join orientations on "trafficSigns".osm_id = "orientations".node_id;
