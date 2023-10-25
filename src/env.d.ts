31927905a7486147881d57bc33fc97f177ddd877
Tobias <tobias@fixmycity.de>
2023-10-25 09:52:16 +0200

    Cleanup vite comments

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
-/// <reference types="vite/client" />
-// About https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
-
 interface ImportMetaEnv {
   readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
 
