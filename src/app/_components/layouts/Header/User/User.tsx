43aff45f6abd72cc15f2b264d03c6e08c5647a8b
Tobias <tobias@fixmycity.de>
2023-10-25 12:01:55 +0200

    Use `as Route` to overwrite TS router warnings
    Follow up to 576ce6a2

--- src/app/_components/layouts/Header/User/User.tsx
+++ src/app/_components/layouts/Header/User/User.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { useRouter } from 'next/navigation'
-
-import { UserIcon } from '@heroicons/react/24/outline'
-// import { useUserStore } from 'src/app/regionen/_components/MapInterface/UserInfo/useUserStore' TODO: remove
-import { useCurrentUser } from 'src/users/hooks/useCurrentUser'
 import { useMutation } from '@blitzjs/rpc'
-import logout from 'src/auth/mutations/logout'
-import { LoggedIn } from './LoggedIn'
+import { UserIcon } from '@heroicons/react/24/outline'
+import { Route } from 'next'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 import { useHasPermissions } from 'src/app/_hooks/useHasPermissions'
+import logout from 'src/auth/mutations/logout'
+import { useCurrentUser } from 'src/users/hooks/useCurrentUser'
+import { LoggedIn } from './LoggedIn'
 
 export const User: React.FC = () => {
   const user = useCurrentUser()
   const [logoutMutation] = useMutation(logout)
   const router = useRouter()
   const hasPermissions = useHasPermissions()
-  const loginUrl =
-    '/login?next=' + encodeURIComponent(`${location.pathname}${location.search}`).toString()
+  const loginUrl = `/login?next=${encodeURIComponent(
+    `${location.pathname}${location.search}`,
+  ).toString()}` as Route
 
   return (
     <div className="sm:ml-6">
       {user === null ? (
         <button
           className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:border sm:border-gray-700"
-          // @ts-ignore
           onClick={() => void router.push(loginUrl)}
         >
           <span className="sr-only">Anmelden</span>
