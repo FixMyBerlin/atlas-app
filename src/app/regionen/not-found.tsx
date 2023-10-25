1ca45f949ad14820c64e16200b534279616e4271
Tobias <tobias@fixmycity.de>
2023-10-25 14:18:16 +0200

    Update wording for 404 of region

--- src/app/regionen/not-found.tsx
+++ src/app/regionen/not-found.tsx
@@ -10,14 +10,14 @@ export default function Page404() {
           <div className="text-center">
             <p className="text-base font-semibold text-yellow-500">404</p>
             <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Seite nicht gefunden
+              Region nicht gefunden
             </h1>
             <p className="mt-2 text-base text-gray-500">
-              Leider konnten wir diese Seite nicht finden.
+              Leider konnten wir diese Region nicht finden.
             </p>
             <div className="mt-6">
-              <Link href="/">
-                Zur Startseite
+              <Link href="/regionen">
+                Zur Regionsliste
                 <span aria-hidden="true"> &rarr;</span>
               </Link>
             </div>
