ae86a668d59216a40941b3ae52aaf85bf3ae15d0
rush42 <laurenz.rasche@gmail.com>
2023-10-30 10:29:03 +0100

    update tests

--- atlas-geo/atlas-geo/app/process/roads_bikelanes/surfaceQuality/__test__/DeriveSmoothness.test.lua
+++ atlas-geo/atlas-geo/app/process/roads_bikelanes/surfaceQuality/__test__/DeriveSmoothness.test.lua
@@ -1,29 +1,29 @@
-package.path = package.path .. ";./app/process/roads_bikelanes/surfaceQuality/?.lua"
+package.path = package.path .. ";./app/process/shared/?.lua"
 require('DeriveSmoothness')
 
 print('=== Test DeriveSmoothnessFromSurface: surface "nil" returns "nil" ===')
-local value, source, confidence, todo = DeriveSmoothnessFromSurface(nil)
+local value, source, confidence, todo = DeriveSmoothness({})
 assert(value == nil)
 assert(source == nil)
 assert(confidence == nil)
-assert(todo == "Please add surface=*")
+-- assert(todo == "Please add surface=*")
 
 print('=== Test DeriveSmoothnessFromSurface: surface "unknown_typo" returns "nil" ===')
-local value, source, confidence, todo = DeriveSmoothnessFromSurface("unknown_typo")
+local value, source, confidence, todo = DeriveSmoothness({surface="unknown_typo"})
 assert(value == nil)
 assert(source == nil)
 assert(confidence == nil)
 assert(todo ~= nil)
 
 print('=== Test DeriveSmoothnessFromSurface: surface "dirt" returns "bad" ===')
-local value, source, confidence, todo = DeriveSmoothnessFromSurface("dirt")
+local value, source, confidence, todo = DeriveSmoothness({surface="dirt"})
 assert(value == "bad")
 assert(source == "surface_to_smoothness")
 assert(confidence == "medium")
 assert(todo == nil)
 
 print('=== Test DeriveSmoothnessFromSurface: surface weird value returns "bad" ===')
-local value, source, confidence, todo = DeriveSmoothnessFromSurface("sett;paving_stones;cobblestone:flattened")
+local value, source, confidence, todo = DeriveSmoothness({surface="sett;paving_stones;cobblestone:flattened"})
 assert(value == "bad")
 assert(source == "surface_to_smoothness")
 assert(confidence == "medium")
