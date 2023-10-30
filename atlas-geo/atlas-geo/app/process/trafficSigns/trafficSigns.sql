d44f04468cfbe2941ff4f69f4649c93d78d3c2dc
rush42 <laurenz.rasche@gmail.com>
2023-10-30 15:57:18 +0100

    add logic for orienting traffic signs based on the way they belong to

--- atlas-geo/atlas-geo/app/process/trafficSigns/trafficSigns.sql
+++ atlas-geo/atlas-geo/app/process/trafficSigns/trafficSigns.sql
@@ -1,2 +1,17 @@
--- SELECT avg(degrees(ST_Azimuth(st_pointn(geom, idx), st_pointn(geom, idx+1)))), node_id FROM "_trafficSignDirections" GROUP BY node_id;
--- SELECT *  FROM "trafficSigns" join orientations on "trafficSigns".osm_id = "orientations".node_id;
+-- compute the orientation based on adjacent nodes
+create temp table orientations as select avg(degrees(ST_Azimuth(st_pointn(geom, idx), st_pointn(geom, idx+1)))) as orientation, node_id from "_trafficSignDirections" group by node_id;
+
+-- update the traffic signs for which we could compute an orientation from the query above taking the `offset` into account
+update "trafficSigns" 
+    set tags=(tags || jsonb_build_object('direction', (orientations.orientation + (tags->>'offset')::INTEGER)::numeric % 360)) 
+    from orientations 
+    where "trafficSigns".osm_id = "orientations".node_id;
+
+-- inverse the sign off all traffic signs which have an offset to avoid ambiguities
+update "trafficSigns" set osm_id=osm_id*-1 where tags->>'offset' = '180';
+
+-- remove `offset` from tags
+update "trafficSigns" set tags=tags-'offset' ;
+
+-- remove direction table 
+drop table "_trafficSignDirections";
