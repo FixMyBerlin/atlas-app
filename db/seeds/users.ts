45ace42461edd32f097ab7055b69842e102ae1ee
Sven König <sven@fixmycity.de>
2023-11-02 10:41:05 +0100

    prepare table User for osm

--- db/seeds/users.ts
+++ db/seeds/users.ts
@@ -5,6 +5,7 @@ type Users = Prisma.UserUncheckedCreateInput[]
 
 export const generateUserEmail = (slug: string) => `${slug}@fixmycity.de`
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
+let osmId = 1000
 
 const seedUsers = async () => {
   const allRegions = await db.region.findMany()
@@ -14,36 +15,36 @@ const seedUsers = async () => {
 
   const genericUsers: Users = [
     {
+      osmId: osmId++,
       email: 'admin@fixmycity.de',
       role: 'ADMIN',
-      firstName: 'Admin',
-      lastName: 'Admin-User',
+      name: 'Admin',
       phone: '030 549 086 65 - 90',
       hashedPassword,
     },
     {
+      osmId: osmId++,
       email: 'all-regions@fixmycity.de',
       role: 'USER',
-      firstName: 'All-Regions',
-      lastName: 'All-Regions-User',
+      name: 'All-Regions',
       phone: '030 549 086 65 - 91',
       hashedPassword,
     },
     {
+      osmId: osmId++,
       email: 'no-region@fixmycity.de',
       role: 'USER',
-      firstName: 'No-Region',
-      lastName: 'No-Region-User',
+      name: 'No-Region',
       phone: '030 549 086 65 - 92',
       hashedPassword,
     },
   ]
 
   const regionAdmins: Users = allRegions.map(({ id, slug }) => ({
+    osmId: osmId++,
     email: generateUserEmail(slug),
     role: 'USER',
-    firstName: `${capitalize(slug)}-Admin`,
-    lastName: `${capitalize(slug)}-Admin-User`,
+    name: `${capitalize(slug)}-Admin`,
     phone: `030 549 086 65 - ${id}`,
     hashedPassword,
   }))
