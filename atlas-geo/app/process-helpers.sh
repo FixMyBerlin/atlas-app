6064c22764cd403ff664b8a5a6fc1a31bf571b67
rush42 <laurenz.rasche@gmail.com>
2023-11-06 16:34:08 +0100

    use `curl` instead of `wget`

--- atlas-geo/app/process-helpers.sh
+++ atlas-geo/app/process-helpers.sh
@@ -9,9 +9,9 @@ notify() {
   if [ -z $SYNOLOGY_TOKEN ]; then 
     return 0;
   fi
-  local text="$ENVIRONMENT: $1"
-  local url="https://fixmy.diskstation.me:54545/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=$SYNOLOGY_TOKEN&payload={\"text\": \"$text\"}"
-  wget --header="Content-Type: application/json" --post-data='{}' "$url" -O /dev/null
+  local payload="{\"text\": \"$ENVIRONMENT: $1\"}"
+  local url="https://fixmy.diskstation.me:54545/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=$SYNOLOGY_TOKEN"
+  curl -s -X POST $url -d "payload=$payload"
 }
 
 run_lua_if_debug() {
