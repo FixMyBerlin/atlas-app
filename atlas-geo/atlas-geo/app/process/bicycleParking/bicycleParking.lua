8ad1fa476e4b9e498da5f7a91821fae491934a14
rush42 <laurenz.rasche@gmail.com>
2023-11-06 10:10:54 +0100

    add processing for bicycle parking areas

--- atlas-geo/atlas-geo/app/process/bicycleParking/bicycleParking.lua
+++ atlas-geo/atlas-geo/app/process/bicycleParking/bicycleParking.lua
@@ -4,7 +4,7 @@ require("Metadata")
 
 local nodeTable = osm2pgsql.define_table({
   name = 'bicycleParking-points',
-  ids = { type = 'node', id_column = 'osm_id', type_column = 'osm_type' },
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
   columns = {
     { column = 'tags', type = 'jsonb' },
     { column = 'meta', type = 'jsonb' },
@@ -14,24 +14,44 @@ local nodeTable = osm2pgsql.define_table({
 
 local areaTable = osm2pgsql.define_table({
   name = 'bicycleParking-areas',
-  ids = { type = 'node', id_column = 'osm_id', type_column = 'osm_type' },
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
   columns = {
     { column = 'tags', type = 'jsonb' },
     { column = 'meta', type = 'jsonb' },
-    { column = 'geom', type = 'linestring' },
+    { column = 'geom', type = 'polygon' },
   }
 })
+
 function ExitProcessing(object) 
   if object.tags.amenity ~= 'bicycle_parking' then
     return true
   end
 end
 
+function capacityNormalization(tags)
+  local capacities = {capacity = tonumber(tags.capacity) or 0}
+  for key, val in pairs(tags) do
+    if osm2pgsql.has_prefix(key, "capacity") then
+      val = tonumber(val)
+      if val ~= nil then
+        capacities.capacity = capacities.capacity - val
+      end
+      capacities[key] = val
+    end
+  end
+  for k, v in pairs(capacities) do
+    if v == 0 then
+      capacities[k] = nil
+    end
+  end
+  return capacities
+end
+
 function osm2pgsql.process_node(object)
   local tags = object.tags
   if ExitProcessing(object) then return end
   nodeTable:insert({
-    tags = tags,
+    tags = capacityNormalization(tags),
     meta = Metadata(object),
     geom = object:as_point()
   })
@@ -40,8 +60,9 @@ end
 function osm2pgsql.process_way(object)
   if ExitProcessing(object) or not object.is_closed then return end
 
+  local tags = object.tags
   areaTable:insert({
-    tags = object.tags,
+    tags = capacityNormalization(tags),
     meta = Metadata(object),
     geom = object:as_polygon(),
   })
