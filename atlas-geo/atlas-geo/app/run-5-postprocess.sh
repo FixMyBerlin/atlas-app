d834af9e31e259635c29631000efb84c212aebc7
rush42 <laurenz.rasche@gmail.com>
2023-11-06 17:09:23 +0100

    silent initialization curl request

--- atlas-geo/atlas-geo/app/run-5-postprocess.sh
+++ atlas-geo/atlas-geo/app/run-5-postprocess.sh
@@ -15,6 +15,6 @@ psql -q -f "./POSTPROCESS.sql"
 
 # Creating verification tables and export functions in api service
 echo -e "INFO: Call api/init to create verification tables and export functions"
-curl -G -d "secret=$API_SECRET" api/init
+curl -G -d "secret=$API_SECRET" api/init --silent --output '/dev/null'
 
 echo -e "\e[1m\e[7m Postprocessing – END \e[27m\e[21m\e[0m"
