49e45d828ec2d439dbcc3ca390e041b479c06063
rush42 <laurenz.rasche@gmail.com>
2023-11-07 12:57:51 +0100

    add dedicated channel for error logging

--- atlas-geo/app/process-helpers.sh
+++ atlas-geo/app/process-helpers.sh
@@ -4,11 +4,11 @@ OSM2PGSQL_BIN=/usr/bin/osm2pgsql
 PROCESS_DIR="./process/"
 
 notify() {
-  if [ -z $SYNOLOGY_TOKEN ]; then 
+  if [ -z $SYNOLOGY_LOG_TOKEN ]; then 
     return 0;
   fi
-  local payload="{\"text\": \"$ENVIRONMENT: $1\"}"
-  local url="https://fixmy.diskstation.me:54545/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=$SYNOLOGY_TOKEN"
+  local payload="{\"text\": \"#$ENVIRONMENT: $1\"}"
+  local url="$SYNOLOGY_URL$SYNOLOGY_LOG_TOKEN"
   curl -X POST $url -d "payload=$payload" --silent --output "/dev/null"
 }
 
@@ -24,7 +24,7 @@ run_lua_if_debug() {
 run_lua() {
   start_time=$(date +%s)
   echo -e "\e[1m\e[7m PROCESS START – Topic: $1 LUA \e[27m\e[21m\e[0m"
-  notify "PROCESS START – Topic: $1 LUA"
+  # notify "PROCESS START – Topic: #$1 LUA"
 
   # optional log output:
   # https://osm2pgsql.org/doc/manual.html#logging
@@ -35,23 +35,25 @@ run_lua() {
 
   end_time=$(date +%s)
   diff=$((end_time - start_time))
-  run_time=`date -d@$diff -u +%H:%M:%S`
+  # run_time=`date -d@$diff -u +%H:%M:%S`
+  run_time=`date -d@$diff -u +%M\m\ %S\s`
 
-  notify "PROCESS END – Topic: $1 LUA – took $run_time"
+  notify "#$1 #LUA finished in: *$run_time*"
   echo -e "\e[1m\e[7m PROCESS END – Topic: $1 LUA \e[27m\e[21m took $run_time\e[0m"
 }
 
 run_psql() {
   start_time=$(date +%s)
   echo -e "\e[1m\e[7m PROCESS START – Topic: $1 SQL \e[27m\e[21m\e[0m"
-  notify "PROCESS START – Topic: $1 SQL"
+  # notify "PROCESS START – Topic: #$1 SQL"
 
   psql -q -f "${PROCESS_DIR}$1.sql"
 
   end_time=$(date +%s)
   diff=$((end_time - start_time))
-  run_time=`date -d@$diff -u +%H:%M:%S`
+  # run_time=`date -d@$diff -u +%H:%M:%S`
+  run_time=`date -d@$diff -u +%M\m\ %S\s`
 
-  notify "PROCESS END – Topic: $1 SQL – took $run_time"
+  notify "#$1 #SQL finished in: *$run_time*"
   echo -e "\e[1m\e[7m PROCESS END – Topic: $1 SQL \e[27m\e[21m took $run_time\e[0m"
 }
--- atlas-geo/app/run.sh
+++ atlas-geo/app/run.sh
@@ -11,32 +11,42 @@ export SKIP_DOWNLOAD=${SKIP_DOWNLOAD:-0}
 export SKIP_TAG_FILTER=${SKIP_TAG_FILTER:-0}
 export DEBUG=${DEBUG:-0}
 export ID_FILTER=${ID_FILTER:-''}
+export SYNOLOGY_URL='https://fixmy.diskstation.me:54545/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token='
 
 source ./process-helpers.sh
 
+alert() {
+  if [ -z $SYNOLOGY_ERROR_LOG_TOKEN ]; then 
+    return 0;
+  fi
+  local payload="{\"text\": \"#$ENVIRONMENT: $1\"}"
+  local url="$SYNOLOGY_URL$SYNOLOGY_ERROR_LOG_TOKEN"
+  curl -X POST $url -d "payload=$payload" --silent --output "/dev/null"
+}
+
 if ! ./run-1-download.sh; then 
-    notify 'ERROR: download exited with non-zero status code'
+    alert 'ERROR: download exited with non-zero status code'
 fi
 
 if ! ./run-2-filter.sh; then 
-    notify 'ERROR: Filter exited with non-zero status code'
+    alert 'ERROR: Filter exited with non-zero status code'
 fi
 
 if ! ./run-3-migration.sh; then 
-    notify 'ERROR: Migration exited with non-zero status code'
+    alert 'ERROR: Migration exited with non-zero status code'
 fi
 
 process_start_time=$(date +%s)
 if ! ./run-4-process.sh; then 
-    notify 'ERROR: Process exited with non-zero status code'
+    alert 'ERROR: Process exited with non-zero status code'
 fi
 process_end_time=$(date +%s)
 export PROCESS_RUN_TIME_DIFF=$((process_end_time - process_start_time)) # used by metadata.sh
 
 if ! ./run-5-postprocess.sh; then 
-    notify 'ERROR: Postprocess exited with non-zero status code'
+    alert 'ERROR: Postprocess exited with non-zero status code'
 fi
 
 if ! ./run-7-metadata.sh; then 
-    notify 'ERROR: Metadata exited with non-zero status code'
+    alert 'ERROR: Metadata exited with non-zero status code'
 fi
