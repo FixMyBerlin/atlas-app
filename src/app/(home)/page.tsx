7717e37de13939a13c0e18600b3231945cdd8e0d
Sven König <sven@fixmycity.de>
2023-11-09 17:42:18 +0100

    add <ClientEnv>

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,12 +5,14 @@ import { HomePageHero } from './_components/HomePageHero'
 import { HomePageLive } from './_components/HomePageLive'
 import { HomePagePrimaryFeatures } from './_components/HomePagePrimaryFeatures'
 import { HomePageSecondaryFeaturesJustText } from './_components/HomePageSecondaryFeaturesJustText'
+import { ClientEnv } from './_components/ClientEnv'
 
 export const metadata: Metadata = {} // Using the defaults from src/layout.tsx
 
 export default function Home() {
   return (
     <>
+      <ClientEnv/>
       <HomePageHero />
       <HomePageCompanies />
       <HomePagePrimaryFeatures />
