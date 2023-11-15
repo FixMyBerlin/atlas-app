4beaaa239df538ff55fdb6b997236204d7486998
Tobias <tobias@fixmycity.de>
2023-11-15 10:09:16 +0100

    Refactor UserLoggedInAdminInfo

--- src/app/_components/layouts/Header/User/UserLoggedIn.tsx
+++ src/app/_components/layouts/Header/User/UserLoggedIn.tsx
@@ -5,44 +5,23 @@ import { clsx } from 'clsx'
 import Image from 'next/image'
 import { Fragment } from 'react'
 import { useRegionSlug } from 'src/app/(pages)/_components/regionUtils/useRegionSlug'
-import { LinkExternal } from 'src/app/_components/links/LinkExternal'
-import { linkStyles } from 'src/app/_components/links/styles'
-import { getEnvUrl } from 'src/app/_components/utils/getEnvUrl'
 import { useHasPermissions } from 'src/app/_hooks/useHasPermissions'
-import {
-  googleMapsUrlViewport,
-  mapillaryUrlViewport,
-  osmUrlViewport,
-} from 'src/app/regionen/[regionSlug]/_components/Inspector/Tools/osmUrls/osmUrls'
-import { useMapDebugState } from 'src/app/regionen/[regionSlug]/_components/mapStateInteraction/useMapDebugState'
-import { useMapParam } from 'src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam'
 import logout from 'src/auth/mutations/logout'
 import { isAdmin } from 'src/users/components/utils/usersUtils'
 import { CurrentUser } from 'src/users/queries/getCurrentUser'
+import { UserLoggedInAdminInfo } from './UserLoggedInAdminInfo'
 
-type Props = {
+export type UserLoggedInProp = {
   user: NonNullable<CurrentUser>
 }
 
-export const UserLoggedIn = ({ user }: Props) => {
-  const { toggleShowDebugInfo } = useMapDebugState()
+export const UserLoggedIn = ({ user }: UserLoggedInProp) => {
   const imgSrc = null
-
-  const devUrl = getEnvUrl('development')
-  const stagingUrl = getEnvUrl('staging')
-  const prodUrl = getEnvUrl('production')
 
   const [logoutMutation] = useMutation(logout)
 
   const isRegionsPage = Boolean(useRegionSlug())
   const hasPermissions = useHasPermissions()
-
-  const { mapParam } = useMapParam()
-  const osmUrlViewportUrl = mapParam && osmUrlViewport(mapParam.zoom, mapParam.lat, mapParam.lng)
-  const mapillaryUrlViewportUrl =
-    mapParam && mapillaryUrlViewport(mapParam.zoom, mapParam.lat, mapParam.lng)
-  const googleMapsViewportUrl =
-    mapParam && googleMapsUrlViewport(mapParam.zoom, mapParam.lat, mapParam.lng)
 
   return (
     <Menu as="div" className="relative ml-3 sm:ml-6">
@@ -87,60 +66,9 @@ export const UserLoggedIn = ({ user }: Props) => {
                 Sie haben zur Zeit keine Zugriffsrechte in dieser Region.
               </div>
             )}
-            {isAdmin(user) && <div className="bg-pink-300 text-xs leading-4">Du bist Admin.</div>}
           </div>
 
-          {isAdmin(user) && (
-            <ul className="bg-pink-300 px-4 py-2 text-sm">
-              <li>
-                <button type="button" onClick={() => toggleShowDebugInfo()} className={linkStyles}>
-                  Toggle <code>mapDebug</code>
-                </button>
-              </li>
-              <li>
-                {devUrl && (
-                  <LinkExternal blank href={devUrl}>
-                    Open DEV
-                  </LinkExternal>
-                )}
-              </li>
-              <li>
-                {stagingUrl && (
-                  <LinkExternal blank href={stagingUrl}>
-                    Open Staging
-                  </LinkExternal>
-                )}
-              </li>
-              <li>
-                {prodUrl && (
-                  <LinkExternal blank href={prodUrl}>
-                    Open Production
-                  </LinkExternal>
-                )}
-              </li>
-              {osmUrlViewportUrl && (
-                <li>
-                  <LinkExternal blank href={osmUrlViewportUrl}>
-                    Open OSM
-                  </LinkExternal>
-                </li>
-              )}
-              {mapillaryUrlViewportUrl && (
-                <li>
-                  <LinkExternal blank href={mapillaryUrlViewportUrl}>
-                    Open Mapillary
-                  </LinkExternal>
-                </li>
-              )}
-              {googleMapsViewportUrl && (
-                <li>
-                  <LinkExternal blank href={googleMapsViewportUrl}>
-                    Open GoogleMaps
-                  </LinkExternal>
-                </li>
-              )}
-            </ul>
-          )}
+          <UserLoggedInAdminInfo user={user} />
 
           <Menu.Item>
             {({ active }) => (
