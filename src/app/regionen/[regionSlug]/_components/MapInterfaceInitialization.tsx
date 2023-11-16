41d6366150ef3bb11dda1cc974029a6b24cec7ad
Tobias <tobias@fixmycity.de>
2023-11-16 08:25:53 +0100

    MapInterfaceInitialization: Cleanup variable names and code comments

--- src/app/regionen/[regionSlug]/_components/MapInterfaceInitialization.tsx
+++ src/app/regionen/[regionSlug]/_components/MapInterfaceInitialization.tsx
@@ -1,52 +1,43 @@
 'use client'
 
-import { useQuery } from '@blitzjs/rpc'
 import { useMemo, useState } from 'react'
-import { useRegionSlug } from 'src/app/(pages)/_components/regionUtils/useRegionSlug'
+import { useRegion } from 'src/app/(pages)/_components/regionUtils/useRegion'
+import { Spinner } from 'src/app/_components/Spinner/Spinner'
 import { useConfigParam } from 'src/app/regionen/[regionSlug]/_hooks/useQueryState/useConfigParam'
 import { useMapParam } from 'src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam'
-import getRegion from 'src/regions/queries/getRegion'
 import invariant from 'tiny-invariant'
 import { createMapRegionConfig } from './mapStateConfig/createMapRegionConfig'
 import { initializeMapRegionConfig } from './mapStateConfig/initializeMapRegionConfig'
 
 export const MapInterfaceInitialization = ({ children }: { children: React.ReactNode }) => {
-  const regionSlug = useRegionSlug()
-  const [region] = useQuery(getRegion, { slug: regionSlug })
+  const region = useRegion()
 
-  // Initialize the Map. This will update stale configs.
-  // BUT, only on hard reload; hot reloading is not in scope for this.
+  const { mapParam, setMapParam } = useMapParam()
+  const { configParam, setConfigParam } = useConfigParam()
+
+  // Initialize the Map and migrate old/stale config options.
+  // DEV: Hot reload does not work with this and is out of scope. Reload if things go wrong.
   // Some stale config options are removed, new options are added; state is preserved.
-  // Also guard against empty default searchParams; set them if any is missing (or empty)
   const freshConfig = useMemo(() => {
     invariant(region.themes)
     return createMapRegionConfig(region.themes)
   }, [region.themes])
 
-  const { mapParam, setMapParam } = useMapParam()
-  const { configParam, setConfigParam } = useConfigParam()
+  const [initialized, setInitialized] = useState(false)
 
-  // This initializes the `map` and `config` and allows us reset the config on demand.
-  // When we change stuff in our config, our URL config needs to change.
-  // Otherwise things blow up, becaues we look for config entries that where removed.
-  const [initializeAndReset, setInitializeAndReset] = useState(true)
-
-  if (initializeAndReset === true) {
+  if (initialized === false) {
     // The order in which we initially call set*Param is the order we see in the URL.
-    const map = mapParam || {
-      zoom: Number(region?.map?.zoom ?? 12),
-      lat: Number(region?.map?.lat ?? 52.507),
-      lng: Number(region?.map?.lng ?? 13.367),
-    }
-    void setMapParam(map)
+    void setMapParam(mapParam || region.map)
 
-    const initialConfig = initializeMapRegionConfig({
+    const initializedConfig = initializeMapRegionConfig({
       freshConfig,
       urlConfig: configParam,
     })
-    void setConfigParam(initialConfig)
+    void setConfigParam(initializedConfig)
 
-    setInitializeAndReset(false)
+    setInitialized(true)
+
+    return <Spinner page />
   }
 
   return <>{children}</>
