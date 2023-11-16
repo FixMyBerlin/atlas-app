eddf0f3ec786c0945837fd4d6fbe65047e794fd0
Tobias <tobias@fixmycity.de>
2023-11-16 08:24:10 +0100

    useMapParam: Refactor parse/serialize for readability

--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
@@ -4,13 +4,17 @@ export const useMapParam = () => {
   const fallback = { zoom: 12, lat: 52.507, lng: 13.367 }
 
   const mapParamParser = createParser({
-    parse: (query: string) => ({
-      zoom: Number(query.split('/')[0] ?? fallback.zoom),
-      lat: Number(query.split('/')[1] ?? fallback.lat),
-      lng: Number(query.split('/')[2] ?? fallback.lat),
-    }),
-    serialize: ({ zoom, lat, lng }: { zoom: number; lat: number; lng: number }) =>
-      `${zoom}/${lat}/${lng}`,
+    parse: (query: string) => {
+      const splitQuery = query.split('/')
+      return {
+        zoom: Number(splitQuery[0] ?? fallback.zoom),
+        lat: Number(splitQuery[1] ?? fallback.lat),
+        lng: Number(splitQuery[2] ?? fallback.lat),
+      }
+    },
+    serialize: ({ zoom, lat, lng }: { zoom: number; lat: number; lng: number }) => {
+      return `${zoom}/${lat}/${lng}`
+    },
   }).withOptions({ history: 'push' })
 
   const [mapParam, setMapParam] = useQueryState('map', mapParamParser)
