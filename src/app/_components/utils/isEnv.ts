6984d1a4bbe7539f133378b297621f96c012ff95
Tobias <tobias@fixmycity.de>
2023-11-16 10:24:57 +0100

    Refactor `envKey` to use the new `process.env.NEXT_PUBLIC_ENV `

--- src/app/_components/utils/isEnv.ts
+++ src/app/_components/utils/isEnv.ts
@@ -1,12 +1,11 @@
 export const isProd =
-  process.env.NODE_ENV === 'production' &&
-  process.env.NEXT_PUBLIC_APP_ORIGIN === 'https://radverkehrsatlas.de'
+  process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_ENV === 'production'
 
 export const isStaging =
-  process.env.NODE_ENV === 'production' &&
-  process.env.NEXT_PUBLIC_APP_ORIGIN === 'https://staging.radverkehrsatlas.de'
+  process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_ENV === 'staging'
 
-export const isDev = process.env.NODE_ENV === 'development'
+export const isDev =
+  process.env.NODE_ENV === 'development' && process.env.NEXT_PUBLIC_ENV === 'development'
 
 export const isBrowser = typeof window !== 'undefined'
 
