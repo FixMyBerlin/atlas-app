1e622ec63bc07d0772b4c9001d82a6f946d54f02
Tobias <tobias@fixmycity.de>
2023-11-16 08:23:37 +0100

    useMapParam: Cleanup old code

--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
@@ -1,10 +1,6 @@
 import { createParser, useQueryState } from 'next-usequerystate'
 
 export const useMapParam = () => {
-  // const [lat, setLat] = useQueryState('lat', parseAsFloat.withDefault(region?.map?.lat || 52.507))
-  // const [lng, setLng] = useQueryState('lng', parseAsFloat.withDefault(region?.map?.lng || 13.367))
-  // const [zoom, setZoom] = useQueryState('zoom', parseAsFloat.withDefault(region?.map?.zoom || 12))
-
   const fallback = { zoom: 12, lat: 52.507, lng: 13.367 }
 
   const mapParamParser = createParser({
