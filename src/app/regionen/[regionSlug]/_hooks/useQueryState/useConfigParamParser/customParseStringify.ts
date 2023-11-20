ad6a2ffa07d7fd8227f63da604f0292a61437557
Tobias <tobias@fixmycity.de>
2023-11-20 03:51:59 +0100

    Params: Extract isJsurlString, jsurlParse, jurlStringify

--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useConfigParamParser/customParseStringify.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useConfigParamParser/customParseStringify.ts
@@ -1,19 +1,12 @@
-import jsurl from 'jsurl2'
-import { legacyParse } from './legacyParse'
-import { expandObjectKeys, minimizeObjectKeys } from './minimzeObjectKeys'
 import { initializeMapRegionConfig } from '../../../_components/mapStateConfig/initializeMapRegionConfig'
 import { ThemeConfig } from '../../../_components/mapStateConfig/type'
-
-// Docs: https://react-location.tanstack.com/guides/custom-search-param-serialization#using-jsurl
-// Using https://github.com/wmertens/jsurl2
+import { isJsurlString, jsurlParse, jurlStringify } from './jurlParseStringify'
+import { legacyParse } from './legacyParse'
+import { expandObjectKeys, minimizeObjectKeys } from './minimzeObjectKeys'
 
 export const customParse = (value: string): unknown => {
-  // "!" is an array in jsurl2, which is the default for our config
-  // "(" is an object in jsurl2, which is the reset-fallback
-  const newEncoding = value.startsWith('!') || value.startsWith('(')
-
-  if (newEncoding) {
-    return expandObjectKeys(jsurl.tryParse(value, { reset: true }))
+  if (isJsurlString(value)) {
+    return expandObjectKeys(jsurlParse(value) as Record<string, any>)
   }
 
   return legacyParse(value)
@@ -34,8 +27,5 @@ export const customParseConfig = (value: string, freshConfig: ThemeConfig[]): un
 }
 
 export const customStringify = (value: Record<string, any>) => {
-  return jsurl.stringify(minimizeObjectKeys(value), {
-    rich: true,
-    short: false,
-  })
+  return jurlStringify(minimizeObjectKeys(value))
 }
--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useDrawParam.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useDrawParam.ts
@@ -1,11 +1,15 @@
 import { createParser, useQueryState } from 'next-usequerystate'
 import { DrawArea } from '../../_components/Map/Calculator/CalculatorControlsDrawControl'
-import { customParse, customStringify } from './useConfigParamParser/customParseStringify'
+import { jsurlParse, jurlStringify } from './useConfigParamParser/jurlParseStringify'
 
 export const useDrawParam = () => {
   const drawParamParser = createParser({
-    parse: (query: string) => customParse(query) as DrawArea[],
-    serialize: (value: DrawArea[]) => customStringify(value),
+    parse: (query: string) => {
+      return jsurlParse(query) as DrawArea[]
+    },
+    serialize: (value: DrawArea[]) => {
+      return jurlStringify(value)
+    },
   }).withOptions({ history: 'replace' })
 
   const [drawParam, setDrawParam] = useQueryState('draw', drawParamParser)
