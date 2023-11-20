9c39c179476815bbd627d3d98d5dc717078cad5a
Tobias <tobias@fixmycity.de>
2023-11-20 06:00:56 +0100

    Remove non-needed MapInterfaceInitialization

--- src/app/regionen/[regionSlug]/_components/MapInterfaceInitialization.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export const MapInterfaceInitialization = ({ children }: { children: React.ReactNode }) => {
-  console.log('## MapInterfaceInitialization rendered', {
-    nothing: 'to happening here anymore',
-  })
-
-  return <>{children}</>
-}
