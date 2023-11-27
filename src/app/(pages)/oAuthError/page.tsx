24febb7821a60dc7c29fdcae74476eabab1da371
Tobias <tobias@fixmycity.de>
2023-11-27 15:54:33 +0100

    Fix oAuthError params dump

--- src/app/(pages)/oAuthError/page.tsx
+++ src/app/(pages)/oAuthError/page.tsx
@@ -1,21 +1,15 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
-import imageBmdv from 'src/app/(pages)/kontakt/assets/logo-bmdv-foerderung.svg'
-import imageMfund from 'src/app/(pages)/kontakt/assets/logo-mfund.png'
-import { Link } from 'src/app/_components/links/Link'
-import { LinkMail } from 'src/app/_components/links/LinkMail'
-import { LinkTel } from 'src/app/_components/links/LinkTel'
 
 export const metadata: Metadata = {
   robots: 'noindex',
   title: 'Fehler bei der Nutzung der OpenStreetMap Anmeldung',
 }
 
-export default function Kontakt({ params }) {
+export default function Kontakt({ searchParams }) {
   return (
     <>
       <h1>Fehler bei der Nutzung der OpenStreetMap Anmeldung</h1>
-      <pre className="mt-4">{JSON.stringify(params, undefined, 2)}</pre>
+      <pre className="mt-4">{JSON.stringify(searchParams, undefined, 2)}</pre>
     </>
   )
 }
