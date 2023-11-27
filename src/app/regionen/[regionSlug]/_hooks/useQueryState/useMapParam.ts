0f255424a6ae9b30b7f787ae31c2849edf0d8278
Tobias <tobias@fixmycity.de>
2023-11-27 14:57:52 +0100

    useMapParam: Extract parser and serializer
    Makes the code reusable.

--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam.ts
@@ -1,22 +1,35 @@
 import { createParser, useQueryState } from 'next-usequerystate'
 import { useStaticRegion } from 'src/app/(pages)/_components/regionUtils/useStaticRegion'
 
+const fallback = { lat: 52.5, lng: 13.4, zoom: 12.1 }
+
+const parseMapParam = (query: string) => {
+  const splitQuery = query.split('/')
+  return {
+    zoom: Number(splitQuery[0] ?? fallback.zoom),
+    lat: Number(splitQuery[1] ?? fallback.lat),
+    lng: Number(splitQuery[2] ?? fallback.lng),
+  }
+}
+
+export const serializeMapParam = ({
+  zoom,
+  lat,
+  lng,
+}: {
+  zoom: number
+  lat: number
+  lng: number
+}) => {
+  return `${zoom}/${lat}/${lng}`
+}
+
 export const useMapParam = () => {
   const region = useStaticRegion()
-  const fallback = { lat: 52.5, lng: 13.4, zoom: 12.1 }
 
   const mapParamParser = createParser({
-    parse: (query: string) => {
-      const splitQuery = query.split('/')
-      return {
-        zoom: Number(splitQuery[0] ?? fallback.zoom),
-        lat: Number(splitQuery[1] ?? fallback.lat),
-        lng: Number(splitQuery[2] ?? fallback.lng),
-      }
-    },
-    serialize: ({ zoom, lat, lng }: { zoom: number; lat: number; lng: number }) => {
-      return `${zoom}/${lat}/${lng}`
-    },
+    parse: (query) => parseMapParam(query),
+    serialize: (object) => serializeMapParam(object),
   })
     .withOptions({ history: 'push' })
     .withDefault({
