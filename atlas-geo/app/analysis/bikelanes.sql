63827a6359308a6dbf2715632c2c5dbd2bbcdcd5
rush42 <laurenz.rasche@gmail.com>
2023-11-27 11:05:41 +0100

    introduce filter on `admin_level`
    prefix functions with `atlas_`
    
    rename column `stats` -> `bikelanes_category` to reference source data

--- atlas-geo/app/analysis/bikelanes.sql
+++ atlas-geo/app/analysis/bikelanes.sql
@@ -1,5 +1,5 @@
--- STEP 1: create a function containing for "quantizing" LineStrings into homogenous parts of length  `res` +/- 25% (Note: segments shorter than `res` keep their length)
-CREATE OR REPLACE FUNCTION QuantizeLineString(ls Geometry(LineString), res numeric)
+-- this function quantizes `LineStrings` into `Points` where each point represents a homogenous segment each of length  `res` +/- 25% (Note: segments shorter than `res` keep their length)
+CREATE OR REPLACE FUNCTION atlas_QuantizeLineString(ls Geometry(LineString), res numeric)
     RETURNS TABLE(
         len numeric,
         geom Geometry(point)
@@ -17,16 +17,18 @@ END;
 $$
 LANGUAGE plpgsql;
 
--- create a Table conatining osm_id and tags for all quantized bikelanes geometries
-CREATE temp TABLE _bikelanes_splitted AS
+
+-- STEP 1: quantize bikelane data into points
+CREATE temp TABLE _bikelanesQuantized AS
 SELECT
     osm_id,
     tags,
-(QuantizeLineString(geom, 100)).* AS geom
+(atlas_QuantizeLineString(geom, 100)).* AS geom
 FROM
-    bikelanes
-    -- create a geospatial index on `_bikelanes_splitted` to speed up the spatial join;
-    CREATE INDEX "_bikelanes_stats_geom_idx" ON _bikelanes_splitted USING gist(geom);
+    bikelanes;
+
+-- create a geospatial index on `_bikelanesQuantized` to speed up the spatial join;
+CREATE INDEX "_bikelanes_stats_geom_idx" ON _bikelanesQuantized USING gist(geom);
 
 -- STEP 2: calculate the distribution of our `category` for each area of the `boundary` data set
 -- make `osm_id` the primary of the `boundaries` table to allow selecting non-group-by columns
@@ -36,34 +38,56 @@ ALTER TABLE boundaries
 ALTER TABLE boundaries
     ADD CONSTRAINT osm_id_key PRIMARY KEY (osm_id);
 
--- spatialy join `_bikelanes_splitted` with `boundaries` then group by category and boundaries.osm_id aggreagate the results in a single json object per area
-CREATE TABLE boundary_stats AS
+-- spatialy join `_bikelanesQuantized` with `boundaries` then group by category and boundaries.osm_id aggreagate the results in a single json object per area
+DROP TABLE IF EXISTS "boundaryStats";
+
+CREATE TABLE "boundaryStats" AS
 SELECT
     osm_id,
     geom,
     region,
-    jsonb_object_agg(category, len)
+    jsonb_object_agg(category, len) AS bikelanes_category
 FROM (
     SELECT
         boundaries.osm_id AS osm_id,
         boundaries.geom AS geom,
         boundaries.tags ->> 'name' AS region,
-        _bikelanes_splitted.tags ->> 'category' AS category,
-        sum(_bikelanes_splitted.len) AS len
+        _bikelanesQuantized.tags ->> 'category' AS category,
+        sum(_bikelanesQuantized.len) AS len
     FROM
         boundaries
-        JOIN _bikelanes_splitted ON ST_Intersects(boundaries.geom, _bikelanes_splitted.geom)
-    GROUP BY
-        boundaries.osm_id,
-        _bikelanes_splitted.tags ->> 'category') AS sq
+        JOIN _bikelanesQuantized ON ST_Intersects(boundaries.geom, _bikelanesQuantized.geom)
+    WHERE (boundaries.tags ->> 'admin_level')::int IN (4, 6, 7, 8)
+GROUP BY
+    boundaries.osm_id,
+    _bikelanesQuantized.tags ->> 'category') AS sq
 GROUP BY
     osm_id,
     geom,
     region;
 
-DROP TABLE _bikelanes_splitted;
+DROP TABLE _bikelanesQuantized;
 
 -- this line is only for making pg_tileserve display the table
 SELECT
-    UpdateGeometrySRID('boundary_stats', 'geom', 3857);
+    UpdateGeometrySRID('boundaryStats', 'geom', 3857);
 
+
+
+-- -- for `jsonb` with numeric values return a normalized object (all values sum to 100) which also includes the total value
+-- CREATE OR REPLACE FUNCTION atlas_NormalizeDistribution(dist jsonb)
+--     RETURNS jsonb
+--     AS $$
+-- DECLARE
+--     total numeric := sum(value)
+-- FROM (
+--     SELECT
+--         (jsonb_each(dist)).value::numeric) AS sq;
+-- BEGIN
+--     RETURN jsonb_object_agg(key, round((100 * value::numeric) / total, 2)) || jsonb_build_object('total', total)
+-- FROM (
+--     SELECT
+--         (jsonb_each(dist)).*) AS sq;
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
