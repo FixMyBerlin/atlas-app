038f7f676827fc8269bc04df6a0b143aa00b8dd8
Tobias <tobias@fixmycity.de>
2023-12-01 09:18:12 +0100

    .env: Fix types for environment variables
    Following https://dev.to/krzysztofzuraw/how-to-type-nextjs-env-variables-in-typescript-50cg

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,19 @@
-interface ImportMetaEnv {
-  readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
+namespace NodeJS {
+  interface ProcessEnv {
+    readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
 
-  readonly NEXT_PUBLIC_TILES_ENV: 'staging' | 'production' | 'development'
-  readonly NEXT_PUBLIC_API_ENV: 'staging' | 'production' | 'development'
-}
+    readonly NEXT_PUBLIC_TILES_ENV: 'staging' | 'production' | 'development'
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv
+    readonly NEXT_PUBLIC_APP_ORIGIN: string
+    readonly NEXT_PUBLIC_APP_ENV: 'staging' | 'production' | 'development'
+    readonly SESSION_SECRET_KEY: string
+
+    readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
+    readonly MAPBOX_PARKING_STYLE_ACCESS_TOKEN: `pk.${string}`
+
+    readonly NEXT_PUBLIC_TILES_ENV: 'staging' | 'production' | 'development'
+
+    readonly OSM_CLIENT_ID: string
+    readonly OSM_CLIENT_SECRET: string
+  }
 }
