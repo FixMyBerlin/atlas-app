b79b822199ec2b8b7e91ee1da457b2b75358632a
Sven König <sven@fixmycity.de>
2023-12-14 17:06:13 +0100

    remove unused code

--- scripts/MapboxStyles/util.ts
+++ scripts/MapboxStyles/util.ts
@@ -12,34 +12,3 @@ export const fetchStyle = async (key, url, folder) => {
 export async function saveJson(filename, data) {
   await Bun.write(filename, JSON.stringify(data, null, 2))
 }
-
-export const cleanString = (s) => s.replace(/[^\x00-\x7F]/g, '').replace(/[/\\?%*:|"<>]/g, '')
-
-export const checkIconIds = (styleData) => {
-  // removes non-ascii characters and characters not allowed in filenames from icon ids
-  styleData.layers.forEach((layer) => {
-    ;[
-      ['paint', 'background-pattern'],
-      ['paint', 'fill-pattern'],
-      ['paint', 'line-pattern'],
-      ['paint', 'fill-extrusion-pattern'],
-      ['layout', 'icon-image'],
-    ].forEach(([pl, prop]) => {
-      if (layer[pl]?.[prop]) {
-        const removeNonAscii = (obj, idx, indent = '') => {
-          if (typeof obj[idx] === 'string') {
-            if (obj[idx] !== cleanString(obj[idx])) {
-              throw Error(`"${obj[idx]}" is not a valid icon id.`)
-            }
-          } else if (Array.isArray(obj[idx])) {
-            const arr = obj[idx]
-            arr.forEach((v, i) => {
-              removeNonAscii(arr, i, indent + '  ')
-            })
-          }
-        }
-        removeNonAscii(layer[pl], prop)
-      }
-    })
-  })
-}
