9e26a09b1d8e93363f144aec8ee43d87e6215664
Sven König <sven@fixmycity.de>
2023-12-19 10:00:27 +0100

    add field exportPublic

---
+++ db/migrations/20231219083707_add_field_export_public/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Region" ADD COLUMN     "exportPublic" BOOLEAN NOT NULL DEFAULT false;
--- db/seeds/regions.ts
+++ db/seeds/regions.ts
@@ -5,34 +5,42 @@ const seedRegions = async () => {
     {
       slug: 'bibi',
       public: true,
+      exportPublic: true,
     },
     {
       slug: 'trto',
       public: true,
+      exportPublic: true,
     },
     {
       slug: 'berlin',
       public: false,
+      exportPublic: false,
     },
     {
       slug: 'nudafa',
       public: false,
+      exportPublic: false,
     },
     {
       slug: 'parkraum',
       public: true,
+      exportPublic: true,
     },
     {
       slug: 'rs8',
       public: false,
+      exportPublic: false,
     },
     {
       slug: 'woldegk',
       public: true,
+      exportPublic: false,
     },
     {
       slug: 'deutschland',
       public: false,
+      exportPublic: false,
     },
   ]
 
