ba29b500e55fae219acb2167702858c87330a290
Tobias <tobias@fixmycity.de>
2024-01-04 15:20:34 +0100

    PublicTransport: Add new style

--- src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/subcat_publicTransport.const.ts
+++ src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/subcat_publicTransport.const.ts
@@ -1,12 +1,14 @@
 import { FileMapDataSubcategory } from '../types'
 import { defaultStyleHidden } from './defaultStyle/defaultStyleHidden'
+import { mapboxStyleGroupLayers_atlas_ziele_plus_publictransport } from './mapboxStyles/groups/atlas_ziele_plus_publictransport'
 import { legacyMapboxStyleLayers } from './mapboxStyles/legacyMapboxStyleLayers'
+import { mapboxStyleLayers } from './mapboxStyles/mapboxStyleLayers'
 
 const tpoicId = 'publicTransport'
 const source = 'atlas_publicTransport'
 const sourceLayer = 'publicTransport'
 export type SubcatPublicTransportId = typeof tpoicId
-export type SubcatPublicTransportStyleIds = 'default'
+export type SubcatPublicTransportStyleIds = 'default' | 'default_legacy'
 
 export const subcat_publicTransport: FileMapDataSubcategory = {
   id: tpoicId,
@@ -20,6 +22,17 @@ export const subcat_publicTransport: FileMapDataSubcategory = {
       desc: null,
       layers: legacyMapboxStyleLayers({
         group: 'atlas_publictransport',
+        source,
+        sourceLayer,
+      }),
+    },
+    // TODO: We will likely have to rename this file to "subcat_poi_plus_publictransport"
+    {
+      id: 'default',
+      name: 'Standard',
+      desc: null,
+      layers: mapboxStyleLayers({
+        layers: mapboxStyleGroupLayers_atlas_ziele_plus_publictransport,
         source,
         sourceLayer,
       }),
