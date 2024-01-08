727c72485fb3a2d16a5ee7e071157e3de8705260
rush42 <laurenz@fixmycity.de>
2024-01-08 13:57:50 +0100

    allign order of function definitons with call order

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -17,47 +17,6 @@ import { prismaClientForRawQueries } from 'src/prisma-client'
       Create the post commit hook, tables and indexes that are required for our verification feature.
 */
 
-// specify license and attribution for data export
-const license = "'ODbL 1.0, https://opendatacommons.org/licenses/odbl/'"
-const attribution = "'OpenStreetMap, https://www.openstreetmap.org/copyright; Radverkehrsatlas.de'"
-
-async function initExportFunctions(tables) {
-  return Promise.all(
-    tables.map((tableName) => {
-      const functionName = exportFunctionIdentifier(tableName.toLowerCase())
-
-      return prismaClientForRawQueries.$transaction([
-        prismaClientForRawQueries.$executeRaw`SET search_path TO public;`,
-        prismaClientForRawQueries.$executeRawUnsafe(`
-        CREATE OR REPLACE FUNCTION public.${functionName}(region geometry)
-          RETURNS json
-          LANGUAGE sql
-          AS $function$
-          SELECT
-          json_build_object('type', 'FeatureCollection', 'license', ${license},
-              'attribution', ${attribution}, 'features', json_agg(features.feature))
-          FROM(
-              SELECT
-              jsonb_build_object('type', 'Feature', 'geometry',
-              ST_AsGeoJSON(ST_Transform(geom, 4326))::jsonb,
-              -- Reminder: All tables that can be exported are required to have those exact columns
-              'properties', jsonb_build_object('osm_id', inputs.osm_id) ||
-                  jsonb_build_object('osm_type', inputs.osm_type) || inputs.meta ||
-                  inputs.tags) AS feature
-              FROM(
-                  SELECT
-                      *
-                  FROM
-                    "${tableName}"
-                  WHERE
-                      ST_Intersects(ST_Transform(geom, 4326), region)) inputs) features;
-
-        $function$;`),
-      ])
-    }),
-  )
-}
-
 async function initVerificationViews(tables) {
   return Promise.all(
     tables.map((geometryTable) => {
@@ -129,6 +88,47 @@ async function initVerificationViews(tables) {
           FOR EACH ROW
           EXECUTE PROCEDURE public.atlas_update_${verifiedTable}();`,
         ),
+      ])
+    }),
+  )
+}
+
+// specify license and attribution for data export
+const license = "'ODbL 1.0, https://opendatacommons.org/licenses/odbl/'"
+const attribution = "'OpenStreetMap, https://www.openstreetmap.org/copyright; Radverkehrsatlas.de'"
+
+async function initExportFunctions(tables) {
+  return Promise.all(
+    tables.map((tableName) => {
+      const functionName = exportFunctionIdentifier(tableName.toLowerCase())
+
+      return prismaClientForRawQueries.$transaction([
+        prismaClientForRawQueries.$executeRaw`SET search_path TO public;`,
+        prismaClientForRawQueries.$executeRawUnsafe(`
+        CREATE OR REPLACE FUNCTION public.${functionName}(region geometry)
+          RETURNS json
+          LANGUAGE sql
+          AS $function$
+          SELECT
+          json_build_object('type', 'FeatureCollection', 'license', ${license},
+              'attribution', ${attribution}, 'features', json_agg(features.feature))
+          FROM(
+              SELECT
+              jsonb_build_object('type', 'Feature', 'geometry',
+              ST_AsGeoJSON(ST_Transform(geom, 4326))::jsonb,
+              -- Reminder: All tables that can be exported are required to have those exact columns
+              'properties', jsonb_build_object('osm_id', inputs.osm_id) ||
+                  jsonb_build_object('osm_type', inputs.osm_type) || inputs.meta ||
+                  inputs.tags) AS feature
+              FROM(
+                  SELECT
+                      *
+                  FROM
+                    "${tableName}"
+                  WHERE
+                      ST_Intersects(ST_Transform(geom, 4326), region)) inputs) features;
+
+        $function$;`),
       ])
     }),
   )
