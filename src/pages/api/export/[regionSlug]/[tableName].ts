20f13228632a1c45331922ded2afa28172849cdb
Sven König <sven@fixmycity.de>
2024-01-10 11:30:59 +0100

    export checks permissions

--- src/pages/api/export/[regionSlug]/[tableName].ts
+++ src/pages/api/export/[regionSlug]/[tableName].ts
@@ -1,10 +1,15 @@
+import db from 'db'
+import { prismaClientForRawQueries } from 'src/prisma-client'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { api } from 'src/blitz-server'
+import { getSession } from '@blitzjs/auth'
+
 import { isProd } from 'src/app/_components/utils/isEnv'
 import { exportApiIdentifier } from 'src/app/regionen/[regionSlug]/_mapData/mapDataSources/sources.const'
-import { prismaClientForRawQueries } from 'src/prisma-client'
 import { z } from 'zod'
 
 const ExportSchema = z.object({
+  regionSlug: z.string(),
   tableName: z.enum(exportApiIdentifier),
   minlon: z.coerce.number().default(13.3),
   minlat: z.coerce.number().default(52.2),
@@ -20,6 +25,67 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!isProd) throw e
     res.status(400).send('Bad Request')
     return
+  }
+
+  // calling an anonymous function to easily break out of nested ifs
+  const status = await (async () => {
+    const { regionSlug } = params
+
+    if (true) {
+      // TODO: check master export key
+    }
+
+    let exportPublic: boolean
+    if (regionSlug === 'all') {
+      exportPublic = false
+    } else {
+      const region = await db.region.findFirst({ where: { slug: regionSlug } })
+      if (!region) {
+        return 404 // <==========
+      } else {
+        exportPublic = region.exportPublic
+      }
+    }
+
+    if (exportPublic) {
+      return 200 // <==========
+    }
+
+    // exportPublic === false
+
+    await api(() => null)
+    const session = await getSession(req, res)
+    const { userId, role } = session
+
+    if (!userId) {
+      return 403 // <==========
+    }
+
+    if (role === 'ADMIN') {
+      return 200 // <==========
+    }
+
+    if (regionSlug === 'all') {
+      // because role !== "ADMIN"
+      return 403 // <==========
+    }
+
+    const membershipExists = !!(await db.membership.count({
+      where: { userId: userId!, region: { slug: regionSlug } },
+    }))
+    if (!membershipExists) {
+      return 403 // <==========
+    }
+
+    if (true) {
+      // TODO: check bounding box
+    }
+
+    return 200 // <==========
+  })()
+
+  if (status !== 200) {
+    res.status(status).send(status)
   }
 
   try {
