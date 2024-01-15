8a8713773f6a74b4e067160b817a847f6608ed21
rush42 <laurenz@fixmycity.de>
2024-01-15 19:03:38 +0100

    transform polygon instead of geom column to make use of the gist index

--- app/src/instrumentation/initExportFunctions.ts
+++ app/src/instrumentation/initExportFunctions.ts
@@ -16,7 +16,7 @@ export async function initExportFunctions(tables: typeof exportApiIdentifier) {
       return prismaClientForRawQueries.$transaction([
         prismaClientForRawQueries.$executeRaw`SET search_path TO public;`,
         prismaClientForRawQueries.$executeRawUnsafe(
-          `CREATE OR REPLACE FUNCTION public.${functionName}(region geometry)
+          `CREATE OR REPLACE FUNCTION public.${functionName}(region Geometry(Polygon))
           RETURNS json
           LANGUAGE sql
           AS $function$
@@ -28,21 +28,23 @@ export async function initExportFunctions(tables: typeof exportApiIdentifier) {
             'features', json_agg(features.feature)
           )
           FROM(
-              SELECT
-              jsonb_build_object('type', 'Feature', 'geometry',
-              ST_AsGeoJSON(ST_Transform(geom, 4326))::jsonb,
+            SELECT
+              json_build_object(
+                'type', 'Feature',
+                'geometry', ST_AsGeoJSON(ST_Transform(geom, 4326), 3)::json,
               -- Reminder: All tables that can be exported are required to have those exact columns
               'id', inputs.id,
               'properties', jsonb_build_object('osm_id', inputs.osm_id) ||
                   jsonb_build_object('osm_type', inputs.osm_type) || inputs.meta ||
                   inputs.tags) AS feature
               FROM(
-                  SELECT
-                      *
-                  FROM
-                    "${tableName}"
-                  WHERE
-                      ST_Transform(geom, 4326) && region) inputs) features;
+                SELECT
+                    *
+                FROM
+                  "${tableName}"
+                WHERE
+                    geom && ST_Transform(region, 3857)) inputs
+            ) features;
 
         $function$;`,
         ),
