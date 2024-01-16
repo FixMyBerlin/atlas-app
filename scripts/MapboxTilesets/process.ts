babf08f83164906b61c03263e64acbf07d408c86
Tobias <tobias@fixmycity.de>
2024-01-16 17:28:16 +0100

    Update tilesets: Fix after updated export api
    - the `all` region was simplified in 3ba10f31
    - the try-catch does not work anymore so now the bbox is part of the config so we can use a smaller bbox for 'roads' to start with (roads returns a 502 bad gateway, no idea why the try catch does not work here…)
    - the folder is opened right away so we can start updating files while others are downloaded

--- scripts/MapboxTilesets/process.ts
+++ scripts/MapboxTilesets/process.ts
@@ -7,93 +7,104 @@ import { SourceExportApiIdentifier } from 'src/app/regionen/[regionSlug]/_mapDat
 console.log(chalk.inverse.bold('START'), __filename)
 
 // Configruation:
-const datasets: Record<SourceExportApiIdentifier, { sourceLayer: string; uploadUrl: string }> = {
+// BBox: Brandenburg
+const bboxBrandenburg = {
+  min: [11.2662278, 51.359064],
+  max: [14.7658159, 53.5590907],
+} satisfies StaticRegion['bbox']
+// BBox: A bit of Berlin and rual areas below (NUDAFA)
+const bboxBerlinPlus = {
+  min: [13.2809, 52.2095],
+  max: [13.825, 52.5528],
+} satisfies StaticRegion['bbox']
+// Datasets:
+const datasets: Record<
+  SourceExportApiIdentifier,
+  { sourceLayer: string; uploadUrl: string; bbox: NonNullable<StaticRegion['bbox']> }
+> = {
   bicycleParking_points: {
     sourceLayer: 'bicycleParking_points',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.c65ckqta/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   bicycleParking_areas: {
     sourceLayer: 'bicycleParking_areas',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0p8hdib5/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   bikelanes_verified: {
     sourceLayer: 'bikelanes_verified',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.3i25sspf/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   places: {
     sourceLayer: 'tarmac-places-test-datensatz-5dodjb',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.34e901wi/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   poiClassification: {
     sourceLayer: 'poiClassification',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0wsdyx91/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   publicTransport: {
     sourceLayer: 'publicTransport',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.7fmhzgpi/#14/52.51622/13.39233',
-  },
-  roads: {
-    sourceLayer: 'roads',
-    uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0f7p6nhx/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   trafficSigns: {
     sourceLayer: 'atlas-trafficSigns-attp60',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.8trg6z1b/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   landuse: {
     sourceLayer: 'tarmac-landuse-test-datensatz-0r36t3',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.8xhph4k4/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   barrierAreas: {
     sourceLayer: 'atlas-barrierAreas-b9mb1o',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.3m06nhmv/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   barrierLines: {
     sourceLayer: 'atlas-barrierLines-3ft1bu',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.cx602vbw/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
   },
   boundaries: {
     sourceLayer: 'atlas-boundaries-66z5h4',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.4u44tl11/#14/52.51622/13.37036',
+    bbox: bboxBrandenburg,
+  },
+  roads: {
+    sourceLayer: 'roads',
+    uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0f7p6nhx/#14/52.51622/13.37036',
+    bbox: bboxBerlinPlus,
   },
 }
-
-// BBox: Brandenburg
-const _bboxBrandenburg = {
-  min: [11.2662278, 51.359064],
-  max: [14.7658159, 53.5590907],
-} satisfies StaticRegion['bbox']
-const bbox = _bboxBrandenburg
-// BBox: A bit of Berlin and rual areas below (NUDAFA)
-const fallbackBbox = {
-  min: [13.2809, 52.2095],
-  max: [13.825, 52.5528],
-} satisfies StaticRegion['bbox']
 
 // Folder
 const folderJson = 'scripts/MapboxTilesets/json'
 const folderMbtiles = 'scripts/MapboxTilesets/mbtiles'
 
+console.log(
+  chalk.inverse.bold('INFO'),
+  'Opening mbtiles folder so you may "replace" the Mapbox tilesets in the browser.',
+)
+Bun.spawnSync(['open', folderMbtiles])
+
 // Fetch, Write, Transform
 for (const dataset of Object.entries(datasets)) {
   const datasetKey = dataset[0] as SourceExportApiIdentifier
-  const { sourceLayer, uploadUrl } = dataset[1]
+  const { sourceLayer, uploadUrl, bbox } = dataset[1]
 
   // Fetch Export API
   const accessToken = process.env.EXPORT_ACCESS_TOKEN
-  // const url = getExportApiBboxUrl('all', datasetKey, bbox, 'production')
-  const url = getExportApiBboxUrl('all', datasetKey, bbox, 'staging', accessToken)
+  // The `accessToken` with skip the region check (hence the `noRegion`)
+  const url = getExportApiBboxUrl('noRegion', datasetKey, bbox, 'staging', accessToken)
   console.log('\n', chalk.inverse.bold(chalk.yellow('FETCH')), url)
-  let fetchExport: Awaited<ReturnType<typeof fetch>> | undefined = undefined
-  try {
-    fetchExport = await fetch(url)
-  } catch (error) {
-    // const url = getExportApiBboxUrl('all', datasetKey, fallbackBbox, 'production')
-    const url = getExportApiBboxUrl('all', datasetKey, fallbackBbox, 'staging', accessToken)
-    console.log(chalk.inverse.bold('ERROR'), error, 'trying the fallback bbox now', url)
-    fetchExport = await fetch(url)
-  }
-  if (fetchExport === undefined) continue
+  const fetchExport = await fetch(url)
 
   if (!fetchExport.ok) {
     console.error('Fetch failed', fetchExport)
@@ -145,9 +156,4 @@ for (const dataset of Object.entries(datasets)) {
   }
 }
 
-console.log(
-  '\n',
-  chalk.inverse.bold('DONE'),
-  'Opening mbtiles folder so you may "replace" the Mapbox tilesets in the browser.',
-)
-Bun.spawnSync(['open', folderMbtiles])
+console.log('\n', chalk.inverse.bold('DONE'))
