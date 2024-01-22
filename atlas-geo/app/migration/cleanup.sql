b7c402114ffeb8278bebcb3d5b178f49d4d1fce5
Tobias <tobias@fixmycity.de>
2024-01-22 16:11:17 +0100

    Cleanup: Remove already deleted table

--- atlas-geo/app/migration/cleanup.sql
+++ atlas-geo/app/migration/cleanup.sql
@@ -1,4 +1,5 @@
 -- Learn more about this file in ./README.md
 -- (!) Cleanup the file only AFTER the cleanup was done on production
-
-DROP TABLE IF EXISTS "metadata";
+-- DROP TABLE IF EXISTS "metadata";
+--
+--
