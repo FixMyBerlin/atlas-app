7b48cedbf712155ea4081417205c8f4f996557a3
Sven König <sven@fixmycity.de>
2024-01-23 13:13:20 +0100

    get nudafa-combined from /api/pmtiles

--- src/app/regionen/[regionSlug]/_mapData/mapDataSources/sourcesDatasets/utils/sourceDatasetIdUrl.ts
+++ src/app/regionen/[regionSlug]/_mapData/mapDataSources/sourcesDatasets/utils/sourceDatasetIdUrl.ts
@@ -4,5 +4,11 @@ import { DatasetIds } from '../../datasets/types'
 
 export const sourceDatasetIdUrl = (datasetId: DatasetIds) => {
   invariant(datasets[datasetId], 'Dataset missing')
-  return { id: datasetId, url: `pmtiles://${datasets[datasetId]}` }
+  let url = datasets[datasetId] as string
+  if (url.startsWith('private/')) {
+    const origin = process.env.NEXT_PUBLIC_APP_ORIGIN
+    const slug = url.split('/')[1]
+    url = `${origin}/api/pmtiles/${slug}`
+  }
+  return { id: datasetId, url: `pmtiles://${url}` }
 }
