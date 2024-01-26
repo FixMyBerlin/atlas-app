c6a1db75e1d0aec300a0c962145a5762aa5c2054
Sven König <sven@fixmycity.de>
2024-01-26 15:32:32 +0100

    check permission in /api/uploads/[slug]

--- src/app/api/uploads/[slug]/route.ts
+++ src/app/api/uploads/[slug]/route.ts
@@ -1,13 +1,35 @@
 import db from 'db'
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { GetObjectCommandInput } from '@aws-sdk/client-s3/dist-types/commands/GetObjectCommand'
+import { getBlitzContext } from '@blitzjs/auth'
 
 export async function GET(request: Request, { params }: { params: { slug: string } }) {
   const { slug } = params
 
-  const upload = await db.upload.findFirst({ where: { slug } })
+  const upload = await db.upload.findFirst({
+    where: { slug },
+    include: { regions: { select: { id: true } } },
+  })
   if (upload === null) {
     return Response.json({ statusText: 'Not Found' }, { status: 404 })
+  }
+
+  if (!upload.public) {
+    const forbidden = Response.json({ statusText: 'Forbidden' }, { status: 403 })
+    const session = (await getBlitzContext()).session
+    if (!session.userId) {
+      return forbidden
+    }
+    if (session.role !== 'ADMIN') {
+      // user must be a member in one or more regions the upload is related to
+      const regionIds = upload!.regions.map((region) => region.id)
+      const membershipExists = !!(await db.membership.count({
+        where: { userId: session.userId!, region: { id: { in: regionIds } } },
+      }))
+      if (!membershipExists) {
+        return forbidden
+      }
+    }
   }
 
   const { hostname, pathname } = new URL(upload!.externalUrl)
