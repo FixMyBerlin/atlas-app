bb1937fddb1337229dbe079c1388bde3043ce080
Sven König <sven@fixmycity.de>
2024-01-26 16:23:51 +0100

    add missing resolver.authorize('ADMIN')

--- src/uploads/queries/getUploadWithRegions.ts
+++ src/uploads/queries/getUploadWithRegions.ts
@@ -2,9 +2,13 @@ import { resolver } from '@blitzjs/rpc'
 import db from 'db'
 import { GetUploadSchema } from '../schema'
 
-export default resolver.pipe(resolver.zod(GetUploadSchema), async ({ slug }) => {
-  return await db.upload.findFirstOrThrow({
-    where: { slug },
-    include: { regions: { select: { id: true } } },
-  })
-})
+export default resolver.pipe(
+  resolver.zod(GetUploadSchema),
+  resolver.authorize('ADMIN'),
+  async ({ slug }) => {
+    return await db.upload.findFirstOrThrow({
+      where: { slug },
+      include: { regions: { select: { id: true } } },
+    })
+  },
+)
