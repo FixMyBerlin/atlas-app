59e2b145d0fa5e254de5f5b404842adc4fc2e63f
Tobias <tobias@fixmycity.de>
2024-01-29 11:21:58 +0100

    Env: Add types, update documentation comments
    Also adds a reminder to the example to keep those files in sync.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,37 @@
 namespace NodeJS {
   interface ProcessEnv {
-    readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
-
-    readonly NEXT_PUBLIC_TILES_ENV: 'staging' | 'production' | 'development'
-
+    // NEXTJS
     readonly NEXT_PUBLIC_APP_ORIGIN: string
     readonly NEXT_PUBLIC_APP_ENV: 'staging' | 'production' | 'development'
     readonly SESSION_SECRET_KEY: string
+    // MAP API KEYS
 
     readonly MAPBOX_STYLE_ACCESS_TOKEN: `pk.${string}`
     readonly MAPBOX_PARKING_STYLE_ACCESS_TOKEN: `pk.${string}`
-
+    // DEVELOPMENT
     readonly NEXT_PUBLIC_TILES_ENV: 'staging' | 'production' | 'development'
+    // PRISMA
 
+    readonly DB_HOST: never
+    readonly DB_PORT: never
+    readonly DB_USER: never
+    readonly DB_PASSWORD: never
+    readonly DB_DATABASE: never
+    readonly DATABASE_URL: never
+    // LOGIN
     readonly OSM_CLIENT_ID: string
     readonly OSM_CLIENT_SECRET: string
+    // API
+    readonly EXPORT_ACCESS_TOKEN: string
+    // StaticDatasets
+    readonly S3_PMTILES_KEY: string
+    readonly S3_PMTILES_SECRET: string
+    readonly S3_PMTILES_REGION: 'eu-central-1'
+    readonly API_ROOT_URL:
+      | 'http://127.0.0.1:5173/api'
+      | 'https://staging.radverkehrsatlas.de/api'
+      | 'https://radverkehrsatlas.de/api'
+    readonly S3_PMTILES_BUCKET: string
+    readonly S3_PMTILES_FOLDER: production | staging | localdev
   }
 }
