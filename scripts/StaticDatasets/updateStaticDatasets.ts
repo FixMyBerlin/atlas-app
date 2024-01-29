4b6f898ff66ca409e2dc81590d3c9cd20ce9aaea
Tobias <tobias@fixmycity.de>
2024-01-29 14:19:53 +0100

    StaticDatasets: Fix tippecanoe config
    We need to use the same config that we used before, otherwise the generated styles have errors.

--- scripts/StaticDatasets/updateStaticDatasets.ts
+++ scripts/StaticDatasets/updateStaticDatasets.ts
@@ -16,18 +16,31 @@ const existingRegionSlugs = regions.map((region) => region.slug)
 const existingUploadSlugs = await getSlugs(getUploadsUrl)
 
 const generatePMTilesFile = (inputFile, outputFile) => {
-  Bun.spawnSync(['tippecanoe', `--output=${outputFile}`, '--force', '--layer=default', inputFile], {
-    onExit(_proc, exitCode, _signalCode, error) {
-      if (exitCode) {
-        red(`  exitCode: ${exitCode}`)
-        process.exit(1)
-      }
-      if (error) {
-        red(`  error: ${error.message}`)
-        process.exit(1)
-      }
+  Bun.spawnSync(
+    [
+      'tippecanoe',
+      `--output=${outputFile}`,
+      '--force',
+      '--maximum-zoom=g', // Automatically choose a maxzoom that should be sufficient to clearly distinguish the features and the detail within each feature https://github.com/felt/tippecanoe#zoom-levels
+      '-rg', // If you use -rg, it will guess a drop rate that will keep at most 50,000 features in the densest tile https://github.com/felt/tippecanoe#dropping-a-fixed-fraction-of-features-by-zoom-level
+      '--drop-densest-as-needed', // https://github.com/felt/tippecanoe?tab=readme-ov-file#dropping-a-fraction-of-features-to-keep-under-tile-size-limits
+      '--extend-zooms-if-still-dropping', // https://github.com/felt/tippecanoe?tab=readme-ov-file#zoom-levels
+      '--layer=default',
+      inputFile,
+    ],
+    {
+      onExit(_proc, exitCode, _signalCode, error) {
+        if (exitCode) {
+          red(`  exitCode: ${exitCode}`)
+          process.exit(1)
+        }
+        if (error) {
+          red(`  error: ${error.message}`)
+          process.exit(1)
+        }
+      },
     },
-  })
+  )
 }
 
 const uploadFileToS3 = async (fileToUpload, filename) => {
