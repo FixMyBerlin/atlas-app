b89781961611f7f85a11d832ed9dc7facabf69f9
Tobias <tobias@fixmycity.de>
2024-01-30 13:54:31 +0100

    Datasets: Remove cleanupMapboxCopySelectedLayerJson
    In preparation of migrating those layers to a external file, we should remove this helper, which only adds complexity

--- src/app/regionen/[regionSlug]/_mapData/mapDataSources/sourcesDatasets/utils/cleanupMapboxCopySelectedLayerJson.ts
+++
@@ -1,7 +0,0 @@
-// We can copy layer from Mapbox Studio, but they need to be cleaned up…
-export const cleanupMapboxCopySelectedLayerJson = (layer: any) => {
-  const clean = layer[0]
-  delete clean.source
-  delete clean['source-layer']
-  return clean
-}
