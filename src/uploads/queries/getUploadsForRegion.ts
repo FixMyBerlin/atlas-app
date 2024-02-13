760ba118d841cd97b11a1b908c10106edc1779ff
Sven König <sven@de-selby.com>
2024-02-13 16:48:15 +0100

    getUploadsForRegion returns only uploads the user has permission for

--- src/uploads/queries/getUploadsForRegion.ts
+++ src/uploads/queries/getUploadsForRegion.ts
@@ -6,13 +6,24 @@ const Schema = z.object({
   regionSlug: z.string(),
 })
 
-export default resolver.pipe(
-  resolver.zod(Schema),
-  resolver.authorize('ADMIN'),
-  async ({ regionSlug }) => {
-    return db.upload.findMany({
-      where: { regions: { some: { slug: regionSlug } } },
-      include: { regions: { select: { slug: true } } },
-    })
-  },
-)
+export default resolver.pipe(resolver.zod(Schema), async ({ regionSlug }, ctx) => {
+  const { session } = ctx
+
+  const uploads = await db.upload.findMany({
+    where: { regions: { some: { slug: regionSlug } } },
+    include: { regions: { select: { id: true, slug: true } } },
+  })
+
+  if (!session.userId) {
+    return uploads.filter((upload) => upload.public)
+  } else if (session.role === 'ADMIN') {
+    return uploads
+  } else {
+    const memberships = await db.membership.findMany({ where: { userId: session.userId } })
+    const membershipRegionIds = memberships.map((membership) => membership.regionId)
+    return uploads.filter(
+      (upload) =>
+        upload.public || upload.regions.some((region) => membershipRegionIds.includes(region.id)),
+    )
+  }
+})
