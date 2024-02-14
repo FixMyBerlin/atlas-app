167244a5917a4d50e097391d0fd51465a4f4ba7a
Tobias <tobias@fixmycity.de>
2024-02-14 22:10:49 +0100

    Datasets: Improve seeds

--- db/seeds/pmtiles.ts
+++ db/seeds/pmtiles.ts
@@ -1,9 +1,9 @@
 import { Prisma } from '@prisma/client'
-import { defaultLineLayerStyles } from 'scripts/StaticDatasets/utils/defaultLayerStyles'
+import { defaultLineLayerStyles } from 'scripts/StaticDatasets/geojson/_utils/defaultLayerStyles'
 import db from '../index'
 
 const seedUploads = async () => {
-  const seedUploads: Prisma.UploadUncheckedCreateInput[] = [
+  const seedUploadsNudafa: Prisma.UploadUncheckedCreateInput[] = [
     {
       slug: 'nudafa-combined',
       pmtilesUrl:
@@ -11,9 +11,9 @@ const seedUploads = async () => {
       configs: [
         {
           name: 'Zielnetz Stand 22.11.2023',
-          subId: 'zielnetz',
-          type: 'vector',
+          category: null,
           attributionHtml: '',
+          type: 'vector',
           inspector: {
             enabled: true,
             highlightingKey: 'TODO',
@@ -28,16 +28,60 @@ const seedUploads = async () => {
     },
   ]
 
-  const region = await db.region.findFirstOrThrow({ where: { slug: 'nudafa' } })
-  for (const data of seedUploads) {
-    if (data) {
-      await db.upload.create({
-        data: {
-          ...data,
-          regions: { connect: { id: region.id } },
+  const seedUploadsBibi: Prisma.UploadUncheckedCreateInput[] = [
+    {
+      slug: 'two-configs',
+      pmtilesUrl:
+        'https://atlas-private.s3.eu-central-1.amazonaws.com/test-data/nudafa-combined.pmtiles',
+      configs: [
+        {
+          name: 'two-configs config 1',
+          subId: 'config1',
+          attributionHtml: 'Lorem Ipsum',
+          type: 'vector',
+          inspector: {
+            enabled: true,
+            highlightingKey: 'TODO',
+            documentedKeys: false,
+            disableTranslations: true,
+          },
+          layers: defaultLineLayerStyles({}),
         },
-      })
-    }
+        {
+          name: 'two configs, config 2',
+          subId: 'config2',
+          type: 'vector',
+          attributionHtml: 'Lorem Ipsum',
+          inspector: {
+            enabled: true,
+            highlightingKey: 'TODO',
+            documentedKeys: false,
+            disableTranslations: true,
+          },
+          layers: defaultLineLayerStyles({}),
+        },
+      ],
+    },
+  ]
+
+  const regionNudafa = await db.region.findFirstOrThrow({ where: { slug: 'nudafa' } })
+  for (const data of seedUploadsNudafa) {
+    await db.upload.create({
+      data: {
+        ...data,
+        regions: { connect: { id: regionNudafa.id } },
+      },
+    })
+  }
+
+  const regionBibi = await db.region.findFirstOrThrow({ where: { slug: 'bibi' } })
+  for (const data of seedUploadsBibi) {
+    await db.upload.create({
+      data: {
+        ...data,
+        regions: { connect: { id: regionBibi.id } },
+      },
+    })
   }
 }
 
