2858ecc443bf09bebf64c535602e97f89dc815bb
Tobias <tobias@fixmycity.de>
2024-02-19 10:17:17 +0100

    Revert "TEMP: disable processing"
    This reverts commit 52b44179d8eafdc7fc844a8059801c20d68d5e32.

--- atlas-geo/atlas-geo/app/run.sh
+++ atlas-geo/atlas-geo/app/run.sh
@@ -26,33 +26,33 @@ alert() {
   curl -X POST $url -d "payload=$payload" --silent --output "/dev/null"
 }
 
-# if ! ./run-1-download.sh; then
-#     alert '*ERROR*: #run-1-download exited with non-zero status code'
-# fi
+if ! ./run-1-download.sh; then
+    alert '*ERROR*: #run-1-download exited with non-zero status code'
+fi
 
-# if ! ./run-2-filter.sh; then
-#     alert '*ERROR*: #run-2-filter exited with non-zero status code'
-# fi
+if ! ./run-2-filter.sh; then
+    alert '*ERROR*: #run-2-filter exited with non-zero status code'
+fi
 
-# if ! ./run-3-migration.sh; then
-#     alert '*ERROR*: #run-3-migration exited with non-zero status code'
-# fi
+if ! ./run-3-migration.sh; then
+    alert '*ERROR*: #run-3-migration exited with non-zero status code'
+fi
 
-# process_start_time=$(date +%s)
-# if ! ./run-4-process.sh; then
-#     alert '*ERROR*: #run-4-process exited with non-zero status code'
-# fi
-# process_end_time=$(date +%s)
-# export PROCESS_RUN_TIME_DIFF=$((process_end_time - process_start_time)) # used by metadata.sh
+process_start_time=$(date +%s)
+if ! ./run-4-process.sh; then
+    alert '*ERROR*: #run-4-process exited with non-zero status code'
+fi
+process_end_time=$(date +%s)
+export PROCESS_RUN_TIME_DIFF=$((process_end_time - process_start_time)) # used by metadata.sh
 
-# if ! ./run-5-postprocess.sh; then
-#     alert '*ERROR*: #run-5-postprocess exited with non-zero status code'
-# fi
+if ! ./run-5-postprocess.sh; then
+    alert '*ERROR*: #run-5-postprocess exited with non-zero status code'
+fi
 
-# if ! ./run-6-analysis.sh; then
-#     alert '*ERROR*: #run-6-analysis exited with non-zero status code'
-# fi
+if ! ./run-6-analysis.sh; then
+    alert '*ERROR*: #run-6-analysis exited with non-zero status code'
+fi
 
-# if ! ./run-7-metadata.sh; then
-#     alert '*ERROR*: #run-7-metadata exited with non-zero status code'
-# fi
+if ! ./run-7-metadata.sh; then
+    alert '*ERROR*: #run-7-metadata exited with non-zero status code'
+fi
