f2b83415a3647c8884836ef71c9f226e08995db2
Sven König <sven@de-selby.com>
2024-02-20 16:08:22 +0100

    disable deleting upload

--- src/app/admin/uploads/_components/UploadTable.tsx
+++ src/app/admin/uploads/_components/UploadTable.tsx
@@ -1,23 +1,14 @@
 'use client'
 
-import { useMutation } from '@blitzjs/rpc'
-import { useRouter } from 'next/navigation'
-import { Fragment } from 'react'
 import { MetaData } from 'scripts/StaticDatasets/types'
 import { Link } from 'src/app/_components/links/Link'
-import { linkStyles } from 'src/app/_components/links/styles'
 import { Pill } from 'src/app/_components/text/Pill'
-import deleteUpload from 'src/uploads/mutations/deleteUpload'
 import { TUpload } from 'src/uploads/queries/getUploads'
 import { AdminTable } from '../../_components/AdminTable'
-import { ObjectDump } from '../../_components/ObjectDump'
 
 export const UploadsTable = ({ uploads }: { uploads: TUpload[] }) => {
-  const router = useRouter()
-  const [deleteUploadMutation] = useMutation(deleteUpload)
-
   return (
-    <AdminTable header={['Slug', 'Zugriff', 'Regionen', 'Ansichten', '', '']}>
+    <AdminTable header={['Slug', 'Zugriff', 'Regionen', 'Ansichten', '']}>
       {uploads.map((upload) => {
         return (
           <tr key={upload.id}>
@@ -50,25 +41,6 @@ export const UploadsTable = ({ uploads }: { uploads: TUpload[] }) => {
                     )
                   })}
               </ul>
-            </td>
-            <td>
-              <button
-                type="button"
-                onClick={async () => {
-                  if (window.confirm(`»${upload.slug}« wirklich unwiderruflich löschen?`)) {
-                    try {
-                      await deleteUploadMutation({ id: upload.id })
-                      router.refresh()
-                    } catch (error: any) {
-                      window.alert(error.toString())
-                      console.error(error)
-                    }
-                  }
-                }}
-                className={linkStyles}
-              >
-                Löschen
-              </button>
             </td>
             <td>
               <Link href={`/admin/uploads/${upload.slug}`}>Details</Link>
