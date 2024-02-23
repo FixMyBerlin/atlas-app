853de88a4533fc08c7c01f223a3706b69080dbed
Tobias <tobias@fixmycity.de>
2024-02-23 14:03:12 +0100

    Bikeroutes: Sanitize and document all fields

--- atlas-geo/app/process/roads_bikelanes/bikeroutes/Bikeroutes.lua
+++ atlas-geo/app/process/roads_bikelanes/bikeroutes/Bikeroutes.lua
@@ -11,16 +11,46 @@ end
 ---@return table
 -- given an osm relation
 function Bikeroutes(tags)
-  local networkTypes = Set({ 'lcn', 'rcn', 'ncn', 'icn' })
-  local network_tags_cc = { 'name', 'ref', 'operator', 'cycle_network', 'colour', 'description', 'osmc:symbol',
-    'roundtrip', 'website', 'symbol', 'network:type' }
-  local results = {}
-  results.network = Sanitize(tags.network, networkTypes)
-  results.distance = tonumber(tags.distance)
-  -- we only want cycle_highway = yes. `nil` means no
-  results.cycle_highway = Sanitize(tags.network, Set({ 'yes' }))
-  CopyTags(results, tags, network_tags_cc)
-  return results
+  local result_tags = {}
+
+  -- Supposed to be 'km', always. TODO: Ideally modify ParseLength to handle different default units.
+  result_tags.distance = tonumber(tags.distance)
+  result_tags.network = Sanitize(tags.network, Set({ 'lcn', 'rcn', 'ncn', 'icn' }))
+  -- "Radschnellverbindungen", 15x https://taginfo.geofabrik.de/europe:germany/keys/cycle_highway#values
+  -- We only allow `yes`, `nil`+`no` are both `no`
+  -- Map: https://overpass-turbo.eu/?w=%22cycle_highway%22%3D%22yes%22+in+Germany&R
+  result_tags.cycle_highway = Sanitize(tags.cycle_highway, Set({ 'yes' }))
+  -- Only yes/no are relevant https://taginfo.geofabrik.de/europe:germany/keys/roundtrip#values
+  result_tags.roundtrip = Sanitize(tags.routndtrip, Set({ 'yes', 'no' }))
+  -- Only those two are relevant https://taginfo.geofabrik.de/europe:germany/keys/network%3Atype#values
+  result_tags.network_type = Sanitize(tags['network:type'], Set({ 'node_network', 'basic_network' }))
+
+  -- free form text, mostly with cryptic key https://taginfo.geofabrik.de/europe:germany/keys/cycle_network#values
+  result_tags.cycle_network_key = tags.cycle_network
+  -- free form text, https://taginfo.geofabrik.de/europe:germany/keys/symbol#values
+  result_tags.symbol_description = tags.symbol
+  -- free form text with route stops
+  result_tags.route_description = tags.description or tags['description:de']
+  -- rename to plural since it can be multipe colors. Cleanup tags that use a non standard separator
+  -- https://taginfo.geofabrik.de/europe:germany/keys/colour#values
+  -- (Reminder: Taginfo doesn not just show colour on relations but all usage; Use the "Overpass turbo" link and change query to `relation`)
+  if (tags.colour ~= nil) then
+    result_tags.colours = string.gsub(tags.colour or "", ";", "/")
+    result_tags.colours = string.gsub(result_tags.colour or "", ";", "-")
+  end
+
+  local tags_copied = {
+    'name',
+    'ref',
+    'operator',
+    'website',
+    -- 'wikdata', -- not needed for now
+    'wikipedia',
+    -- Maybe later add "from", "to", "via" used by cycle_highways (in DE:BW, FRM1)
+  }
+  CopyTags(result_tags, tags, tags_copied)
+
+  return result_tags
 end
 
 ---@param wayRouteMapping table
