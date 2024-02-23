01d79b9c5321d4c503ef1a7142c0de62fde26bfa
Sven König <sven@de-selby.com>
2024-02-23 14:29:30 +0100

    refactor coloring

--- cacheWarming/util.js
+++ cacheWarming/util.js
@@ -8,54 +8,50 @@ export function lng2tile(lng, zoom) {
 export function lat2tile(lat, zoom) {
   return Math.floor(
     ((1 -
-        Math.log(Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)) / Math.PI) /
+      Math.log(Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)) / Math.PI) /
       2) *
-    Math.pow(2, zoom),
+      Math.pow(2, zoom),
   )
 }
 
+function colorString(v, s, colorTable) {
+  for (let i in colorTable) {
+    const [treshold, [r, g, b]] = colorTable[i]
+    if (v >= treshold) {
+      return chalk.rgb(r, g, b)(s)
+    }
+  }
+  return s
+}
+
 export function formatDuration(ms) {
-  const s = ms / 1000
-  const secondsToColor = [
+  const seconds = ms / 1000
+  const formatted = `${seconds.toFixed(3)}s`
+  return colorString(seconds, formatted, [
     [120, [255, 0, 0]],
     [60, [255, 64, 0]],
     [30, [255, 128, 0]],
     [10, [255, 192, 0]],
     [5, [255, 255, 0]],
-  ]
-  let formatted = `${s}s`
-  secondsToColor.forEach(([seconds, color]) => {
-    if (s >= seconds) {
-      formatted = chalk.rgb(...color)(formatted)
-      return false
-    }
-  })
-  return formatted
+  ])
 }
 
 // https://stackoverflow.com/a/39906526
 const units = ['B', 'K', 'M', 'G']
-export function formatBytes(b) {
-  let l = 0,
-    n = parseInt(b, 10) || 0
+export function formatBytes(bytes) {
+  let l = 0
+  let n = parseInt(bytes, 10) || 0
   while (n >= 1024 && ++l) {
     n = n / 1024
   }
   let formatted = n.toFixed(n < 10 && l > 0 ? 1 : 0) + units[l]
-  const bytesToColor = [
+  if (bytes === 0) {
+    return chalk.red(formatted)
+  }
+  return colorString(bytes, formatted, [
     [2500000, [255, 0, 0]],
     [1250000, [255, 128, 0]],
     [500000, [255, 192, 0]],
     [250000, [255, 255, 0]],
-  ]
-  bytesToColor.forEach(([bytes, color]) => {
-    if (b >= bytes) {
-      formatted = chalk.rgb(...color)(formatted)
-      return false
-    }
-  })
-  if (b === 0) {
-    formatted = chalk.red(formatted)
-  }
-  return formatted
+  ])
 }
