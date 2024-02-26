7960dac2d3589a8862efe6f003962734cf10ca68
Tobias <tobias@fixmycity.de>
2024-02-26 15:28:11 +0100

    Inspector: Improve the iD Editor Links
    Mainly add pre defined hashtags

--- src/app/regionen/[regionSlug]/_components/SidebarInspector/Tools/osmUrls/osmUrls.tsx
+++ src/app/regionen/[regionSlug]/_components/SidebarInspector/Tools/osmUrls/osmUrls.tsx
@@ -14,13 +14,13 @@ export const osmEditIdUrl = (type: OsmShortType, id: number | string) => {
   if (!type || (type && !longOsmType[type])) return undefined
 
   // return `https://www.openstreetmap.org/edit?${longOsmType[type]}=${id}`
-  return `https://pr-1137--ideditor-presets-preview.netlify.app/id/dist/#background=Brandenburg-DOP20c&disable_features=boundaries&id=${type.toLocaleLowerCase()}${id}&locale=de`
+  return `https://pr-1137--ideditor-presets-preview.netlify.app/id/dist/#id=${type.toLocaleLowerCase()}${id}&background=Brandenburg-DOP20c&disable_features=boundaries&locale=de&hashtags=radverkehrsatlas`
 }
 
 export const osmEditRapidUrl = (type: OsmShortType, id: number | string) => {
   if (!type || (type && !longOsmType[type])) return undefined
 
-  return `https://rapideditor.org/edit#id=${type.toLocaleLowerCase()}${id}`
+  return `https://rapideditor.org/edit#id=${type.toLocaleLowerCase()}${id}&disable_features=boundaries&locale=de&hashtags=radverkehrsatlas`
 }
 
 export const historyUrl = (type: OsmShortType, id: number | string) => {
