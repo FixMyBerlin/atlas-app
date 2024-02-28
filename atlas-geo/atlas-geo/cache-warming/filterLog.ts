11eb7e7263e8b4f2b9c7f0100cd5e7d188a0f5be
Sven König <sven@de-selby.com>
2024-02-28 10:22:59 +0100

    clean up filterLog.ts

--- atlas-geo/atlas-geo/cache-warming/filterLog.ts
+++ atlas-geo/atlas-geo/cache-warming/filterLog.ts
@@ -3,7 +3,7 @@
 import { parseArgs } from 'node:util'
 import fs from 'node:fs'
 import chalk from 'chalk'
-import { displayHelp } from './util'
+import { parseSize, parseTime, displayHelp } from './util'
 
 function error(message) {
   console.error(message)
@@ -52,38 +52,6 @@ const filename = positionals[0]
 if (!fs.existsSync(filename)) error(`File "${filename}" not found.`)
 if (args.hit && args.miss) error('Supply only one of "hit" or "miss"')
 
-function parseNumber(valueName) {
-  if (!(valueName in values)) return null
-  const v = Number(values[valueName])
-  if (isNaN(v)) error(`Argument "${valueName}" is not a number.`)
-  return v
-}
-
-function parseTime(time) {
-  const num = Number(time)
-  if (!isNaN(num)) return time
-  const m = time.trim().match(/^([0-9]+\.[0-9]{3})s$/)
-  if (!m) error(`Could not parse time "${time}".`)
-  return Number(m[1])
-}
-
-function parseSize(size) {
-  const num = Number(size)
-  if (!isNaN(num)) return num
-  const m = size.trim().match(/^([0-9.]+)([BKMG])$/)
-  if (!m) error(`Could not parse size "${size}".`)
-  let [_, bytes, unit] = m
-  return Math.round(
-    bytes *
-      {
-        B: 1,
-        K: 1024,
-        M: 1024 ** 2,
-        G: 1024 ** 3,
-      }[unit],
-  )
-}
-
 const logData = (await Bun.file(filename).text()).split('\n')
 let i = 0
 while (i < logData.length) {
@@ -118,7 +86,7 @@ while (i < logData.length) {
   } else if (args.miss && cacheStatus !== 'MISS') {
     // don't log
   } else if (args.minSize === null && args.minTime === null) {
-    // don't log if neither min-size nor min-time is given
+    // don't log if neither minSize nor minTime is given
   } else {
     let [cacheStatus, timeFormatted, sizeFormatted] = response.slice(2).split(' - ')
     const logSize = args.minSize === null ? false : parseSize(sizeFormatted) >= args.minSize
