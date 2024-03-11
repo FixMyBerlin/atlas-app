91767f491b7ae07ecad487f156510863628dd4b8
Tobias <tobias@fixmycity.de>
2024-03-11 15:03:51 +0100

    Revert "Maplibre: Improve wrapFilterWithAll"
    This reverts commit 41c5515429d8009b053ca9e36fa1aa5b2bb0b098.

--- src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.ts
+++ src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.ts
@@ -1,24 +1,16 @@
 export const wrapFilterWithAll = (filterArray: any[] | undefined) => {
-  let workingArray: typeof filterArray = []
-  // Cleanup input: `[null]` becomes `[]`
-  workingArray = filterArray?.filter(Boolean)
-  // Cleanup input: `[]` becomes `undefined`
-  if (workingArray?.length === 0) {
-    workingArray = undefined // `[]` becomes `undefined`
-  }
-
   // Case: Input empty/undefined
-  if (!workingArray) {
+  if (!filterArray) {
     return ['all']
   }
-
-  // Cleanup input: 'all's are removed
-  workingArray = workingArray.filter((item) => !(typeof item === 'string' && item === 'all'))
-
+  // Case: Input already wrapped in 'all'
+  if (filterArray[0] === 'all') {
+    return filterArray.filter(Boolean)
+  }
   // Case: Input is multi dimensional array of filters
-  if (Array.isArray(workingArray[0])) {
-    return ['all', workingArray].flat().filter(Boolean)
+  if (typeof filterArray[0] === 'object') {
+    return ['all', filterArray].flat().filter(Boolean)
   }
   // Case: Input is only one filter
-  return ['all', workingArray].filter(Boolean)
+  return ['all', filterArray]
 }
