4b535cf8a65054697bc0158ce8d1e0ac3a13bc21
rush42 <laurenz@fixmycity.de>
2024-03-12 16:04:27 +0100

    add bounds to vector tile specification

--- src/instrumentation/initGeneralizationFunctions.ts
+++ src/instrumentation/initGeneralizationFunctions.ts
@@ -7,13 +7,29 @@ export async function initGeneralizationFunctions(tables) {
   return Promise.all(
     tables.map(async (tableName) => {
       const functionName = generalizationFunctionIdentifier(tableName)
-      // Get table columns and types for vector tile specification (tile.json)
-      const queryResults = await prismaClientForRawQueries.$queryRawUnsafe(`
-        SELECT jsonb_object_agg(column_name, udt_name) - 'geom' as fields
+      // Gather meta information for the tile specification
+
+      // Get column names and types
+      const columnInformation = await prismaClientForRawQueries.$queryRawUnsafe(`
+        SELECT jsonb_object_agg(column_name, udt_name) - 'geom' AS fields
           FROM information_schema.columns
           WHERE table_schema = 'public' AND table_name = '${tableName}';`)
-      const { fields } = queryResults && queryResults[0]
-      const vectorLayerDescriptions = { vector_layers: [{ id: functionName, fields }] }
+      const { fields } = columnInformation && columnInformation[0]
+
+      // Get the geometric extent
+      const bbox = await prismaClientForRawQueries.$queryRawUnsafe(
+        `SELECT ST_XMIN(bbox) AS xmin, ST_YMIN(bbox) AS ymin, ST_XMAX(bbox) AS xmax, ST_YMAX(bbox) AS ymax
+          from (
+            SELECT ST_Transform(ST_SetSRID(ST_Extent(geom), 3857), 4326) AS bbox
+              from ${tableName}
+            ) extent;`,
+      )
+      const { xmin, ymin, xmax, ymax } = bbox && bbox[0]
+      // format as vector tile specifaction
+      const tileSpecification = {
+        vector_layers: [{ id: functionName, fields }],
+        bounds: [xmin, ymin, xmax, ymax],
+      }
       return prismaClientForRawQueries.$transaction([
         prismaClientForRawQueries.$executeRaw`SET search_path TO public;`,
         prismaClientForRawQueries.$executeRawUnsafe(
@@ -32,13 +48,13 @@ export async function initGeneralizationFunctions(tables) {
                     4096, 64, true) AS g
               FROM "${tableName}"
               WHERE (geom && ST_TileEnvelope(z, x, y)) and (not tags?'_minZoom' or z >= (tags->'_minZoom')::integer )
-            ) as tile WHERE geom IS NOT NULL;
+            ) AS tile WHERE geom IS NOT NULL;
             RETURN mvt;
           END
           $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;`,
         ),
         prismaClientForRawQueries.$executeRawUnsafe(
-          `COMMENT ON FUNCTION ${functionName} IS '${JSON.stringify(vectorLayerDescriptions)}';`,
+          `COMMENT ON FUNCTION ${functionName} IS '${JSON.stringify(tileSpecification)}';`,
         ),
       ])
     }),
