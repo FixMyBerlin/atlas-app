d505cdec3ea8e3d887d23e7639f8ae30c0a9664f
Tobias <tobias@fixmycity.de>
2024-03-12 09:56:45 +0100

    Boundaries: Update styles

--- src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/mapboxStyles/groups/atlas_boundaries.ts
+++ src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/mapboxStyles/groups/atlas_boundaries.ts
@@ -6,24 +6,24 @@ import { MapboxStyleLayer } from '../types'
 export const mapboxStyleGroupLayers_atlas_boundaries: MapboxStyleLayer[] = [
   {
     type: 'line',
-    id: 'category_district_landkreis',
-    paint: {
-      'line-color': '#f094cd',
-      'line-width': 2,
-      'line-opacity': 0.6,
-      'line-dasharray': [2.5, 1, 1, 1],
-    },
-    filter: ['has', 'category_district'],
-  },
-  {
-    type: 'line',
     id: 'category_municipality_gemeinden',
     paint: {
-      'line-color': '#e6a3cc',
+      'line-color': '#efc6a9',
       'line-opacity': 0.6,
       'line-dasharray': [2.5, 1, 1, 1],
       'line-width': 1.5,
     },
     filter: ['has', 'category_municipality'],
+  },
+  {
+    type: 'line',
+    id: 'category_district_landkreis',
+    paint: {
+      'line-color': '#eab590',
+      'line-width': 2,
+      'line-opacity': 0.6,
+      'line-dasharray': [2.5, 1, 1, 1],
+    },
+    filter: ['has', 'category_district'],
   },
 ]
