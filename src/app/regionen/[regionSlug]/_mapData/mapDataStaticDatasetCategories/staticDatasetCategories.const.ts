ead9579ac6780a5d9ffed0d36ad828dc58ff4e2f
Tobias <tobias@fixmycity.de>
2024-03-27 16:39:04 +0100

    Datasets: Update category titles

--- src/app/regionen/[regionSlug]/_mapData/mapDataStaticDatasetCategories/staticDatasetCategories.const.ts
+++ src/app/regionen/[regionSlug]/_mapData/mapDataStaticDatasetCategories/staticDatasetCategories.const.ts
@@ -3,19 +3,19 @@ export type StaticDatasetCategoryKey = keyof typeof staticDatasetCategories
 export const staticDatasetCategories = {
   'bb/Bestandsdaten': {
     title: 'Netzentwicklung Land',
-    subtitle: 'Statische Daten zur Entwicklung des Radnetz für Brandenburg',
+    subtitle: 'Statische Daten zur Entwicklung des Radnetzes für Brandenburg',
   },
   'bb/Radnetze': {
     title: 'Bestandsdaten',
-    subtitle: 'Statische Daten zur vorhandenen Radinfrastruktur ',
+    subtitle: 'Statische Daten zur vorhandenen Radinfrastruktur',
   },
   'bb/Netzkonzeption': {
-    title: 'Radnetze und Route',
+    title: 'Radnetze und Routen',
     subtitle:
       'Statische Daten zu vorhandenen Radnetzen, touristischen Routen, Bedarfen und Planungen',
   },
   'bb/Landesdaten': {
     title: 'Weitere Daten',
-    subtitle: 'Weitere Statische Daten',
+    subtitle: 'Weitere statische Daten',
   },
 } as const
