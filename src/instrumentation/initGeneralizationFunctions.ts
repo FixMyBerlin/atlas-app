143cc575218b9a5d39f18dedd3ea335e51c71b74
rush42 <laurenz@fixmycity.de>
2024-03-28 13:54:43 +0100

    remove `minzoom` from exposed columns

--- src/instrumentation/initGeneralizationFunctions.ts
+++ src/instrumentation/initGeneralizationFunctions.ts
@@ -11,7 +11,7 @@ export async function initGeneralizationFunctions(tables) {
 
       // Get column names and types
       const columnInformation = await prismaClientForRawQueries.$queryRawUnsafe(`
-        SELECT jsonb_object_agg(column_name, udt_name) - 'geom' AS fields
+        SELECT jsonb_object_agg(column_name, udt_name) - 'geom' - 'minzoom' AS fields
           FROM information_schema.columns
           WHERE table_schema = 'public' AND table_name = '${tableName}';`)
       const { fields } = columnInformation && columnInformation[0] // this object has the form {columnName: columnType}
@@ -75,57 +75,3 @@ export async function initGeneralizationFunctions(tables) {
     }),
   )
 }
-
-// original adapted
-
-// CREATE OR REPLACE
-//           FUNCTION public.${functionName}(z integer, x integer, y integer)
-//           RETURNS bytea AS $$
-//           BEGIN
-//           SELECT ST_AsMVT(tile, "${functionName}", 4096, 'geom')
-//           FROM (
-//             SELECT
-//               ST_AsMVTGeom(
-//                   ST_Transform(ST_CurveToLine("geom"), 3857),
-//                   ST_TileEnvelope(z, x, y),
-//                   4096, 64, true
-//               ) AS geom
-//               , "meta", "minzoom", "osm_id", "osm_type", "tags"
-//             FROM
-//               "public".${tableName}
-//             WHERE
-//               "geom" && ST_Transform(ST_TileEnvelope(z, x, y, margin => 0.015625), 3857)
-
-//           ) AS tile;
-//           END
-//           $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
-
-// with simplify
-//  `CREATE OR REPLACE
-//  FUNCTION public.${functionName}(z integer, x integer, y integer)
-//  RETURNS bytea AS $$
-//  DECLARE
-//    mvt bytea;
-//    tolerance float;
-//  BEGIN
-//    IF z BETWEEN 6 AND 14 THEN
-//      tolerance = 0;
-//    ELSE
-//      tolerance = 0;
-//    END IF;
-//    SELECT INTO mvt ST_AsMVT(tile, '${functionName}', 4096, 'g') FROM (
-//      select
-//      *,
-//        ST_AsMVTGeom(
-//            ST_CurveToLine(
-//              ST_Simplify(geom, tolerance, true)
-//            ),
-//            ST_TileEnvelope(z, x, y),
-//            4096, 64, true) AS g
-//      FROM "${tableName}"
-//      WHERE (geom && ST_TileEnvelope(z, x, y))
-//        and  z >= minzoom
-//    ) AS tile WHERE geom IS NOT NULL;
-//    RETURN mvt;
-//  END
-//  $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;`,
