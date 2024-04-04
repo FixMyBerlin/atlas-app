d18483cc04ffddb8ec078e20fe4f6be10468ee01
rush42 <laurenz@fixmycity.de>
2024-04-04 15:30:51 +0200

    add delete and added rows to diff

--- atlas-geo/app/JSONDiff.sql
+++ atlas-geo/app/JSONDiff.sql
@@ -12,12 +12,26 @@ BEGIN
             diff = diff || jsonb_build_object(v.key, (v.value)::text || ' -> ' || (new -> v.key)::text );
             new = new - v.key;
         ELSE
-            diff = diff || jsonb_build_object(v.key,  '-' || (v.value)::text );
+            diff = diff || jsonb_build_object(v.key,  '(-)' || (v.value)::text );
         END IF;
     END LOOP;
     FOR v IN SELECT * FROM jsonb_each(new) LOOP
-        diff = diff || jsonb_build_object(v.key,  '+' || v.value::text );
+        diff = diff || jsonb_build_object(v.key,  '(+)' || v.value::text );
     END LOOP;
     RETURN diff;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION jsonb_prefix_values(json_in JSONB, prefix text)
+RETURNS JSONB AS $$
+DECLARE
+    json_out JSONB;
+    v RECORD;
+BEGIN
+    json_out = '{}'::jsonb;
+    FOR v IN SELECT * FROM jsonb_each(json_in) LOOP
+        json_out = json_out || jsonb_build_object(v.key,  prefix || v.value::text );
+    END LOOP;
+    RETURN json_out;
 END;
 $$ LANGUAGE plpgsql;
--- atlas-geo/app/compute_diff.sh
+++ atlas-geo/app/compute_diff.sh
@@ -16,6 +16,24 @@ ON \"$table\".osm_id = \"$backup_table\".osm_id
 WHERE diff != '{}'::jsonb;"
 echo $query | psql -q
 
+# create diffs for added rows
+query="
+INSERT INTO \"$diff_table\"
+SELECT jsonb_prefix_values(\"$table\".tags, '(+)') as diff, \"$table\".osm_id, \"$table\".meta, \"$table\".geom
+FROM \"$table\"
+FULL OUTER JOIN \"$backup_table\"
+ON \"$table\".osm_id = \"$backup_table\".osm_id
+WHERE \"$backup_table\".osm_id IS NULL;"
+echo $query | psql -q
 
-# TODO add deleted and added rows to diff table
+# create diffs for deleted rows
+query="
+INSERT INTO \"$diff_table\"
+SELECT jsonb_prefix_values(\"$backup_table\".tags, '(-)') as diff, \"$backup_table\".osm_id, \"$backup_table\".meta, \"$backup_table\".geom
+FROM \"$backup_table\"
+FULL OUTER JOIN \"$table\"
+ON \"$backup_table\".osm_id = \"$table\".osm_id
+WHERE \"$table\".osm_id IS NULL;"
+echo $query | psql -q
+
 psql -q -c "DROP TABLE \"$backup_table\";"
