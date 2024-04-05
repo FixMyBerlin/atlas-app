3340e5521e7b865c1d22a55e09099cfb60d0ed43
rush42 <laurenz@fixmycity.de>
2024-04-05 14:22:45 +0200

    move `compute_diff` into `process-helpers.sh`

--- atlas-geo/atlas-geo/app/process-helpers.sh
+++ atlas-geo/atlas-geo/app/process-helpers.sh
@@ -110,9 +110,58 @@ update_hash() {
 }
 
 
-backup_table() {
-  psql -q -c "ALTER TABLE \"$1\" RENAME TO \"$1_backup\";"
+compute_diff() {
+  backup_table=$1_backup
+  table=$1
+  diff_table=$1_diff
+
+  psql -q -c "DROP TABLE IF EXISTS \"$diff_table\";" &> /dev/null
+
+  query="
+    SELECT diff, osm_id, meta, geom INTO \"$diff_table\"
+    FROM (
+    SELECT jsonb_diff(\"$backup_table\".tags, \"$table\".tags) AS diff, \"$table\".osm_id, \"$table\".meta, \"$table\".geom
+    FROM \"$table\"
+    JOIN \"$backup_table\"
+    ON \"$table\".osm_id = \"$backup_table\".osm_id
+    ) sq
+    WHERE diff != '{}'::jsonb;"
+  echo $query | psql -q
+
+  # create diffs for added rows
+  query="
+    INSERT INTO \"$diff_table\"
+    SELECT jsonb_prefix_values(\"$table\".tags, '(+)') as diff,
+    \"$table\".osm_id,
+    \"$table\".meta,
+    \"$table\".geom
+    FROM \"$table\"
+    FULL OUTER JOIN \"$backup_table\"
+    ON \"$table\".osm_id = \"$backup_table\".osm_id
+    WHERE \"$backup_table\".osm_id IS NULL;"
+  echo $query | psql -q
+
+  # create diffs for deleted rows
+  query="
+    INSERT INTO \"$diff_table\"
+    SELECT
+    jsonb_prefix_values(\"$backup_table\".tags, '(-)') as diff,
+    \"$backup_table\".osm_id,
+    \"$backup_table\".meta,
+    \"$backup_table\".geom
+    FROM \"$backup_table\"
+    FULL OUTER JOIN \"$table\"
+    ON \"$backup_table\".osm_id = \"$table\".osm_id
+    WHERE \"$table\".osm_id IS NULL;"
+  echo $query | psql -q
+
+  n_changes=$(psql -t -A -c "SELECT count(*) FROM \"$diff_table\";")
+  if [ "$n_changes" == 0 ]; then
+    psql -q -c "DROP TABLE \"$diff_table\";"
+  fi
+  log "$n_changes changes in $table!"
 }
+
 
 
 run_lua() {
@@ -211,7 +260,7 @@ run_dir() {
     if [ -f $backuped_tables ]; then
       for table in $(cat $backuped_tables); do
         log "Creating diff for $table."
-        /app/compute_diff.sh $table
+        compute_diff $table
         psql -q -c "DROP TABLE \"${table}_backup\";"
       done
       rm $backuped_tables
