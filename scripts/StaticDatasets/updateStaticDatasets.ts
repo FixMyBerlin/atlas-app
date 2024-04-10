790b0b243c32d52fb46efc4f46babcbb3590c7d1
Tobias <tobias@fixmycity.de>
2024-04-10 11:53:38 +0200

    Datasets: Refactor updateStaticDataset and rework to use region-group-subfolders
    main things:
    - static datasets are now grouped in region groups
    - quite a bit of refactoring
    - `dry-run` now creates transformed geojson but no uploads, no db, no pmtiles
    - filenames of geojson are now based on foldername, not input filename
    - docs are updated
    
    import:
    - Improve return typing
    - Move "file missing" warning in main loop to simplify function arguments
    - Refactor if-else in if-guard
    
    General:
    - inline `pp` as path.parse which allows to check types and function specs more inline
    - clarify dryRun logging
    
    generatePMTilesFile:
    - move dryRun check in main loop to make function self contained
    - refactor main loop and function to handle file path and file names inside the function and return the filename from the function

--- scripts/StaticDatasets/updateStaticDatasets.ts
+++ scripts/StaticDatasets/updateStaticDatasets.ts
@@ -10,16 +10,15 @@ import { createUpload, getRegions } from './api'
 import { green, inverse, red, yellow } from './log'
 import { MetaData } from './types'
 
-const pp = path.parse
 const geoJsonFolder = 'scripts/StaticDatasets/geojson'
 const tmpDir = path.join(os.tmpdir(), 'pmtiles')
 const regions = await getRegions()
 const existingRegionSlugs = regions.map((region) => region.slug)
 
-// script can be run with --dry-run to just run all checks
-// without transforming and processing geojsons, uploading pmtiles and saving to db
+// use --dry-run to run all checks and transformation (but no pmtiles created, no upload to S3, no DB modifications)
 // use --keep-tmp to keep temporary generated files
-// use --folder-filter to run only this folders that include this filter string
+// use --keep-tmp to keep temporary generated files
+// use --folder-filter to run only folders that include this filter string (check the full path, so `region-bb` (group folder) and `bb-` (dataset folder) will both work)
 const { values, positionals } = parseArgs({
   args: Bun.argv,
   options: {
@@ -44,19 +43,24 @@ inverse('Starting update with settings', [
   },
 ])
 
-const generatePMTilesFile = (inputFile: string, outputFile: string) => {
-  if (dryRun) return '/tmp/does-not-exist.pmtiles'
+/** @returns pmtiles outputFullFile */
+const generatePMTilesFile = async (inputFullFile: string) => {
+  const outputFilename = path.parse(inputFullFile).name
+  const outputFullFile = path.join(tmpDir, `${outputFilename}.pmtiles`)
+
+  console.log(`  Generating pmtiles file "${outputFullFile}"...`)
+
   Bun.spawnSync(
     [
       'tippecanoe',
-      `--output=${outputFile}`,
+      `--output=${outputFullFile}`,
       '--force',
       '--smallest-maximum-zoom-guess=8', // Smallest maxzoom which is acceptable for our precision requirements, is higher, if tippecanoe guesses a higher maxzoom, it will be used ttps://github.com/felt/tippecanoe#zoom-levels / Automatic --maximum-zoom didn't have the required precision
       '-rg', // If you use -rg, it will guess a drop rate that will keep at most 50,000 features in the densest tile https://github.com/felt/tippecanoe#dropping-a-fixed-fraction-of-features-by-zoom-level
       '--drop-densest-as-needed', // https://github.com/felt/tippecanoe?tab=readme-ov-file#dropping-a-fraction-of-features-to-keep-under-tile-size-limits
       '--extend-zooms-if-still-dropping', // https://github.com/felt/tippecanoe?tab=readme-ov-file#zoom-levels
       '--layer=default',
-      inputFile,
+      inputFullFile,
     ],
     {
       onExit(_proc, exitCode, _signalCode, error) {
@@ -71,10 +75,14 @@ const generatePMTilesFile = (inputFile: string, outputFile: string) => {
       },
     },
   )
+
+  return outputFullFile
 }
 
-export const uploadFileToS3 = async (fileToUpload: string, filename: string) => {
-  if (dryRun) return 'http://example.com/does-not-exist.pmtiles'
+/** @returns URL of pmtile on S3 */
+export const uploadFileToS3 = async (uploadFullFilename: string, datasetFolder: string) => {
+  console.log('  Uploading generated pmtiles file to S3...')
+
   const accessKeyId = process.env.S3_KEY
   const secretAccessKey = process.env.S3_SECRET
   const region = process.env.S3_REGION
@@ -86,13 +94,14 @@ export const uploadFileToS3 = async (fileToUpload: string, filename: string) => 
     region,
   })
 
-  const fileKey = `uploads/${folder}/${filename}`
+  const remoteFilename = `${datasetFolder}/${path.parse(uploadFullFilename).base}`
+  const fileKey = `uploads/${folder}/${remoteFilename}`
   try {
     await s3Client.send(
       new PutObjectCommand({
         Bucket: bucket,
         Key: fileKey,
-        Body: fs.readFileSync(fileToUpload),
+        Body: fs.readFileSync(uploadFullFilename),
       }),
     )
   } catch (e) {
@@ -103,79 +112,70 @@ export const uploadFileToS3 = async (fileToUpload: string, filename: string) => 
   return `https://${bucket}.s3.${region}.amazonaws.com/${fileKey}`
 }
 
-const findGeojson = (folderName) => {
-  const folderPath = path.join(geoJsonFolder, folderName)
-  const filenames = fs.readdirSync(folderPath).filter((filename) => pp(filename).ext === '.geojson')
+/** @returns geojson fullFilename | null */
+const findGeojson = (datasetFolderPath: string) => {
+  const filenames = fs
+    .readdirSync(datasetFolderPath)
+    .filter((filename) => path.parse(filename).ext === '.geojson')
+
   if (filenames.length === 0) {
-    yellow(`  Folder "${folderName}" does not contain a geojson.`)
+    yellow(`  Folder "${datasetFolderPath}" does not contain a geojson.`)
     return null
   }
   if (filenames.length > 1) {
-    yellow(`  Folder "${folderName}" contains multiple geojsons.`)
+    yellow(`  Folder "${datasetFolderPath}" contains multiple geojsons.`)
     return null
   }
 
-  const filename = filenames[0]!
-  const filePath = path.join(folderPath, filename)
-  if (!fs.lstatSync(filePath).isFile()) {
-    yellow(`  Folder "${folderName}/${filename}" is not a file.`)
+  const fullFilename = path.join(datasetFolderPath, filenames[0]!)
+  if (!fs.lstatSync(fullFilename).isFile()) {
+    yellow(`  Path "${fullFilename}" is not a file.`)
     return null
   }
 
-  return filename
+  return fullFilename
 }
 
-const checkFilename = (filename) => {
-  const m = filename.match(/[^\-.0-9a-zA-Z_]/g)
-  if (m !== null) {
-    const chars = JSON.stringify(m).slice(1, -1)
-    red(
-      `  Filename "${filename}" contains potentially problematic ${pluralize(
-        'character. This file is skipped. Please rename the file and run again.',
-        m.length,
-      )} ${chars}.`,
-    )
-    return false
-  }
-  return true
-}
-
-const import_ = async (folderName, moduleName, valueName, warnIfModuleDoesNotExist?) => {
+/** @returns Object or Function | null */
+const import_ = async <ReturnModule extends Function | Object>(
+  folderName: string,
+  moduleName: string,
+  valueName: string,
+) => {
   const moduleFileName = `${moduleName}.ts`
-  const moduleFilePath = path.join(geoJsonFolder, folderName, moduleFileName)
-  if (!fs.existsSync(moduleFilePath)) {
-    if (warnIfModuleDoesNotExist) {
-      yellow(`  ${moduleFileName} is missing in folder ${folderName}`)
-    }
+  const moduleFullFilename = path.join(folderName, moduleFileName)
+
+  if (!fs.existsSync(moduleFullFilename)) {
     return null
-  } else {
-    const module_ = await import(`./geojson/${folderName}/${moduleName}`)
-    if (!(valueName in module_)) {
-      yellow(`  ${moduleFileName} does not export value "${valueName}".`)
-      return null
-    }
-    return module_[valueName]
   }
+
+  const module_ = await import(moduleFullFilename)
+
+  if (!(valueName in module_)) {
+    yellow(`  ${moduleFileName} does not export value "${valueName}".`)
+    return null
+  }
+  return module_[valueName] as ReturnModule
 }
 
-const transformFile = async (folderName, inputFilepath) => {
-  const transform = await import_(folderName, 'transform', 'transform')
+/** @returns geojson outputFullFilename which is either the initial geojson or the transformed geojson  */
+const transformFile = async (datasetFolderPath: string, geojsonFullFilename: string) => {
+  type TransformFunc = (data: any) => string
+  const transform = await import_<TransformFunc>(datasetFolderPath, 'transform', 'transform')
+
   if (transform === null) {
-    return inputFilepath
+    return geojsonFullFilename
   }
 
-  const outputFilepath = path.join(tmpDir, `${pp(inputFilepath).name}.transformed.geojson`)
-  keepTemporaryFiles
-    ? console.log(`  Transforming geojson file to "${outputFilepath}"...`)
-    : console.log(`  Transforming geojson file...`)
-  if (dryRun) {
-    return outputFilepath
-  }
-  const data = await Bun.file(inputFilepath).json()
+  const datasetFolderName = datasetFolderPath.split('/').at(-1)
+  const outputFullFilename = path.join(tmpDir, `${datasetFolderName}.transformed.geojson`)
+  console.log(`  Transforming geojson file...`, outputFullFilename)
+
+  const data = await Bun.file(geojsonFullFilename).json()
   const transformedData = transform(data)
-  Bun.write(outputFilepath, JSON.stringify(transformedData, null, 2))
+  Bun.write(outputFullFilename, JSON.stringify(transformedData, null, 2))
 
-  return outputFilepath
+  return outputFullFilename
 }
 
 // create tmp folder
@@ -184,65 +184,79 @@ if (!fs.existsSync(tmpDir)) {
 }
 
 if (!fs.existsSync(geoJsonFolder)) {
-  red(
-    `folder "${geoJsonFolder}" does not exists. Have you forgot to run "npm run link-atlas-static-data"?`,
-  )
+  red(`folder "${geoJsonFolder}" does not exists. Please run "npm run link-atlas-static-data"?`)
   process.exit(1)
 }
 
-const folderNames = fs
+// Collect the file and folder data that we iterate over
+const regionGroupFolderPaths = fs
   .readdirSync(geoJsonFolder)
+  // Make sure we only select folders, no files
+  .filter((item) => fs.statSync(path.join(geoJsonFolder, item)).isDirectory())
   // We skip `_utils` and by convention prefix unpublished datasets with underscore
   .filter((folder) => !folder.startsWith('_'))
-  // If a `folder-filter` is given, we only look at folder that include this term
-  .filter((folder) => (folderFilterTerm ? folder.includes(folderFilterTerm) : true))
-  .sort()
+const datasetFileFolderData = regionGroupFolderPaths
+  .map((regionGroupFolder) => {
+    const subFolders = fs.readdirSync(path.join(geoJsonFolder, regionGroupFolder))
+    return subFolders.map((datasetFolder) => {
+      const targetFolder = path.join(geoJsonFolder, regionGroupFolder, datasetFolder)
+      // We skip `_utils` and by convention prefix unpublished datasets with underscore
+      if (datasetFolder.startsWith('_')) return
+      // If a `folder-filter` is given, we only look at folder that include this term
+      if (folderFilterTerm && !targetFolder.includes(folderFilterTerm)) return
+      // Make sure we only select folders, no files
+      if (!fs.statSync(targetFolder).isDirectory()) return
+      return { datasetFolderPath: targetFolder, regionFolder: regionGroupFolder, datasetFolder }
+    })
+  })
+  .flat()
+  .filter(Boolean)
+  .sort((a, b) => a.datasetFolderPath.localeCompare(b.datasetFolderPath))
 
-for (const i in folderNames) {
-  const folderName = folderNames[i]!
-  const folderPath = path.join(geoJsonFolder, folderName)
+for (const { datasetFolderPath, regionFolder, datasetFolder } of datasetFileFolderData) {
+  const regionAndDatasetFolder = `${regionFolder}/${datasetFolder}`
+  inverse(`Processing folder "${regionAndDatasetFolder}"...`)
 
-  if (!fs.lstatSync(folderPath).isDirectory()) {
-    // is not a folder
-    continue
-  }
-
-  inverse(`Processing folder "${folderName}"...`)
-
-  const uploadSlug = slugify(folderName.replaceAll('_', '-'))
-  if (folderName !== uploadSlug) {
+  // Guard invalid folder names (characters)
+  const uploadSlug = slugify(datasetFolder.replaceAll('_', '-'))
+  if (datasetFolder !== uploadSlug) {
     yellow(
-      `  Folder name "${folderName}" is not a valid slug. A valid slug could be "${uploadSlug}"`,
+      `  Folder name "${datasetFolder}" in ${regionAndDatasetFolder} is not a valid slug.
+      \n  A valid slug would be "${uploadSlug}"`,
     )
     continue
   }
 
-  const metaData = (await import_(folderName, 'meta', 'data', true)) as MetaData
+  // Get the `meta.js` data ready
+  const metaData = await import_<MetaData>(datasetFolderPath, 'meta', 'data')
   if (metaData === null) {
+    yellow(`  File 'meta.ts' is missing in folder ${datasetFolderPath}`)
     continue
   }
 
-  const geojsonFullFilename = findGeojson(folderName)
-  if (!geojsonFullFilename) continue
-
-  if (!checkFilename(geojsonFullFilename)) {
-    // logs info is filename is not ok
+  // Get the one `.geojson` file that we will handle ready
+  const geojsonFullFilename = findGeojson(datasetFolderPath)
+  if (!geojsonFullFilename) {
+    // Logging is part of findGeojson()
     continue
   }
 
-  const inputFilepath = await transformFile(folderName, path.join(folderPath, geojsonFullFilename))
-  const outputFilename = pp(inputFilepath).name
-  const outputFilepath = path.join(tmpDir, `${outputFilename}.pmtiles`)
+  // Create the transformed data
+  const inputFullFilepath = await transformFile(datasetFolderPath, geojsonFullFilename)
 
-  keepTemporaryFiles
-    ? console.log(`  Generating pmtiles file "${outputFilepath}"...`)
-    : console.log(`  Generating pmtiles file...`)
-  await generatePMTilesFile(inputFilepath, outputFilepath)
+  // Create the pmtiles
+  const outputFullFilepath = dryRun
+    ? '/tmp/does-not-exist.pmtiles'
+    : await generatePMTilesFile(inputFullFilepath)
+  if (dryRun) console.log(`  DRY RUN: SKIPPING Generating pmtiles file...`)
 
-  console.log('  Uploading generated pmtiles file...')
-  const s3filename = `${uploadSlug}/${pp(outputFilepath).base}`
-  const pmtilesUrl = await uploadFileToS3(outputFilepath, s3filename)
+  // Upload pmtiles to S3
+  const pmtilesUrl = dryRun
+    ? 'http://example.com/does-not-exist.pmtiles'
+    : await uploadFileToS3(outputFullFilepath, datasetFolder)
+  if (dryRun) console.log(`  DRY RUN: SKIPPING Uploading generated pmtiles file to S3...`)
 
+  // Create database entries dataset per region (from meta.ts)
   const regionSlugs: string[] = []
   metaData.regions.forEach((regionSlug) => {
     if (existingRegionSlugs.includes(regionSlug)) {
@@ -256,8 +270,9 @@ for (const i in folderNames) {
     regionSlugs.length === 0
       ? 'will not be assigned to any region'
       : `will be assigned to ${pluralize('region', regionSlugs.length)} ${regionSlugs.join(', ')}`
-  console.log(`  Saving upload to DB (${info})...`)
+  if (dryRun) console.log(`  DRY RUN: SKIPPING Saving upload to DB (${info})...`)
   if (!dryRun) {
+    console.log(`  Saving upload to DB (${info})...`)
     await createUpload({
       uploadSlug,
       pmtilesUrl,
