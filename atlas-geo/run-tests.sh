ec4e2d2a10fb4653ea0d1cfe459b19340a44db4d
rush42 <laurenz@fixmycity.de>
2024-04-11 10:42:45 +0200

    mount `/processing` to run new code without build

--- atlas-geo/run-tests.sh
+++ atlas-geo/run-tests.sh
@@ -2,4 +2,4 @@
 # Start docker and run all tests there.
 # This file is used by `npm run test`
 
-docker compose -f docker-compose.development.yml run --build --entrypoint "busted /processing/topics/ -p %.test%.lua$" processing
+docker compose -f docker-compose.development.yml run --entrypoint "busted /processing/topics/ -p %.test%.lua$" processing
