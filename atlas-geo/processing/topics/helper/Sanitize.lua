779b7bc938f1cec290869f5b156994426fdb9447
rush42 <laurenz@fixmycity.de>
2024-04-17 13:45:34 +0200

    update comment

--- atlas-geo/processing/topics/helper/Sanitize.lua
+++ atlas-geo/processing/topics/helper/Sanitize.lua
@@ -2,7 +2,7 @@
 ---@param allowed table
 ---@param fallback? any
 ---@return any
--- makes sure that `value` is in the set `allowed`. Returns `default` if value is nil
+-- makes sure that `value` is in the set `allowed`. Returns `fallback` if value is nil
 function Sanitize(value, allowed, fallback)
   if value == nil then
     return fallback
