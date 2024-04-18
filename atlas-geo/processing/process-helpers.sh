98bb9b56df35be5e82a5640e7cd117d253c004a0
rush42 <laurenz@fixmycity.de>
2024-04-18 10:33:25 +0200

    add option to only compute diffs without updating the data

--- atlas-geo/processing/process-helpers.sh
+++ atlas-geo/processing/process-helpers.sh
@@ -77,7 +77,7 @@ run_dir() {
     fi
   else
     # Backup tables for diffs
-    if [ "$COMPUTE_DIFFS" == 1 ]; then
+    if [ "$COMPUTE_DIFFS" == 1 ] || [ "$COMPUTE_DIFFS" == 2 ]; then
       if [ -f "$processed_tables" ]; then
         for table in $(cat $processed_tables); do # iterate over tables from last run
           psql -q -c "DROP TABLE IF EXISTS \"${table}_backup\";" &> /dev/null
@@ -100,14 +100,20 @@ run_dir() {
     # Create diffs for all backedup tables that where already available
     if [ -f $backedup_tables ]; then
       log_start "Compute Diffs"
-      log "Computing diffs (.env 'COMPUTE_DIFFS=1') for: $(tr '\n' ' ' < $backedup_tables)"
+      log "Computing diffs (.env 'COMPUTE_DIFFS=1|2') for: $(tr '\n' ' ' < $backedup_tables)"
       for table in $(cat $backedup_tables); do
         if grep -q -E "^${table}\$" $processed_tables; then
           compute_diff $table
         else
           log "$table got deleted."
         fi
-        psql -q -c "DROP TABLE \"${table}_backup\";"
+        if [ "$COMPUTE_DIFFS" == 2 ]; then
+          log "Restoring data. (.env 'COMPUTE_DIFFS = 2')"
+          psql -q -c "DROP TABLE \"${table}\";"
+          psql -q -c "ALTER TABLE \"${table}_backup\" RENAME TO \"${table}\";"
+        else
+          psql -q -c "DROP TABLE \"${table}_backup\";"
+        fi
       done
       rm $backedup_tables
       log_end "Compute Diffs"
