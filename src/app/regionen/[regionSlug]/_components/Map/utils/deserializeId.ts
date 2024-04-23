9cf01f494a1d1742bd678d3ff58984fe21c72257
rush42 <laurenz@fixmycity.de>
2024-04-23 15:26:08 +0200

    deserializeId: make function type safe

--- src/app/regionen/[regionSlug]/_components/Map/utils/deserializeId.ts
+++ src/app/regionen/[regionSlug]/_components/Map/utils/deserializeId.ts
@@ -1,4 +1,4 @@
 export const deserializeId = (id: string) => {
   const [osm_type, osm_id] = id.split('/')
-  return { osm_type, osm_id }
+  return { osm_type: osm_type || '', osm_id: Number(osm_id) }
 }
