dba4e5e998d165a5a0235a8806eb6bccec237f3f
Tobias <tobias@fixmycity.de>
2024-04-24 14:48:50 +0200

    URL Migration: Add docs as reminder to sync param names

--- src/middleware.ts
+++ src/middleware.ts
@@ -68,6 +68,7 @@ export function middleware(request: NextRequest) {
   migratedUrl = migrateUrl(migratedUrl)
 
   // Remove unused params
+  // List had to be in sync with `use*Param`s, see `useQueryState/README.md`
   const usedParams = ['map', 'config', 'f', 'v', 'bg', 'data', 'debugMap', 'draw', 'osmNotes']
   const u = new URL(migratedUrl)
   Array.from(u.searchParams.keys()).forEach((key) => {
