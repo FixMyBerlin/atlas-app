cc4a27ce18e04477ebbec247567b66a24c5c85b3
Tobias <tobias@fixmycity.de>
2024-04-29 12:29:37 +0200

    Refactoring: Remove double assign, simplify TS

--- src/app/regionen/[regionSlug]/_hooks/useQueryState/useFeaturesParam/useSelectedFeatures.ts
+++ src/app/regionen/[regionSlug]/_hooks/useQueryState/useFeaturesParam/useSelectedFeatures.ts
@@ -10,17 +10,17 @@ type Result = {
   mapFeature: MapGeoJSONFeature | null
 }
 
-const emptyArray: Result[] = []
+// State variables
 let prevMapLoaded = false
 let prevMapBounds: Store['mapBounds'] = null
-let prevResult = emptyArray
+let prevResult: Result[] = []
 
 export const useSelectedFeatures = () => {
   const { mainMap: map } = useMap()
   const { mapLoaded, mapBounds } = useMapStateInteraction()
   const { featuresParam } = useFeaturesParam()
 
-  if (!map || !mapLoaded || !mapBounds || !featuresParam) return emptyArray
+  if (!map || !mapLoaded || !mapBounds || !featuresParam) return []
   if (mapLoaded === prevMapLoaded && mapBounds === prevMapBounds) return prevResult
 
   // code below only runs if map is loaded and map bounds has changed
@@ -30,7 +30,6 @@ export const useSelectedFeatures = () => {
     const mapFeature = findFeature(renderedFeatures, urlFeature.properties) || null
     return { urlFeature, mapFeature }
   })
-  prevResult = result
 
   prevMapLoaded = mapLoaded
   prevMapBounds = mapBounds
