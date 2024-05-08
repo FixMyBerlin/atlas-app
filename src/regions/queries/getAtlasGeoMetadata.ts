ae6c8f525ca1463bb83ff4b3c248b1bd84d1ba9e
Tobias <tobias@fixmycity.de>
2024-05-08 18:34:19 +0200

    Meta: Fix auth error
    This query is open for any user; before it crashed the app with an unhandled auth error.

--- src/regions/queries/getAtlasGeoMetadata.ts
+++ src/regions/queries/getAtlasGeoMetadata.ts
@@ -1,14 +1,17 @@
 import { resolver } from '@blitzjs/rpc'
 import db from 'db'
 
-export default resolver.pipe(resolver.authorize(/* ok */), async ({}) => {
-  type Query = { processed_at: string; osm_data_from: string }[]
-  const result = await db.$queryRaw<Query>`
+export default resolver.pipe(
+  // resolver.authorize(/* ok */), // Open without Auth
+  async ({}) => {
+    type Query = { processed_at: string; osm_data_from: string }[]
+    const result = await db.$queryRaw<Query>`
       SELECT processed_at, osm_data_from
       FROM public.meta
       ORDER BY id DESC
       LIMIT 1
     `
 
-  return result[0]
-})
+    return result[0]
+  },
+)
