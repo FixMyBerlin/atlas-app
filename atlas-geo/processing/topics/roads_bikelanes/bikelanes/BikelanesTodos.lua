a3c23d9151a62588951217c6fda76a5f0f717a47
Tobias <tobias@fixmycity.de>
2024-05-08 09:15:55 +0200

    BikelanesTodos: Rework to use table as args

--- atlas-geo/processing/topics/roads_bikelanes/bikelanes/BikelanesTodos.lua
+++ atlas-geo/processing/topics/roads_bikelanes/bikelanes/BikelanesTodos.lua
@@ -1,103 +1,107 @@
 TodoIssue = {}
 TodoIssue.__index = TodoIssue
 
-function TodoIssue.new(identifier, description, conditionFunc)
+-- @param args table
+-- @param args.key string
+-- @param args.desc string
+-- @param args.conditions function
+function TodoIssue.new(args)
   local self = setmetatable({}, TodoIssue)
-  self.identifier = identifier
-  self.description = description
-  self.conditionFunc = conditionFunc
+  self.key = args.key
+  self.desc = args.desc
+  self.conditions = args.conditions
   return self
 end
 
 function TodoIssue:checkCondition(objectTags, resultTags)
-  if self.conditionFunc(objectTags, resultTags) then
-    return self.identifier
+  if self.conditions(objectTags, resultTags) then
+    return self.key
   else
     return nil
   end
 end
 
 -- === Fahrradstraßen ===
-local missing_traffic_sign_244 = TodoIssue.new(
-  "missing_traffic_sign_244",
-  "Expecting tag `traffic_sign=DE:244.1` or similar.",
-  function(tagsObject, _)
+local missing_traffic_sign_244 = TodoIssue.new({
+  key = "missing_traffic_sign_244",
+  desc = "Expecting tag `traffic_sign=DE:244.1` or similar.",
+  conditions = function(tagsObject, _)
     return tagsObject.bicycle_road == "yes" and not IsTermInString('DE:244.1', tagsObject.traffic_sign)
   end
-)
-local missing_traffic_sign_vehicle_destination = TodoIssue.new(
-  "missing_traffic_sign_vehicle_destination",
-  "Expecting tag traffic_sign 'Anlieger frei' `traffic_sign=DE:244.1,1020-30` or similar.",
-  function(tagsObject, _)
+})
+local missing_traffic_sign_vehicle_destination = TodoIssue.new({
+  key = "missing_traffic_sign_vehicle_destination",
+  desc = "Expecting tag traffic_sign 'Anlieger frei' `traffic_sign=DE:244.1,1020-30` or similar.",
+  conditions = function(tagsObject, _)
     return tagsObject.bicycle_road == "yes"
         and tagsObject.vehicle == "destination"
         and IsTermInString("1020-30", tagsObject.traffic_sign)
   end
-)
-local missing_acccess_tag_bicycle_road = TodoIssue.new(
-  "missing_acccess_tag_bicycle_road",
-  "Expected access tag `bicycle=designated` that is required for routing.",
-  function(tagsObject, _)
+})
+local missing_acccess_tag_bicycle_road = TodoIssue.new({
+  key = "missing_acccess_tag_bicycle_road",
+  desc = "Expected access tag `bicycle=designated` that is required for routing.",
+  conditions = function(tagsObject, _)
     return tagsObject.bicycle_road == "yes"
         and (tagsObject.vehicle == "no" or tagsObject.vehicle == "destination")
         and tagsObject.bicycle == 'designated'
   end
-)
+})
 
 -- === Verkehrszeichen ===
-local missing_traffic_sign = TodoIssue.new(
-  "missing_traffic_sign",
-  "Expected tag `traffic_sign=DE:*` or `traffic_sign=none`.",
-  function(tagsObject, _)
+local missing_traffic_sign = TodoIssue.new({
+  key = "missing_traffic_sign",
+  desc = "Expected tag `traffic_sign=DE:*` or `traffic_sign=none`.",
+  conditions = function(tagsObject, _)
     return tagsObject.traffic_sign == nil
         and not (
           missing_traffic_sign_244:checkCondition(tagsObject) or
           missing_traffic_sign_vehicle_destination:checkCondition(tagsObject)
         )
   end
-)
+})
 
 -- === Fuß- und Radweg ===
-local missing_access_tag_240 = TodoIssue.new(
-  "missing_access_tag_240",
-  "Expected tag `bicycle=designated` and `foot=designated`.",
-  function(tagsObject, _)
+local missing_access_tag_240 = TodoIssue.new({
+  key = "missing_access_tag_240",
+  desc = "Expected tag `bicycle=designated` and `foot=designated`.",
+  conditions = function(tagsObject, _)
     return (IsTermInString('DE:240', tagsObject.traffic_sign) or IsTermInString('DE:241', tagsObject.traffic_sign))
         and tagsObject.bicycle ~= 'designated' and tagsObject.foot ~= "designated"
   end
-)
-local missing_segregated = TodoIssue.new(
-  "missing_segregated",
-  "Expected tag `segregated=yes` or `segregated=no`.",
-  function(tagsObject, resultTags)
+})
+local missing_segregated = TodoIssue.new({
+  key = "missing_segregated",
+  desc = "Expected tag `segregated=yes` or `segregated=no`.",
+  conditions = function(tagsObject, resultTags)
     return resultTags.category == "needsClarification"
         and (
           (tagsObject.bicycle == "designated" and tagsObject.foot == "designated")
           or osm2pgsql.has_prefix(tagsObject.traffic_sign, "DE:240")
         )
   end
-)
+})
 
 -- === Sidepath ===
-local missing_sidepath = TodoIssue.new(
-  "missing_sidepath",
-  "Expected tag `is_sidepath=yes` or `is_sidepath=no`.",
-  function(_, resultTags)
+local missing_sidepath = TodoIssue.new({
+  key = "missing_sidepath",
+  desc = "Expected tag `is_sidepath=yes` or `is_sidepath=no`.",
+  conditions = function(_, resultTags)
     return resultTags.category == "footAndCyclewayShared_adjoiningOrIsolated"
         or resultTags.category == "footAndCyclewaySegregated_adjoiningOrIsolated"
         or resultTags.category == "footwayBicycleYes_adjoiningOrIsolated"
         or resultTags.category == "cycleway_adjoiningOrIsolated"
   end
-)
+})
 
 -- === cycleway:side=lane subcategory ===
-local missing_cycleway_lane = TodoIssue.new(
-  "missing_cycleway_lane",
-  "Expected tag `cycleway:lane=advisory` or `cycleway:lane=exclusive`.",
-  function(_, resultTags)
+local missing_cycleway_lane = TodoIssue.new({
+  key = "missing_cycleway_lane",
+  desc = "Expected tag `cycleway:lane=advisory` or `cycleway:lane=exclusive`.",
+  conditions = function(_, resultTags)
     return resultTags.category == "cyclewayOnHighway_advisoryOrExclusive"
   end
-)
+})
 
 -- Public funtion
 function BikelanesTodos(tagsObject, resultTags)
