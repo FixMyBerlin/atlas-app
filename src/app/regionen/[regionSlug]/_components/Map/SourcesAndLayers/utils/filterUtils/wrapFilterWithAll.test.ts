700e035be89bdda942e01a49e6fe868a5e294934
Tobias <tobias@fixmycity.de>
2024-05-15 17:14:32 +0200

    Map: Rework `wrapFilterWithAll` to flatten filters first

--- src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.test.ts
+++ src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { wrapFilterWithAll } from './wrapFilterWithAll'
+import { _flattenFilter, wrapFilterWithAll } from './wrapFilterWithAll'
 
 describe('wrapFilterWithAll()', () => {
   test('"undefined" input returns an empty "all" wrapper', () => {
@@ -28,5 +28,53 @@ describe('wrapFilterWithAll()', () => {
     const filter = ['==', 'id', 'none']
     const result = wrapFilterWithAll(filter)
     expect(result).toMatchObject(['all', filter])
+  })
+
+  test('mapillary highlighting filter', () => {
+    const filter = [[['==', ['get', 'is_pano'], true]], ['==', 'id', 123]]
+    const result = wrapFilterWithAll(filter)
+    expect(result).toMatchObject(['all', ['==', ['get', 'is_pano'], true], ['==', 'id', 123]])
+  })
+})
+
+describe('flattenFilter()', () => {
+  test('handels undefined', () => {
+    const result = _flattenFilter(undefined)
+    expect(result).toMatchObject([])
+  })
+  test('clenaup one "all"', () => {
+    const result = _flattenFilter(['all'])
+    expect(result).toMatchObject([])
+  })
+  test('cleanup multiple "all"', () => {
+    const filter = ['has', 'foo']
+    const result = _flattenFilter(['all', 'all', filter, 'all'])
+    expect(result).toMatchObject([filter])
+  })
+  test('filter expressions passed directly DO NOT WORK and are handeled in wrapFilterWithAll', () => {
+    const filter = ['has', 'foo']
+    const result = _flattenFilter(filter)
+    expect(result).toMatchObject([])
+  })
+  test('handel 1 level', () => {
+    const filter = ['has', 'foo']
+    const result = _flattenFilter([filter])
+    expect(result).toMatchObject([filter])
+  })
+  test('handel 1 level', () => {
+    const filter = ['has', 'foo']
+    const result = _flattenFilter([filter, ['all', filter]])
+    expect(result).toMatchObject([filter, filter])
+  })
+  test('handel multipe levels', () => {
+    const filter1 = ['==', 'id', 'none']
+    const filter2 = ['==', 'is_pano', !0]
+    const result = _flattenFilter([
+      filter1,
+      ['all', filter2],
+      ['all', ['all', ['all'], filter1], filter2],
+      filter1,
+    ])
+    expect(result).toMatchObject([filter1, filter2, filter1, filter2, filter1])
   })
 })
--- src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.ts
+++ src/app/regionen/[regionSlug]/_components/Map/SourcesAndLayers/utils/filterUtils/wrapFilterWithAll.ts
@@ -1,20 +1,44 @@
 // See https://github.com/maplibre/maplibre-style-spec/blob/main/build/generate-style-spec.ts#L192
 import { type ExpressionSpecification } from 'maplibre-gl'
 
+/** @desc Private method, exported only for testing */
+export const _flattenFilter = (filterArray: any[] | undefined) => {
+  if (!Array.isArray(filterArray)) {
+    return []
+  }
+
+  const result: any[] = []
+  // TS: Array.isArray will break TS and https://github.com/total-typescript/ts-reset/pull/23 does not fix it, yet. So we have to cast `as any[]` again here
+  for (const item of filterArray as any[]) {
+    if (Array.isArray(item) && typeof item[0] === 'string' && item[0] !== 'all') {
+      // If item is an array and its first element is a string which is not "all", add it to the result
+      result.push(item)
+    } else {
+      // Recursively process item and add the results to the result
+      result.push(..._flattenFilter(item))
+    }
+  }
+
+  return result
+}
+
 export const wrapFilterWithAll = (filterArray: any[] | undefined) => {
-  // Case: Input empty/undefined
-  if (!filterArray) {
+  // Case: Filter expression is passed directly, no wrapping array
+  if (
+    Array.isArray(filterArray) &&
+    typeof filterArray[0] === 'string' &&
+    filterArray[0] !== 'all'
+  ) {
+    // @ts-expect-error no idea what TS wants, but this should be fine
+    return ['all', filterArray] as ExpressionSpecification
+  }
+
+  // Case: Array of filter expressions that we cleanup recursively and then wrap
+  const flatFilterArray = _flattenFilter(filterArray)
+
+  if (flatFilterArray.length === 0) {
     return ['all'] as ExpressionSpecification
   }
-  // Case: Input already wrapped in 'all'
-  if (filterArray[0] === 'all') {
-    return filterArray.filter(Boolean) as ExpressionSpecification
-  }
-  // Case: Input is multi dimensional array of filters
-  if (typeof filterArray[0] === 'object') {
-    return ['all', filterArray].flat().filter(Boolean) as ExpressionSpecification
-  }
-  // Case: Input is only one filter
-  // @ts-expect-error (this line should be less of a problem that a `as unkown`?)
-  return ['all', filterArray] as ExpressionSpecification
+
+  return ['all', ...flatFilterArray] as ExpressionSpecification
 }
