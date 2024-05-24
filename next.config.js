2d7b653dfdae900d97f6b847434d2378992319d9
Tobias <tobias@fixmycity.de>
2024-05-24 13:32:10 +0200

    React Compiler: Enable strict mode
    Following https://react.dev/learn/react-compiler
    
    Make sure we have strict mode on in NextJS which apparently word differently for NextJS

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const { withBlitz } = require('@blitzjs/next')
-const { withNextAuthAdapter } = require("@blitzjs/auth/next-auth")
+const { withNextAuthAdapter } = require('@blitzjs/auth/next-auth')
 
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
  **/
 module.exports = withBlitz(
   withNextAuthAdapter({
+    // StrictMode: Should be default, but just in case…
+    // Docs: https://nextjs.org/docs/app/api-reference/next-config-js/reactStrictMode
+    // See also: https://github.com/facebook/react/issues/29130
+    reactStrictMode: true,
     experimental: {
       // https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links
       typedRoutes: true,
