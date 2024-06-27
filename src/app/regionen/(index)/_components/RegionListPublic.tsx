f6dce3b542b1f59bf2e9fad03f35ac9234044807
Tobias <tobias@fixmycity.de>
2024-06-27 07:27:58 +0200

    /regionen: Improve wording, styling

--- src/app/regionen/(index)/_components/RegionListPublic.tsx
+++ src/app/regionen/(index)/_components/RegionListPublic.tsx
@@ -7,16 +7,16 @@ export const RegionListPublic = async () => {
   const publicRegions = await invoke(getRegions, { where: { public: true } })
 
   return (
-    <div className="bg-transparent">
-      <div className="mx-auto max-w-7xl overflow-hidden sm:px-6 lg:px-8">
-        <h2 className="sr-only">Regionen</h2>
+    <div className="mx-auto max-w-7xl overflow-hidden sm:px-6 lg:px-8">
+      <div className="prose mt-5 px-4 sm:px-0">
+        <h2>Öffentliche Regionen</h2>
+      </div>
 
-        <div className="my-10 grid grid-cols-2 border-l border-t border-gray-200 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
-          {publicRegions?.map((region) => <RegionTeaser key={region.slug} region={region} />)}
-          {publicRegions?.length === 0 && (
-            <div className="p-4 font-semibold text-gray-400">Keine Regionen</div>
-          )}
-        </div>
+      <div className="my-10 grid grid-cols-2 border-l border-t border-gray-200 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
+        {publicRegions?.map((region) => <RegionTeaser key={region.slug} region={region} />)}
+        {publicRegions?.length === 0 && (
+          <div className="p-4 font-semibold text-gray-400">Keine Regionen</div>
+        )}
       </div>
     </div>
   )
