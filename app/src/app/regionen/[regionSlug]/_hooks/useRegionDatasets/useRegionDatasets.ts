b3bf399bd3ecf987b6c101a0b0464bbcb19f6adf
Sven König <sven@de-selby.com>
2024-07-17 09:54:04 +0200

    improve memoization of useRegionDatasets

--- app/src/app/regionen/[regionSlug]/_hooks/useRegionDatasets/useRegionDatasets.ts
+++ app/src/app/regionen/[regionSlug]/_hooks/useRegionDatasets/useRegionDatasets.ts
@@ -6,39 +6,41 @@ import { getStaticDatasetUrl } from '../../../../_components/utils/getStaticData
 import { useRegionSlug } from '../../_components/regionUtils/useRegionSlug'
 import { Prettify } from 'src/app/_components/types/types'
 
-const memoized = memoize(
-  (regionDatasets) => regionDatasets,
-  (regionDatasets) => regionDatasets.map((r) => r.id).join(),
+type RegionDataset = Prettify<
+  MetaData['configs'][number] & {
+    isPublic: boolean
+    id: string
+    url: string
+    type: 'PMTILES' | 'GEOJSON'
+    githubUrl: string // an addition to MetaData['configs'] in updateStaticDatasets.ts
+  }
+>
+
+const getDatasets = memoize(
+  (uploads) => {
+    const regionDatasets: RegionDataset[] = []
+    uploads.forEach((upload) => {
+      upload.configs.forEach((config) => {
+        regionDatasets.push({
+          ...config,
+          isPublic: upload.public,
+          id: upload.slug,
+          type: upload.type,
+          url: getStaticDatasetUrl(upload.slug),
+        })
+      })
+    })
+    return regionDatasets
+  },
+  (uploads) => uploads.map((upload) => upload.id).join(),
 )
 
 export const useRegionDatasets = () => {
   const regionSlug = useRegionSlug()
-  const [uploads] = useQuery(getUploadsForRegion, { regionSlug: regionSlug! })
-
-  type RegionDataset = Prettify<
-    MetaData['configs'][number] & {
-      isPublic: boolean
-      id: string
-      url: string
-      type: 'PMTILES' | 'GEOJSON'
-      githubUrl: string // an addition to MetaData['configs'] in updateStaticDatasets.ts
-    }
-  >
-  const regionDatasets: RegionDataset[] = []
-  uploads.forEach((upload) => {
-    // @ts-expect-error
-    upload.configs.forEach((config) => {
-      regionDatasets.push({
-        ...config,
-        isPublic: upload.public,
-        id: upload.slug,
-        type: upload.type,
-        url: getStaticDatasetUrl(upload.slug),
-      })
-    })
-  })
-
-  regionDatasets.sort((a, b) => a.name.localeCompare(b.name))
-
-  return memoized(regionDatasets) as typeof regionDatasets
+  const [uploads] = useQuery(
+    getUploadsForRegion,
+    { regionSlug: regionSlug! },
+    { cacheTime: Infinity },
+  )
+  return getDatasets(uploads)
 }
