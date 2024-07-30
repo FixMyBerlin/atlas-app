891a342a65c834db3f1c1ecbd44a2f1d63f31a7d
Tobias <tobias@fixmycity.de>
2024-07-30 16:54:46 +0200

    Radplus: Add categories to static data

--- app/src/app/regionen/[regionSlug]/_mapData/mapDataStaticDatasetCategories/staticDatasetCategories.const.ts
+++ app/src/app/regionen/[regionSlug]/_mapData/mapDataStaticDatasetCategories/staticDatasetCategories.const.ts
@@ -47,4 +47,14 @@ export const staticDatasetCategories = {
     title: 'Daten für nudafa.de',
     subtitle: 'Statische Daten die auf nudafa.de verwendet werden.',
   },
+  'radplus/fahrten': {
+    order: 1,
+    title: 'Rad+ Fahrten',
+    subtitle: 'Statische Daten der Fahrten von Rad+ Nutzer:innen.',
+  },
+  'radplus/radaparken': {
+    order: 2,
+    title: 'radparken.info',
+    subtitle: 'Statische Daten der radparken.info Umfragen.',
+  },
 } as const
