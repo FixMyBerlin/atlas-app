2e1c39593d073df2c2faa3b3603b796d7fdb9832
Tobias <tobias@fixmycity.de>
2024-08-06 09:12:30 +0200

    Map: Disable "locate mit" until fixed
    It does mess with the calculation tool…

--- app/src/app/regionen/[regionSlug]/_components/Map/Map.tsx
+++ app/src/app/regionen/[regionSlug]/_components/Map/Map.tsx
@@ -218,7 +218,7 @@ export const Map = () => {
       {isDev ? <SourceGeojson /> : null}
       <AttributionControl compact={true} position="bottom-left" />
 
-      <NavigationControl showCompass={true} visualizePitch={true} />
+      <NavigationControl showCompass={false /* TODO: See Story */} visualizePitch={true} />
       <Calculator />
       {/* <GeolocateControl /> */}
       {/* <ScaleControl /> */}
