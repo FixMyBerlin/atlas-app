f1b9b7042190860538532541f71b003a5d9f971e
Tobias <tobias@fixmycity.de>
2024-08-13 09:33:43 +0200

    Revert "show progress cursor if mapDataLoading"
    This reverts commit 0eeb8fe01e2ead5b279dddd12b64abd512c04c1b.
    
    This gives too much visibility to the loading state which is often times not relevant because the main data is loaded already. We can ignore the loading state more easily when we only use the FYI type loading indicator that we already have.

--- app/src/app/regionen/[regionSlug]/_components/Map/Map.tsx
+++ app/src/app/regionen/[regionSlug]/_components/Map/Map.tsx
@@ -14,11 +14,7 @@ import {
 } from 'react-map-gl/maplibre'
 import { isDev } from 'src/app/_components/utils/isEnv'
 import { useMapParam } from 'src/app/regionen/[regionSlug]/_hooks/useQueryState/useMapParam'
-import {
-  useMapInspectorFeatures,
-  useMapActions,
-  useMapDataLoading,
-} from '../../_hooks/mapState/useMapState'
+import { useMapInspectorFeatures, useMapActions } from '../../_hooks/mapState/useMapState'
 import {
   convertToUrlFeature,
   useFeaturesParam,
@@ -72,7 +68,6 @@ export const Map = () => {
   const isSmBreakpointOrAbove = useBreakpoint('sm')
   const region = useStaticRegion()
   const [cursorStyle, setCursorStyle] = useState('grab')
-  const mapDataLoading = useMapDataLoading()
   const regionDatasets = useRegionDatasets()
 
   // Position the map when URL change is triggered from the outside (eg a Button that changes the URL-state to move the map)
@@ -240,7 +235,7 @@ export const Map = () => {
       interactiveLayerIds={interactiveLayerIds}
       // onMouseMove={}
       // onLoad={handleInspect}
-      cursor={mapDataLoading ? 'progress' : cursorStyle}
+      cursor={cursorStyle}
       onMoveEnd={handleMoveEnd}
       // onZoomEnd={} // zooming is always also moving
       onClick={handleClick}
