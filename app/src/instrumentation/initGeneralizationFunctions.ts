83700d48e8c161fb171b00caa5b8c2fe693fdb5a
Laurenz Rasche <laurenz@fixmycity.de>
2024-08-26 13:05:23 +0200

    explicitly name schema in  `initGeneralizationFunctions`

--- app/src/instrumentation/initGeneralizationFunctions.ts
+++ app/src/instrumentation/initGeneralizationFunctions.ts
@@ -50,7 +50,7 @@ export async function initGeneralizationFunctions(
           prismaClientForRawQueries.$executeRaw`SET search_path TO public;`,
           prismaClientForRawQueries.$executeRawUnsafe(
             `CREATE OR REPLACE
-          FUNCTION public.${functionName}(z integer, x integer, y integer)
+          FUNCTION public."${functionName}"(z integer, x integer, y integer)
           RETURNS bytea AS $$
           DECLARE
             mvt bytea;
@@ -73,7 +73,7 @@ export async function initGeneralizationFunctions(
                   stylingKeys,
                 )}) END as tags,
                 CASE WHEN z >= ${minzoom} THEN meta ELSE NULL END as meta
-              FROM "${tableName}"
+              FROM public."${tableName}"
               WHERE (geom && ST_TileEnvelope(z, x, y))
               AND z >= minzoom
             ) AS tile;
