e7d1e1580404614cc5b7165d9df16e3e286e21cd
Laurenz Rasche <laurenz@fixmycity.de>
2024-08-27 16:54:02 +0200

    refine instrumentation logging

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -9,8 +9,13 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     try {
       await initCustomFunctions()
-      initExportFunctions(exportApiIdentifier)
-      initGeneralizationFunctions(interactivityConfiguration)
+      console.log(' ✓ Custom SQL functions initialized')
+      initExportFunctions(exportApiIdentifier).then(() =>
+        console.log(' ✓ Export functions initialized'),
+      )
+      initGeneralizationFunctions(interactivityConfiguration).then(() =>
+        console.log(' ✓ Generalization functions initialized'),
+      )
     } catch (e) {
       console.error('\n\nINSTRUMENTATION HOOK FAILED', e)
     }
