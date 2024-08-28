1f655a42f9625f5ed04a7b89c810b20cea32f01f
Laurenz Rasche <laurenz@fixmycity.de>
2024-08-28 15:14:34 +0200

    use ternary for `DATABASE_URL` in `geoDataClient`

--- app/src/prisma-client.ts
+++ app/src/prisma-client.ts
@@ -2,6 +2,12 @@ import { PrismaClient } from '@prisma/client'
 
 // This is the client for accessing the geo data.
 // It allows direct SQL queries to the database, so it should be used cautiously as it bypasses Prisma's integrated security checks.
+
+const url = process.env.DATABASE_URL ? process.env.DATABASE_URL.replace(/\?schema.*/, '') : ''
 export const geoDataClient = new PrismaClient({
-  datasourceUrl: (process.env.DATABASE_URL || '').replace(/\?schema.*/, ''),
+  datasources: {
+    db: {
+      url,
+    },
+  },
 })
