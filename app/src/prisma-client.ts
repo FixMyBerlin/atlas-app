3ddcd8c5802d9ebd07cc3b4fe83e00154a67ea3f
Laurenz Rasche <laurenz@fixmycity.de>
2024-08-28 11:44:31 +0200

    geoDataClient: fallback to empty string

--- app/src/prisma-client.ts
+++ app/src/prisma-client.ts
@@ -3,5 +3,5 @@ import { PrismaClient } from '@prisma/client'
 // This is the client for accessing the geo data.
 // It allows direct SQL queries to the database, so it should be used cautiously as it bypasses Prisma's integrated security checks.
 export const geoDataClient = new PrismaClient({
-  datasourceUrl: (process.env.DATABASE_URL as string).replace(/\?schema.*/, ''),
+  datasourceUrl: ((process.env.DATABASE_URL || '') as string).replace(/\?schema.*/, ''),
 })
