b3645590c942d8d429e62ef2d14166343cd97f0a
Laurenz Rasche <laurenz@fixmycity.de>
2024-08-29 12:30:42 +0200

    fix dockerized hostname issues

--- app/src/app/_components/utils/isEnv.ts
+++ app/src/app/_components/utils/isEnv.ts
@@ -2,7 +2,8 @@ export const isProd = process.env.NEXT_PUBLIC_APP_ENV === 'production'
 
 export const isStaging = process.env.NEXT_PUBLIC_APP_ENV === 'staging'
 
-export const isDev = process.env.NEXT_PUBLIC_APP_ENV === 'development'
+export const isDev =
+  process.env.NEXT_PUBLIC_APP_ENV === 'development' && process.env.NODE_ENV === 'development'
 
 export const isBrowser = typeof window !== 'undefined'
 
