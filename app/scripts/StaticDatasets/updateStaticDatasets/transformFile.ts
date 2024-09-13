04a5cd144788c596df273ae7d703cf01807a280d
Tobias <tobias@fixmycity.de>
2024-09-13 08:14:39 +0200

    StaticData: Refactor out `getDecompressedFilename`

--- app/scripts/StaticDatasets/updateStaticDatasets/transformFile.ts
+++ app/scripts/StaticDatasets/updateStaticDatasets/transformFile.ts
@@ -1,8 +1,7 @@
 import path from 'node:path'
-import fs from 'node:fs'
 import { import_ } from '../updateStaticDatasets'
-import { red } from '../utils/log'
 import { addUniqueIds } from './addUniqueIds'
+import { getDecompressedFilename } from './getDecompressedFilename'
 
 /** @returns geojson outputFullFilename which is either the initial geojson or the transformed geojson  */
 export const transformFile = async (
@@ -11,30 +10,16 @@ export const transformFile = async (
   outputFolder: string,
 ) => {
   const datasetFolderName = datasetFolderPath.split('/').at(-1)
+  const filenameToRead = getDecompressedFilename({
+    inputFilename: geojsonFullFilename,
+    outputFilename: datasetFolderName,
+    outputFolder,
+  })
 
-  let filenameToRead = geojsonFullFilename
-  if (path.parse(geojsonFullFilename).ext === '.gz') {
-    // When our input is zipped, we copy the file to the temp folder and unzip it, which replaces the given file
-    console.log(`  Unzipping file...`)
-    const compressedCopy = path.join(outputFolder, `${datasetFolderName}.decompressed.geojson.gz`)
-    // Using Bun.spanSync to copy the file did not work; using Bun to copy did not work either.
-    fs.copyFileSync(geojsonFullFilename, compressedCopy)
-
-    const bunFeedback = Bun.spawnSync(['gzip', '--decompress', '--force', compressedCopy], {})
-    if (bunFeedback.success === false) {
-      red(
-        'ERROR with Bun.spawnSync gzip decompress',
-        bunFeedback.stdout.toString(),
-        bunFeedback.stderr.toString(),
-      )
-    }
-    filenameToRead = compressedCopy.replace('.gz', '')
-  }
   let data = await Bun.file(filenameToRead).json()
 
   type TransformFunc = (data: any) => string
   const transform = await import_<TransformFunc>(datasetFolderPath, 'transform', 'transform')
-
   if (transform !== null) {
     console.log(`  Transforming geojson file...`)
     data = transform(data)
