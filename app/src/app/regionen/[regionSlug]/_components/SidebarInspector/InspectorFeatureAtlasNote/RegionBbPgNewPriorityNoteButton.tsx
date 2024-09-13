876014f5269b2942dbbc691d2af06beab83b7a68
Tobias <tobias@fixmycity.de>
2024-09-13 16:55:50 +0200

    RegionBbPgNewPriorityNoteButton: Updated interaction and wording
    - activate notes when first note is set
    - move map to show note in center
    - hide button after note was saved and show success

--- app/src/app/regionen/[regionSlug]/_components/SidebarInspector/InspectorFeatureAtlasNote/RegionBbPgNewPriorityNoteButton.tsx
+++ app/src/app/regionen/[regionSlug]/_components/SidebarInspector/InspectorFeatureAtlasNote/RegionBbPgNewPriorityNoteButton.tsx
@@ -1,28 +1,38 @@
 import { useSession } from '@blitzjs/auth'
 import { getQueryClient, useMutation } from '@blitzjs/rpc'
+import { useState } from 'react'
+import { useMap } from 'react-map-gl/maplibre'
 import { SmallSpinner } from 'src/app/_components/Spinner/SmallSpinner'
 import { buttonStylesOnYellow } from 'src/app/_components/links/styles'
 import createNote from 'src/notes/mutations/createNote'
+import { useShowAtlasNotesParam } from '../../../_hooks/useQueryState/useNotesAtlasParams'
 import { useQueryKey } from '../../notes/AtlasNotes/utils/useQueryKey'
 import { useRegionSlug } from '../../regionUtils/useRegionSlug'
 import { InspectorFeature } from '../Inspector'
 import { pointFromGeometry } from '../Tools/osmUrls/pointFromGeometry'
-import { Layer } from 'react-map-gl'
 
 export const RegionBbPgNewPriorityNoteButton = ({ feature }: Pick<InspectorFeature, 'feature'>) => {
+  const { setShowAtlasNotesParam } = useShowAtlasNotesParam()
   const [createNoteMutation, { isLoading, error }] = useMutation(createNote)
   const regionSlug = useRegionSlug()
   const queryKey = useQueryKey()
   const session = useSession()
   const isAuthenticated = session.osmToken !== null
+  const { mainMap } = useMap()
+
+  const [formSubmitted, setFormSubmitted] = useState(false)
 
   if (regionSlug !== 'bb-pg') return null
   if (!isAuthenticated) return null
   if (!feature.layer.id.includes('bb-ramboll-umgelegte-linien')) return null
 
-  const handleSubmit = async (importance: 'sehr wichtig' | 'wichtig' | 'weniger wichtig') => {
+  const handleSubmit = async (
+    importance: 'höchste Priorität' | 'hohe Priorität' | 'geringere Priorität',
+  ) => {
+    setShowAtlasNotesParam(true)
+
     const body = `
-Wichtigkeit: ${importance}
+Priorität: ${importance}
 
 Verbindungs-ID: ${feature.properties.Verbindung}
 
@@ -39,13 +49,19 @@ ID: ${feature.properties.id}
     createNoteMutation(
       {
         regionSlug,
-        subject: `Wichtigkeit: ${importance}`,
+        subject: `Hohe Prioritätkeit: ${importance}`,
         latitude: point[1],
         longitude: point[0],
         body,
       },
       {
         onSuccess: () => {
+          mainMap?.flyTo({ center: point, padding: { left: 270, right: 550 } })
+          // mainMap?.fitBounds(
+          //   buffer(feature.geometry, 100).geometry.bbox as [number, number, number, number],
+          // )
+
+          setFormSubmitted(true)
           getQueryClient().invalidateQueries(queryKey)
         },
       },
@@ -59,42 +75,54 @@ ID: ${feature.properties.id}
           Einen Prioritäten-Hinweis hinterlassen
         </h2>
       </div>
-      <form className="p-4">
-        <p className="mb-3 leading-snug">
-          Diese speziellen interne Hinweise sind nur für angemeldete Nutzer:innen sichtbar, die für
-          diese Region freigeschaltet wurden. Sie erzeugen automatisch einen Hinweis entlang der
-          ausgewählten Strecke mit vorausgefüllter Beschreibung.
-        </p>
-        <div className="flex items-center gap-1 leading-tight">
-          <button
-            type="button"
-            onClick={() => handleSubmit('sehr wichtig')}
-            className={buttonStylesOnYellow}
-            disabled={isLoading}
-          >
-            Sehr wichtig
-          </button>
-          <button
-            type="button"
-            onClick={() => handleSubmit('wichtig')}
-            className={buttonStylesOnYellow}
-            disabled={isLoading}
-          >
-            Wichtig
-          </button>
-          <button
-            type="button"
-            onClick={() => handleSubmit('weniger wichtig')}
-            className={buttonStylesOnYellow}
-            disabled={isLoading}
-          >
-            Weniger wichtig
-          </button>
-          {isLoading && <SmallSpinner />}
-        </div>
-        {/* @ts-expect-errors TODO Research how the error message is provided by Blitz */}
-        {error ? <p className="text-red-500">{error.message}</p> : null}
-      </form>
+      {formSubmitted ? (
+        <>
+          <p className="p-3 leading-snug">
+            <strong>Der Prioritäten-Hinweis wurde hinterlegt.</strong>
+            <br />
+            Um Prioritäten zu ändern oder zu löschen klicken Sie auf das Fragezeichen-Icon und dort
+            auf den Stift zum bearbeiten.
+          </p>
+        </>
+      ) : (
+        <form className="p-4">
+          <p className="mb-3 leading-snug">
+            Mit Klick auf einen der Button vergeben Sie eine Priorität für die Verbindungslinie, es
+            erscheint dann ein Pin mit einem Fragezeichen-Icon. Vergeben Sie pro Linie eine
+            Priorität, bitte beachten Sie dabei, dass die drei Prioritäten für Ihre Region
+            ausgewogen sein sollen, jeweils ca. 1/3.
+          </p>
+          <div className="flex items-center gap-1 leading-tight">
+            <button
+              type="button"
+              onClick={() => handleSubmit('höchste Priorität')}
+              className={buttonStylesOnYellow}
+              disabled={isLoading}
+            >
+              höchste Priorität
+            </button>
+            <button
+              type="button"
+              onClick={() => handleSubmit('hohe Priorität')}
+              className={buttonStylesOnYellow}
+              disabled={isLoading}
+            >
+              hohe Priorität
+            </button>
+            <button
+              type="button"
+              onClick={() => handleSubmit('geringere Priorität')}
+              className={buttonStylesOnYellow}
+              disabled={isLoading}
+            >
+              geringere Priorität
+            </button>
+            {isLoading && <SmallSpinner />}
+          </div>
+          {/* @ts-expect-errors TODO Research how the error message is provided by Blitz */}
+          {error ? <p className="text-red-500">{error.message}</p> : null}
+        </form>
+      )}
     </section>
   )
 }
