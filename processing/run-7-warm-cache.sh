d7ba26e97f549ef33d1164008d9e2cc180cf072e
Laurenz Rasche <laurenz@fixmycity.de>
2024-10-01 14:55:40 +0200

    also restart martin when cache warming is disabled

--- processing/run-7-warm-cache.sh
+++ processing/run-7-warm-cache.sh
@@ -4,11 +4,12 @@ set -e
 source /processing/utils/logging.sh
 log_start "$0"
 
+log "Restart Martin"
+docker restart tiles
+
 if [ "${SKIP_WARM_CACHE:-0}" == 1 ]; then
   log "💥 SKIPPED Warm Cache with .env 'SKIP_WARM_CACHE=1'"
 else
-  log "Restart Martin"
-  docker restart tiles
   sleep 10
   log "Flush Cache"
   rm -rf /var/cache/nginx/*
