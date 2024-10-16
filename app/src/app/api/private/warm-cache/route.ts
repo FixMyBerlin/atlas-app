ab8fbd4126e84ec5ff5855fb2f0f8e7602e0fd71
Laurenz Rasche <laurenz@fixmycity.de>
2024-10-16 17:29:51 +0200

    use `zod.safeParse`

--- app/src/app/api/private/warm-cache/route.ts
+++ app/src/app/api/private/warm-cache/route.ts
@@ -1,28 +1,24 @@
 import chalk from 'chalk'
 import { NextRequest, NextResponse } from 'next/server'
-import { isProd } from 'src/app/_components/utils/isEnv'
 import { staticRegion } from 'src/app/regionen/(index)/_data/regions.const'
 import { z } from 'zod'
 import { warmCache } from './warmCache'
 
-const WarmCacheSchema = z.object({
+const Schema = z.object({
   apiKey: z.string(),
 })
 
 export async function GET(req: NextRequest) {
   // Parse and validate the query string
-  let params
-  try {
-    const url = new URL(req.url)
-    params = WarmCacheSchema.parse(Object.fromEntries(url.searchParams.entries()))
-  } catch (e) {
-    if (!isProd) throw e
-    return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
+  const requestUrl = new URL(req.url)
+  const params = Schema.safeParse(Object.fromEntries(requestUrl.searchParams.entries()))
+  if (params.success == false) {
+    console.error("Couldn't parse query string", params.error)
+    return NextResponse.json({ error: 'Invalid input', ...params.error }, { status: 400 })
   }
-
   // Check the API key
   try {
-    if (params.apiKey !== process.env.ATLAS_API_KEY) {
+    if (params.data.apiKey !== process.env.ATLAS_API_KEY) {
       return NextResponse.json({ message: 'Forbidden' }, { status: 403 })
     }
 
