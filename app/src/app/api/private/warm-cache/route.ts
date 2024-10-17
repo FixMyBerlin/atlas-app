a3804c5b5852adbd035c9354b8f72c7d4044bdbb
Laurenz Rasche <laurenz@fixmycity.de>
2024-10-17 17:40:52 +0200

    parallelize over regions to make endpoint non blocking

--- app/src/app/api/private/warm-cache/route.ts
+++ app/src/app/api/private/warm-cache/route.ts
@@ -26,7 +26,7 @@ export async function GET(req: NextRequest) {
       if (region.cacheWarming != undefined && region.bbox != null) {
         const { minZoom, maxZoom, tables } = region.cacheWarming
         console.log(chalk.grey(' ○'), `Warming cache for ${region.slug} (${minZoom}-${maxZoom})`)
-        await warmCache(region.bbox, minZoom, maxZoom, tables)
+        warmCache(region.bbox, minZoom, maxZoom, tables)
         console.log(chalk.bold(chalk.green(' ✓')), `Warmed cache for ${region.slug}`)
       }
     }
