195507ede2997927e03bd42006b00c78c3d964f5
Tobias <tobias@fixmycity.de>
2024-10-18 09:28:10 +0200

    Navigation: Make Login Links more prominent

--- app/src/app/_components/layouts/Header/User/UserLoggedOut.tsx
+++ app/src/app/_components/layouts/Header/User/UserLoggedOut.tsx
@@ -1,16 +1,16 @@
 import { useStartUserLogin } from '@/src/users/hooks/useStartUserLogin'
-import { UserIcon } from '@heroicons/react/24/outline'
+// import { UserIcon } from '@heroicons/react/24/outline'
 
 export const UserLoggedOut = () => {
   const handleLogin = useStartUserLogin()
 
   return (
     <button
-      className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:ml-6 sm:border sm:border-gray-700"
+      className="inline-flex items-center justify-center rounded-md p-2 text-gray-200 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:ml-6 sm:border sm:border-gray-500"
       onClick={handleLogin}
     >
-      <span className="sr-only">Anmelden</span>
-      <UserIcon className="h-6 w-6" aria-hidden="true" />
+      {/* <UserIcon className="h-6 w-6" aria-hidden="true" /> */}
+      Anmelden
     </button>
   )
 }
