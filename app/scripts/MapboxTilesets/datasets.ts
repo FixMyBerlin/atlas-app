d6b36656d6501198b4fcd26fe1ca708e102091c2
Tobias <tobias@fixmycity.de>
2024-10-21 16:21:47 +0200

    Tilesets: Update script to use Flatgeobuf with larger area
    Note: Using node:fs here instead of Bun.write since that stopped working without error

--- app/scripts/MapboxTilesets/datasets.ts
+++ app/scripts/MapboxTilesets/datasets.ts
@@ -8,10 +8,10 @@ const bboxBrandenburg = {
   max: [14.7658159, 53.5590907],
 } satisfies StaticRegion['bbox']
 // BBox: A bit of Berlin and rual areas below (NUDAFA)
-const bboxBerlinPlus = {
-  min: [13.2809, 52.2095],
-  max: [13.825, 52.5528],
-} satisfies StaticRegion['bbox']
+// const bboxBerlinPlus = {
+//   min: [13.2809, 52.2095],
+//   max: [13.825, 52.5528],
+// } satisfies StaticRegion['bbox']
 // BBox: Whole Germany
 const bboxPartOfNorthGermany = {
   min: [6.635773437502678, 50.666899472786724],
@@ -26,77 +26,77 @@ export const tilesetConfigs: Record<
   bicycleParking_points: {
     sourceLayer: 'bicycleParking_points',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.c65ckqta/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   bicycleParking_areas: {
     sourceLayer: 'bicycleParking_areas',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0p8hdib5/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   bikelanes: {
     sourceLayer: 'bikelanes_verified', // Note: Fixed name
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.3i25sspf/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   places: {
     sourceLayer: 'tarmac-places-test-datensatz-5dodjb',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.34e901wi/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   poiClassification: {
     sourceLayer: 'poiClassification',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0wsdyx91/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   publicTransport: {
     sourceLayer: 'publicTransport',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.7fmhzgpi/#14/52.51622/13.39233',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   trafficSigns: {
     sourceLayer: 'atlas-trafficSigns-attp60',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.8trg6z1b/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   landuse: {
     sourceLayer: 'tarmac-landuse-test-datensatz-0r36t3',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.8xhph4k4/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   barrierAreas: {
     sourceLayer: 'atlas-barrierAreas-b9mb1o',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.3m06nhmv/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   barrierLines: {
     sourceLayer: 'atlas-barrierLines-3ft1bu',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.cx602vbw/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   boundaries: {
     sourceLayer: 'atlas-boundaries-66z5h4',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.4u44tl11/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   boundaryLabels: {
     sourceLayer: 'atlas_boundariesLabel-1q1c4n',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.cuem4dsu/#14/52.51622/13.37036',
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   roads: {
     sourceLayer: 'roads',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.0f7p6nhx/#14/52.51622/13.37036',
-    bbox: bboxBerlinPlus,
+    bbox: bboxBrandenburg,
   },
   roadsPathClasses: {
     sourceLayer: 'roadsPathClasses',
     uploadUrl: 'https://studio.mapbox.com/tilesets/hejco.ajf6t9k7/#14/52.51622/13.37036',
-    bbox: bboxBerlinPlus,
+    bbox: bboxBrandenburg,
   },
   bikelanesPresence: {
     sourceLayer: 'bikelanesPresence',
     uploadUrl: '', //
-    bbox: bboxBrandenburg,
+    bbox: bboxPartOfNorthGermany,
   },
   bikeroutes: {
     sourceLayer: 'bikeroutes',
--- app/scripts/MapboxTilesets/process.ts
+++ app/scripts/MapboxTilesets/process.ts
@@ -1,14 +1,14 @@
 // We use bun.sh to run this file
-import chalk from 'chalk'
 import { getExportApiBboxUrl } from '@/src/app/_components/utils/getExportApiUrl'
-import { StaticRegion } from '@/src/app/regionen/(index)/_data/regions.const'
 import { SourceExportApiIdentifier } from '@/src/app/regionen/[regionSlug]/_mapData/mapDataSources/export/exportIdentifier'
+import chalk from 'chalk'
+import fs from 'node:fs'
 import { tilesetConfigs } from './datasets'
 
 console.log(chalk.inverse.bold('START'), __filename)
 
 // Folder
-const folderJson = 'scripts/MapboxTilesets/json'
+const folderFgb = 'scripts/MapboxTilesets/flatgeobuf'
 const folderMbtiles = 'scripts/MapboxTilesets/mbtiles'
 
 console.log(
@@ -19,30 +19,31 @@ Bun.spawnSync(['open', folderMbtiles])
 
 // Fetch, Write, Transform
 for (const dataset of Object.entries(tilesetConfigs)) {
+  console.log('\n')
   const datasetKey = dataset[0] as SourceExportApiIdentifier
   const { sourceLayer, uploadUrl, bbox } = dataset[1]
 
   // Fetch Export API
   const apiKey = process.env.ATLAS_API_KEY
-  // The `apiKey` with skip the region check (hence the `noRegion`)
+  // The `apiKey` will skip the region check (hence the `noRegion`)
   const url = getExportApiBboxUrl('noRegion', datasetKey, bbox, 'staging', apiKey)
-  console.log('\n', chalk.inverse.bold(chalk.yellow('FETCH')), url)
-  const fetchExport = await fetch(url)
+  console.log(chalk.inverse.bold(chalk.yellow('  FETCH')), url)
+  const fetchExportFgb = await fetch(url)
 
-  if (!fetchExport.ok) {
-    console.error('Fetch failed', fetchExport)
+  if (!fetchExportFgb.ok) {
+    console.error('  Fetch failed', fetchExportFgb)
     process.exit()
   }
-  const rawData: any = await fetchExport.json()
 
   // For debugging: Write JSON Response
-  const jsonFile = `${folderJson}/atlas_${datasetKey}.geojson`
-  console.log(chalk.inverse.bold('WRITE'), jsonFile)
-  await Bun.write(jsonFile, JSON.stringify(rawData, undefined, 2))
+  const fgbFile = `${folderFgb}/atlas_${datasetKey}.fgb`
+  console.log(chalk.inverse.bold(chalk.yellow('  WRITE')), fgbFile)
+  const fgbBuffer = await fetchExportFgb.arrayBuffer()
+  fs.writeFileSync(fgbFile, Buffer.from(fgbBuffer))
 
   // Create mbTiles with Tippecanoe
   const mbtilesFile = `${folderMbtiles}/atlas_${datasetKey}.mbtiles`
-  console.log(chalk.inverse.bold('RUN'), 'tippecanoe', mbtilesFile)
+  console.log(chalk.inverse.bold('  RUN'), 'tippecanoe', mbtilesFile)
   Bun.spawnSync(
     [
       'tippecanoe',
@@ -53,7 +54,7 @@ for (const dataset of Object.entries(tilesetConfigs)) {
       '--drop-densest-as-needed', // https://github.com/felt/tippecanoe?tab=readme-ov-file#dropping-a-fraction-of-features-to-keep-under-tile-size-limits
       '--extend-zooms-if-still-dropping', // https://github.com/felt/tippecanoe?tab=readme-ov-file#zoom-levels
       `--layer=${sourceLayer}`, // We need to specify a specifiy layer name which was initially used by Mapbox when we uploaded the geojson files, otherwise the stiles need to be updated…
-      jsonFile,
+      fgbFile,
     ],
     {
       onExit(_proc, exitCode, _signalCode, error) {
@@ -65,11 +66,10 @@ for (const dataset of Object.entries(tilesetConfigs)) {
 
   // Open URL to upload
   if (uploadUrl) {
-    console.log(chalk.inverse.bold('NOW…'), 'upload', mbtilesFile, 'on', uploadUrl)
-    Bun.spawnSync(['open', uploadUrl])
+    console.log(chalk.inverse.bold('  NOW…'), 'upload', mbtilesFile, 'on', uploadUrl)
   } else {
     console.log(
-      chalk.inverse.bold('NOW…'),
+      chalk.inverse.bold('  NOW…'),
       'upload',
       mbtilesFile,
       'to',
