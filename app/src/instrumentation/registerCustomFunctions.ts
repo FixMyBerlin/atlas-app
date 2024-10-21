2dd2b59e83c00d8d1b427025420281303b967050
Laurenz Rasche <laurenz@fixmycity.de>
2024-10-21 11:17:29 +0200

    don't calculate lengths, use tags->'length' instead

--- app/src/instrumentation/registerCustomFunctions.ts
+++ app/src/instrumentation/registerCustomFunctions.ts
@@ -20,58 +20,57 @@ export async function registerCustomFunctions() {
     $$ LANGUAGE plpgsql;`
   // used in analysis
   await geoDataClient.$executeRaw`
-    CREATE OR REPLACE FUNCTION atlas_quantize_linestring(ls Geometry(LineString), res numeric)
+    CREATE OR REPLACE FUNCTION atlas_segmentize_linestring(input_geom Geometry(LineString), input_length FLOAT, res INT)
     RETURNS TABLE(
-      len numeric,
+      length FLOAT,
       geom Geometry(point)
     )
     AS $$
     DECLARE
-      len numeric := st_length(ST_Transform(ls, 25833));
-      n int := greatest(round(len / res), 1)::int;
+      n INT := greatest(round(input_length / res), 1)::INT;
     BEGIN
       RETURN query
       SELECT
-        len / n,
-        ST_LineInterpolatePoint(ls,(generate_series(1, n) - 0.5) / n::numeric) AS geom;
+        input_length / n as length,
+        ST_LineInterpolatePoint(input_geom, (generate_series(1, n) - 0.5) / n) AS geom;
     END;
     $$
     LANGUAGE plpgsql;
   `
   geoDataClient.$executeRaw`
-    CREATE OR REPLACE FUNCTION atlas_count_category_lengths(polygon_input GEOMETRY)
+    CREATE OR REPLACE FUNCTION atlas_count_category_lengths(input_polygon GEOMETRY)
     RETURNS JSONB AS $$
     DECLARE
       category_length JSONB;
     BEGIN
         -- create a temporary table which holds all bikelanes cutted into points with their respective length
       BEGIN
-        CREATE temp TABLE temp_bikelanes_quantized AS
+        CREATE temp TABLE temp_bikelanes_segmentized AS
           SELECT
             id,
             tags->>'category' AS category,
-            (atlas_QuantizeLineString(geom, 100)).*
+            (atlas_segmentize_linestring(geom, (tags->>'length')::FLOAT, 100)).*
           FROM
             bikelanes;
-        CREATE INDEX "temp_bikelanes_quantized_geom_idx" ON temp_bikelanes_quantized USING gist(geom);
-        CREATE INDEX "temp_bikelanes_quantized_category_idx" ON temp_bikelanes_quantized (category);
+        CREATE INDEX "temp_bikelanes_segmentized_geom_idx" ON temp_bikelanes_segmentized USING gist(geom);
+        CREATE INDEX "temp_bikelanes_segmentized_category_idx" ON temp_bikelanes_segmentized (category);
       EXCEPTION
         WHEN duplicate_table THEN
           -- If the table already exists, do nothing
-          RAISE NOTICE 'Using cached "temp_bikelanes_quantized" table';
+          RAISE NOTICE 'Using cached "temp_bikelanes_segmentized" table';
       END;
 
       SELECT
-        jsonb_object_agg(category, ROUND(total_length_km))
+        jsonb_object_agg(category, ROUND(total_length_km / 1000.0))
       INTO category_length
       FROM (
         SELECT
           category,
-          SUM(bikelanes.len) / 1000.0 AS total_length_km
+          SUM(bikelanes.length) AS total_length_km
         FROM
-          temp_bikelanes_quantized as bikelanes
+          temp_bikelanes_segmentized as bikelanes
         WHERE
-          ST_Intersects(bikelanes.geom, polygon_input)  -- Spatial intersection with input polygon
+          ST_Intersects(bikelanes.geom, input_polygon)  -- Spatial intersection with input polygon
         GROUP BY
           category
       ) AS aggregated_data;
