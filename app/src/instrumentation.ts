6be8465d614c16051cf8811538618a299af3bc11
Laurenz Rasche <laurenz@fixmycity.de>
2024-10-23 13:49:23 +0200

    instrumentation: import modules after runtime check

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -1,7 +1,7 @@
-import { registerSQLFunctions } from './registerSQLFunctions/registerSQLFunctions'
-// This function gets called on every server startup. For details see /src/registerSQLFunctions/README.md
+// This function gets called on every server startup. For details see /src/registerSQLFunctions/README.md or https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { registerSQLFunctions } = await import('./registerSQLFunctions/registerSQLFunctions')
     return registerSQLFunctions()
   }
 }
