81bbf3c994c1f6d3a3ebc553c43b29962a1cd9eb
Tobias <tobias@fixmycity.de>
2024-11-05 16:59:35 +0100

    StaticData: Reduce precision for WFS files

--- app/scripts/StaticDatasets/updaetWfsSources/transformGeopackageToGeojson.ts
+++ app/scripts/StaticDatasets/updaetWfsSources/transformGeopackageToGeojson.ts
@@ -1,5 +1,6 @@
 export const transformGeopackageToGeojson = async (input: string, output: string) => {
-  Bun.spawnSync(['ogr2ogr', '-f', 'GeoJSON', output, input], {
+  console.log('  Run ogr2ogr')
+  Bun.spawnSync(['ogr2ogr', '-f', 'GeoJSON', output, input, '-lco', 'COORDINATE_PRECISION=8'], {
     onExit(_proc, exitCode, _signalCode, error) {
       exitCode && console.log('exitCode:', exitCode)
       error && console.log('error:', error)
