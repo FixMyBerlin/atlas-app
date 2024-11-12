cd204018da1f7067560ab61ff5e08ed8d88beafc
Laurenz Rasche <laurenz@fixmycity.de>
2024-11-12 01:59:29 +0100

    fix osm timestamp parsing

--- processing/steps/metadata.ts
+++ processing/steps/metadata.ts
@@ -4,17 +4,19 @@ import { filteredFile } from './filter'
 
 const prisma = new PrismaClient()
 export async function getFileTimestamp(fileName: string) {
-  return $`osmium fileinfo ${filteredFile(fileName)} -g header.option.timestamp`.text()
+  return $`osmium fileinfo ${filteredFile(fileName)} -g header.option.timestamp`
+    .text()
+    .then((timestamp) => timestamp.trim())
 }
 export async function createMetadataTable() {
   return prisma.$executeRaw`
     CREATE TABLE IF NOT EXISTS public.meta
       (id SERIAL PRIMARY KEY, processed_at TIMESTAMP, processing_duration TIME, osm_data_from TIMESTAMP);`
 }
-export async function writeMetadata(fileName: string, processingDurationSeconds: number) {
-  const filesTimestamp = await getFileTimestamp(fileName)
+export async function writeMetadata(fileName: string, processingDurationMS: number) {
+  const filesTimestamp = new Date(await getFileTimestamp(fileName))
   const processingDate = new Date()
-  const processingDuration = new Date(processingDurationSeconds * 1000)
+  const processingDuration = new Date(processingDurationMS)
   return prisma.$executeRaw`
     INSERT INTO public.meta
       (processed_at, processing_duration, osm_data_from)
