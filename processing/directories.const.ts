ddb8d9c7c32e2ebbc4763d391ca44cd9281a9755
Laurenz Rasche <laurenz@fixmycity.de>
2024-11-12 12:32:15 +0100

    fix filter expression dir

--- processing/directories.const.ts
+++ processing/directories.const.ts
@@ -1,7 +1,7 @@
 export const OSM_DOWNLOAD_DIR = '/data/downloads'
 export const OSM_FILTERED_DIR = '/data/filtered'
 export const HASHES_DIR = '/data/hashes'
-export const FILTER_DIR = '/filter'
+export const FILTER_DIR = '/processing/filter'
 export const FILTER_EXPRESSIONS = `${FILTER_DIR}/filter-expressions.txt`
 export const TOPIC_DIR = '/processing/topics'
 export const ID_FILTERED_FILE = `id_filtered.osm.pbf`
