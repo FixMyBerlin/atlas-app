5ff204a5be824e0acaa6e5fe33859ee0ee757a81
Laurenz Rasche <laurenz@fixmycity.de>
2024-11-12 12:47:03 +0100

    more doc strings

--- processing/directories.const.ts
+++ processing/directories.const.ts
@@ -1,7 +1,34 @@
+/**
+ * The path to the directory containing downloaded OSM files.
+ */
 export const OSM_DOWNLOAD_DIR = '/data/downloads'
+
+/**
+ * The path to the directory containing filtered OSM files.
+ */
 export const OSM_FILTERED_DIR = '/data/filtered'
-export const HASHES_DIR = '/data/hashes'
+
+/**
+ * The path to the directory containing persistent data. E.g. directory hashes.
+ */
+export const PERSISTENT_DIR = '/data/hashes'
+
+/**
+ * The path to the directory containing the OSMIUM filter expressions.
+ */
 export const FILTER_DIR = '/processing/filter'
+
+/**
+ * The path to the file containing the OSMIUM filter expressions.
+ */
 export const FILTER_EXPRESSIONS = `${FILTER_DIR}/filter-expressions.txt`
+
+/**
+ * The path to the directory containing the topics.
+ */
 export const TOPIC_DIR = '/processing/topics'
+
+/**
+ * The file name for the id filtered OSM file.
+ */
 export const ID_FILTERED_FILE = `id_filtered.osm.pbf`
--- processing/utils/persistentData.ts
+++ processing/utils/persistentData.ts
@@ -1,7 +1,7 @@
-import { HASHES_DIR } from '../directories.const'
 import { join } from 'path'
+import { PERSISTENT_DIR } from '../directories.const'
 
-const filePath = (id: string) => join(HASHES_DIR, id)
+const filePath = (id: string) => join(PERSISTENT_DIR, id)
 
 export async function readPersistent(id: string) {
   const file = await Bun.file(filePath(id))
