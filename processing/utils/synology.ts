a107e65516ef82e466d98a395176de91d86161ae
Laurenz Rasche <laurenz@fixmycity.de>
2024-11-18 12:34:33 +0100

    take synology url from .env

--- processing/utils/synology.ts
+++ processing/utils/synology.ts
@@ -1,11 +1,10 @@
-import { join } from 'path'
 import { params } from './parameters'
 
 async function logToSynology(message: string, token: string) {
   if (!params.synologyURL) {
     return
   }
-  const url = join(params.synologyURL, token)
+  const url = params.synologyURL + token
   const payload = { text: `#${params.environment}: ${message}` }
   await fetch(url, {
     method: 'POST',
