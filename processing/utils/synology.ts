d411772d473e46546254506118104ce19e9984ca
Laurenz Rasche <laurenz@fixmycity.de>
2024-11-18 17:09:48 +0100

    manually build synology chat url

--- processing/utils/synology.ts
+++ processing/utils/synology.ts
@@ -5,11 +5,22 @@ async function logToSynology(message: string, token: string) {
   if (!params.synologyURL) {
     return
   }
-  // prepare the URL and payload
-  const url = params.synologyURL + token
+
+  // prepare the URL
+  const synologyParams = {
+    token: decodeURIComponent(token), // the token is already encoded in the env
+    api: 'SYNO.Chat.External',
+    method: 'incoming',
+    version: '2',
+  }
+  const url = new URL(params.synologyURL)
+  Object.entries(synologyParams).forEach(([key, value]) => {
+    url.searchParams.append(key, value)
+  })
+
+  //prepare the payload
   const payload = { text: `#${params.environment}: ${message}` }
   const body = new URLSearchParams({ payload: JSON.stringify(payload) })
-
   try {
     const response = await fetch(url, {
       method: 'POST',
