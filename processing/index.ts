baca6bef25c2a2770a5a607bee41d5a235216e77
Tobias <tobias@fixmycity.de>
2025-01-08 13:24:48 +0100

    Processing: Fix logging
    It looks like the try-catch only works inside a function, not top level in a file.

--- processing/index.ts
+++ processing/index.ts
@@ -15,54 +15,58 @@ import { logTileInfo } from './utils/logging'
 import { params } from './utils/parameters'
 import { synologyLogError } from './utils/synology'
 
-try {
-  // Setup directories and backup schema
-  await setup()
+async function main() {
+  try {
+    // Setup directories and backup schema
+    await setup()
 
-  // Wait for fresh data
-  if (params.waitForFreshData) {
-    await waitForFreshData(params.fileURL, 24, 10)
+    // Wait for fresh data
+    if (params.waitForFreshData) {
+      await waitForFreshData(params.fileURL, 24, 10)
+    }
+
+    // Download osm file
+    let { fileName, fileChanged } = await downloadFile(params.fileURL, params.skipDownload)
+
+    // Filter osm file with /filter/filter-expressions.txt
+    await tagFilter(fileName, fileChanged)
+
+    // Filter osm file by ids if given
+    if (params.idFilter !== '') {
+      fileName = await idFilter(fileName, params.idFilter)
+      fileChanged = true
+    }
+
+    // Process topics
+    const { timeElapsed: processingTime, processedTables } = await processTopics(
+      topicList,
+      fileName,
+      fileChanged,
+      params.skipUnchanged,
+      params.computeDiffs,
+      params.freezeData,
+    )
+
+    await generateTypes(params.environment, processedTables)
+    await writeMetadata(fileName, processingTime)
+
+    // Call the frontend update hook which registers sql functions and starts the analysis run
+    await triggerPostProcessing()
+
+    // Restart `tiles` container to refresh `/catalog`
+    await restartTileServer()
+
+    // Handle cache warming hook
+    await clearCache()
+    if (!params.skipWarmCache) {
+      await triggerCacheWarming()
+    }
+
+    await logTileInfo(params.environment)
+  } catch (error) {
+    // This `catch` will only trigger if child functions are `await`ed AND file calls a `main()` function. Top level code does not work.
+    synologyLogError(`Processing failed: ${error}`)
   }
-
-  // Download osm file
-  let { fileName, fileChanged } = await downloadFile(params.fileURL, params.skipDownload)
-
-  // Filter osm file with /filter/filter-expressions.txt
-  await tagFilter(fileName, fileChanged)
-
-  // Filter osm file by ids if given
-  if (params.idFilter !== '') {
-    fileName = await idFilter(fileName, params.idFilter)
-    fileChanged = true
-  }
-
-  // Process topics
-  const { timeElapsed: processingTime, processedTables } = await processTopics(
-    topicList,
-    fileName,
-    fileChanged,
-    params.skipUnchanged,
-    params.computeDiffs,
-    params.freezeData,
-  )
-
-  await generateTypes(params.environment, processedTables)
-  await writeMetadata(fileName, processingTime)
-
-  // Call the frontend update hook which registers sql functions and starts the analysis run
-  await triggerPostProcessing()
-
-  // Restart `tiles` container to refresh `/catalog`
-  await restartTileServer()
-
-  // Handle cache warming hook
-  await clearCache()
-  if (!params.skipWarmCache) {
-    await triggerCacheWarming()
-  }
-
-  await logTileInfo(params.environment)
-} catch (error) {
-  // This `catch` will only trigger if child functions are `await`ed AND file calls a `main()` function. Top level code does not work.
-  synologyLogError(`Processing failed: ${error}`)
 }
+
+main()
