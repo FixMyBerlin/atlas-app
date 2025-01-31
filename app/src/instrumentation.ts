63d475cf40ae32f66a0aecce44624c5144190c31
Tobias <tobias@fixmycity.de>
2025-01-31 13:39:37 +0100

    Campaigns: Introduce `cacheRadinfraDeCampaigns` during instrumentation

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -4,5 +4,8 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { registerSQLFunctions } = await import('./instrumentation/registerSQLFunctions')
     registerSQLFunctions()
+
+    const { cacheRadinfraDeCampaigns } = await import('./instrumentation/cacheRadinfraDeCampaigns')
+    cacheRadinfraDeCampaigns()
   }
 }
