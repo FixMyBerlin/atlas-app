ad44f239dc668a491ee32453094f65d7aed4d0a3
Tobias <tobias@fixmycity.de>
2025-02-17 16:12:42 +0100

    Map: Add quick links to DEV header

--- app/src/app/_components/layouts/RenderIfNotDoNotNavigate.tsx
+++ app/src/app/_components/layouts/RenderIfNotDoNotNavigate.tsx
@@ -1,9 +1,28 @@
+import { LinkExternal } from '../links/LinkExternal'
 import { isDev } from '../utils/isEnv'
+import { getAdminInfoEnvUrl } from './Header/User/utils/getAdminInfoEnvUrl'
 
 export function RenderIfNotDoNotNavigate({ children }: { children: React.ReactNode }) {
   const doNotNavigate = !!process.env.NEXT_PUBLIC_DO_NOT_NAVIGATE
+  const stagingUrl = getAdminInfoEnvUrl('staging')
+  const prodUrl = getAdminInfoEnvUrl('production')
+
   return isDev && doNotNavigate ? (
-    <pre className="text-red-400">NEXT_PUBLIC_DO_NOT_NAVIGATE</pre>
+    <div className="flex items-center justify-between">
+      <pre className="text-red-400">NEXT_PUBLIC_DO_NOT_NAVIGATE</pre>
+      <div className="space-x-4 text-white">
+        {stagingUrl && (
+          <LinkExternal blank href={stagingUrl}>
+            Open Staging
+          </LinkExternal>
+        )}
+        {prodUrl && (
+          <LinkExternal blank href={prodUrl}>
+            Open Production
+          </LinkExternal>
+        )}
+      </div>
+    </div>
   ) : (
     children
   )
