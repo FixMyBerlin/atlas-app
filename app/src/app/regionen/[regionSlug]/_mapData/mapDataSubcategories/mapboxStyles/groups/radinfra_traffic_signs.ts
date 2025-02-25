2f34bbd1b3acd523d90b829358045091c46deed8
Tobias <tobias@fixmycity.de>
2025-02-25 17:10:21 +0100

    Map: Update styles

--- app/src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/mapboxStyles/groups/radinfra_traffic_signs.ts
+++ app/src/app/regionen/[regionSlug]/_mapData/mapDataSubcategories/mapboxStyles/groups/radinfra_traffic_signs.ts
@@ -5,24 +5,6 @@ import { MapboxStyleLayer } from '../types'
 
 export const mapboxStyleGroupLayers_radinfra_traffic_signs: MapboxStyleLayer[] = [
   {
-    maxzoom: 13,
-    filter: ['!', ['has', 'traffic_sign']],
-    type: 'heatmap',
-    id: 'sign-heatmap',
-    paint: {
-      'heatmap-color': [
-        'interpolate',
-        ['linear'],
-        ['heatmap-density'],
-        0,
-        'rgba(0, 0, 255, 0)',
-        1,
-        '#fda5e4',
-      ],
-      'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 6, 2, 8, 4, 11, 16, 13, 20],
-    },
-  },
-  {
     minzoom: 11,
     layout: {
       'line-cap': 'round',
