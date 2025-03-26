a7c3aa400690f109e3b53d8bc0634b5d6f221509
Tobias <tobias@fixmycity.de>
2025-03-26 13:39:58 +0100

    Radinfra: Update config for oneway map and campaign
    Reverting fa0797a559376088fe1a4e263e00128c9ba91acc because this change has both.

--- app/src/app/regionen/[regionSlug]/_hooks/useQueryState/useCategoriesConfig/v2/configs/index.ts
+++ app/src/app/regionen/[regionSlug]/_hooks/useQueryState/useCategoriesConfig/v2/configs/index.ts
@@ -12,10 +12,10 @@ import { _1dhu3si } from './_1dhu3si'
 import { _1e6163i } from './_1e6163i'
 import { _1gk36d0 } from './_1gk36d0'
 import { _1j7ax4m } from './_1j7ax4m'
-import { _1jfclvk } from './_1jfclvk'
 import { _1k7izoy } from './_1k7izoy'
 import { _1lhd7a } from './_1lhd7a'
 import { _1ljsnj0 } from './_1ljsnj0'
+import { _1ops5da } from './_1ops5da'
 import { _1oy2q4i } from './_1oy2q4i'
 import { _1p0kkkw } from './_1p0kkkw'
 import { _1qeycq8 } from './_1qeycq8'
@@ -24,13 +24,13 @@ import { _1r88sri } from './_1r88sri'
 import { _1swjsz2 } from './_1swjsz2'
 import { _1tdocms } from './_1tdocms'
 import { _1x5utcr } from './_1x5utcr'
+import { _1xgd8y8 } from './_1xgd8y8'
 import { _bv3m8i } from './_bv3m8i'
 import { _diykzs } from './_diykzs'
 import { _e6ilf4 } from './_e6ilf4'
 import { _eu560i } from './_eu560i'
 import { _gdl8v6 } from './_gdl8v6'
 import { _ife2uk } from './_ife2uk'
-import { _ipmbmm } from './_ipmbmm'
 import { _l464fe } from './_l464fe'
 import { _nq3m12 } from './_nq3m12'
 import { _qver32 } from './_qver32'
@@ -57,10 +57,10 @@ export const configs = {
   '1e6163i': _1e6163i,
   '1gk36d0': _1gk36d0,
   '1j7ax4m': _1j7ax4m,
-  '1jfclvk': _1jfclvk,
   '1k7izoy': _1k7izoy,
   '1lhd7a': _1lhd7a,
   '1ljsnj0': _1ljsnj0,
+  '1ops5da': _1ops5da,
   '1oy2q4i': _1oy2q4i,
   '1p0kkkw': _1p0kkkw,
   '1qeycq8': _1qeycq8,
@@ -69,13 +69,13 @@ export const configs = {
   '1swjsz2': _1swjsz2,
   '1tdocms': _1tdocms,
   '1x5utcr': _1x5utcr,
+  '1xgd8y8': _1xgd8y8,
   bv3m8i: _bv3m8i,
   diykzs: _diykzs,
   e6ilf4: _e6ilf4,
   eu560i: _eu560i,
   gdl8v6: _gdl8v6,
   ife2uk: _ife2uk,
-  ipmbmm: _ipmbmm,
   l464fe: _l464fe,
   nq3m12: _nq3m12,
   qver32: _qver32,
