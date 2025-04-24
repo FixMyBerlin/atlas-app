0365064075f459360ded893b6f38695fad0905f7
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-24 12:25:21 +0200

    move tables to files were the logic is

--- processing/topics/parking/obstacles/parking_source_obstacle_points.lua
+++ processing/topics/parking/obstacles/parking_source_obstacle_points.lua
@@ -6,6 +6,22 @@ require("MergeTable")
 require("categorize_and_transform_points")
 require("result_tags_obstacles")
 
+local obstacle_points_table = osm2pgsql.define_table({
+  name = 'parking_obstacle_points',
+  ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+  columns = {
+    { column = 'id',      type = 'text',      not_null = true },
+    { column = 'tags',    type = 'jsonb' },
+    { column = 'meta',    type = 'jsonb' },
+    { column = 'geom',    type = 'point', projection = 5243 },
+    { column = 'minzoom', type = 'integer' },
+  },
+  indexes = {
+    { column = { 'minzoom', 'geom' }, method = 'gist' },
+    { column = 'id',                  method = 'btree', unique = true }
+  }
+})
+
 function parking_source_obstacle_points(object)
   local results = {}
   if next(object.tags) == nil then return results end
@@ -13,7 +29,7 @@ function parking_source_obstacle_points(object)
   local self_left_right = categorize_and_transform_points(object)
   for _, result in pairs(self_left_right) do
     if result.object then
-      table.insert(results, MergeTable({ geom = result.object:as_point() }, result_tags_obstacles(result)))
+      obstacle_points_table:insert(results, MergeTable({ geom = result.object:as_point() }, result_tags_obstacles(result)))
     end
   end
 
