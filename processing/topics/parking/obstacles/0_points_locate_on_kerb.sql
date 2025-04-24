8c20646e3c975eae076836342a304113289401d7
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-24 18:51:34 +0200

    only locate points where `perform_snap=side`

--- processing/topics/parking/obstacles/0_points_locate_on_kerb.sql
+++ processing/topics/parking/obstacles/0_points_locate_on_kerb.sql
@@ -10,7 +10,18 @@
 -- And the "distance to move" is hardcoded, of course, it should be derived from the perform_move value of the right highway on the kerb table.
 -- Actually, it might be better to roll back to "punching" (stanzen) those obstacles in a separate punch step before the parking lines get moved to the left/right so we don't need to bother with moving the points correctly. For the debug view, having the points moved would be better, though…
 DROP TABLE IF EXISTS parking_obstacle_points_located;
-SELECT osm_type, osm_id, id, tags, meta, geom, minzoom, (locate_on_kerb(osm_id, geom)).* INTO parking_obstacle_points_located FROM parking_obstacle_points;
+SELECT
+  osm_type,
+  osm_id,
+  id,
+  tags,
+  meta,
+  geom,
+  minzoom,
+  (locate_on_kerb(osm_id, geom)).*
+INTO parking_obstacle_points_located
+FROM parking_obstacle_points
+WHERE tags->>'perform_snap' = 'side';
 
 ALTER TABLE parking_obstacle_points_located
 ALTER COLUMN geom TYPE geometry(Geometry, 5243)
