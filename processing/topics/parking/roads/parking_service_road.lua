9719b99914af0a0ecd213fee11b27db9dcc517d6
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-24 12:49:57 +0200

    fix insert logic

--- processing/topics/parking/roads/parking_service_road.lua
+++ processing/topics/parking/roads/parking_service_road.lua
@@ -23,6 +23,6 @@ local service_roads_table = osm2pgsql.define_table({
 
 function parking_service_road(object)
   if exit_processing_service_roads(object.tags) then return nil end
-
-  service_roads_table:insert(MergeTable({ geom = object:as_linestring() }, result_tags_roads(object)))
+  local row = MergeTable({ geom = object:as_linestring() }, result_tags_roads(object))
+  service_roads_table:insert(row)
 end
