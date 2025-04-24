9c73bdacc7bc10d4c41d1748d52d5ea6b38de845
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-24 16:33:39 +0200

    create a node -> kerb mapping instead of node -> street

--- processing/topics/parking/parking.sql
+++ processing/topics/parking/parking.sql
@@ -15,6 +15,7 @@
 \i '/processing/topics/parking/kerbs/0_perform_move.sql'
 \i '/processing/topics/parking/kerbs/1_merge_kerbs.sql'
 \i '/processing/topics/parking/kerbs/2_define_kerb_projection.sql'
+\i '/processing/topics/parking/kerbs/3_define_locate_on_kerb.sql'
 
 -- HANDLE OBSTACLES
 --
