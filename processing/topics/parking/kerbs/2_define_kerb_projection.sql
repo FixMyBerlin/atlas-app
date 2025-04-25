b08a243ef752b73ca8eb3053b745d551cefdffd4
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-25 14:51:15 +0200

    `project_to_kern()` extract projection to line into separate function

--- processing/topics/parking/kerbs/2_define_kerb_projection.sql
+++ processing/topics/parking/kerbs/2_define_kerb_projection.sql
@@ -4,6 +4,34 @@
 -- a small quantization results in a better approximation of the projection while increasing computational cost
 
 -- TODO: we probably need to project to the k nearest kerbs see: https://tiles-inspector.netlify.app/#20.03/52.50324/13.3912685
+
+CREATE OR REPLACE FUNCTION project_to_line(
+  input_geom geometry,
+  line geometry,
+  quantization double precision
+)
+RETURNS geometry AS $$
+DECLARE
+  quantized_line geometry := ST_Segmentize(line, quantization);
+  rec RECORD;
+  point_on_line geometry;
+  rel_position double precision;
+  substring_start double precision := 1.0;
+  substring_end double precision := 0.0;
+BEGIN
+  FOR rec IN SELECT * FROM ST_DumpPoints(input_geom)
+  LOOP
+    point_on_line := ST_ClosestPoint(quantized_line, rec.geom);
+    rel_position := ST_LineLocatePoint(line, point_on_line);
+    substring_start := LEAST(substring_start, rel_position);
+    substring_end := GREATEST(substring_end, rel_position);
+  END LOOP;
+
+  RETURN ST_LineSubstring(line, substring_start, substring_end);
+END;
+$$ LANGUAGE plpgsql STABLE;
+
+
 CREATE OR REPLACE FUNCTION project_to_closest_kerb(
   input_geom geometry,
   tolerance double precision,
@@ -12,15 +40,7 @@ CREATE OR REPLACE FUNCTION project_to_closest_kerb(
 RETURNS geometry AS $$
 DECLARE
   closest_kerb geometry;
-  closest_kerb_quantized geometry;
-  point_on_kerb geometry;
-  rec RECORD;
-  rel_position_on_kerb double precision;
-  substring_start double precision := 1.0;
-  substring_end double precision := 0.0;
 BEGIN
-
-  -- Find the closest kerb from parking_kerbs_merged
   SELECT geom
   INTO closest_kerb
   FROM parking_kerbs_merged
@@ -28,30 +48,10 @@ BEGIN
   ORDER BY ST_Distance(input_geom, geom)
   LIMIT 1;
 
-  -- If no closest geometry (kerb) is found, return the input geometry
   IF closest_kerb IS NULL THEN
-      RETURN input_geom;
+    RETURN input_geom;
   END IF;
 
-  -- Apply ST_Segmentize to discretize the closest kerb geometry
-  closest_kerb_quantized := ST_Segmentize(closest_kerb, quantization);
-
-
-  -- Loop through each point in the input geometry using ST_DumpPoints
-  FOR rec IN
-    SELECT * FROM ST_DumpPoints(input_geom)
-  LOOP
-    -- For each point, get the relative position on the closest kerb
-    point_on_kerb := ST_ClosestPoint(closest_kerb_quantized, rec.geom);
-    rel_position_on_kerb :=  ST_LineLocatePoint(closest_kerb, point_on_kerb);
-
-    -- Check if the point extends the substring
-    substring_start := LEAST(substring_start, rel_position_on_kerb);
-    substring_end := GREATEST(substring_end, rel_position_on_kerb);
-  END LOOP;
-
-  -- Use the relative start and end position to extract the projected substring
-  RETURN ST_LineSubstring(closest_kerb, substring_start, substring_end);
-
+  RETURN project_to_line(input_geom, closest_kerb, quantization);
 END;
 $$ LANGUAGE plpgsql STABLE;
