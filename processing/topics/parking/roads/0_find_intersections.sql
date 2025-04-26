4de30d22e90145ddbc9af86fbbb47f8e7b2a5826
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-26 17:19:11 +0200

    define intersection on `parking_roads` instead of `parking_kerbs`

--- processing/topics/parking/roads/0_find_intersections.sql
+++ processing/topics/parking/roads/0_find_intersections.sql
@@ -6,22 +6,25 @@ WITH intersections AS (
     COUNT(way_id) AS way_count,
     MIN(way_id) AS way_id
   FROM "_node_kerb_mapping"
+  WHERE
+    way_id IN (SELECT osm_id FROM parking_roads)
   GROUP BY node_id
-  HAVING COUNT(way_id) > 2 OR (COUNT(way_id) = 2 AND NOT bool_and(is_terminal_node))
+  HAVING
+    COUNT(way_id) > 2
+    OR (COUNT(way_id) = 2 AND NOT bool_and(is_terminal_node))
 )
 SELECT
   i.node_id,
   i.way_id,
   i.way_count,
-  ST_PointN(kerb.geom, nkm.idx) AS geom
+  ST_PointN(road.geom, nkm.idx) AS geom
 INTO parking_intersections
 FROM intersections i
 JOIN "_node_kerb_mapping" nkm
   ON i.way_id = nkm.way_id
   AND i.node_id = nkm.node_id
-JOIN parking_kerbs kerb
-  ON kerb.osm_id = nkm.way_id
-WHERE kerb.tags->>'side' = 'right';
+JOIN parking_roads road
+  ON road.osm_id = nkm.way_id;
 
 
 ALTER TABLE parking_intersections
