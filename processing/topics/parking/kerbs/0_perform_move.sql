a3090eec87726ca489f7701c3bc69e2b85a18f8b
Laurenz Rasche <laurenz@fixmycity.de>
2025-04-28 22:04:23 +0200

    don't simplify before offsetting kerbs and also translate invalid, closed, and complex geometries

--- processing/topics/parking/kerbs/0_perform_move.sql
+++ processing/topics/parking/kerbs/0_perform_move.sql
@@ -6,17 +6,10 @@ SELECT
   id,
   tags,
   meta,
-  ST_OffsetCurve (
-    ST_Simplify (geom, 0.5),
-    (tags ->> 'perform_move')::numeric
-  ) as geom,
+  ST_OffsetCurve (geom, (tags ->> 'perform_move')::numeric) as geom,
   minzoom INTO parking_kerbs_moved
 FROM
-  parking_kerbs
-WHERE
-  st_issimple (geom)
-  and st_isvalid (geom)
-  and not st_isclosed (geom);
+  parking_kerbs;
 
 ALTER TABLE parking_kerbs_moved
 ALTER COLUMN geom TYPE geometry (Geometry, 5243) USING ST_SetSRID (geom, 5243);
