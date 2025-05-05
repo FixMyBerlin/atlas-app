662188183841a6c46aef67ec29fa20405bb862b2
Laurenz Rasche <laurenz@fixmycity.de>
2025-05-05 11:30:22 +0200

    parkings: allow service roads

--- processing/topics/parking/parkings/helper/exit_processing_parkings.lua
+++ processing/topics/parking/parkings/helper/exit_processing_parkings.lua
@@ -5,11 +5,12 @@ require("JoinSets")
 require("HighwayClasses")
 require("Log")
 require("exit_processing_roads")
+require("exit_processing_service_roads")
 
 function exit_processing_parkings(tags)
   if not tags.highway then return true end
 
-  if exit_processing_roads(tags) then return true end
+  if exit_processing_roads(tags) and exit_processing_service_roads(tags) then return true end
 
   for key, _ in pairs(tags) do
     if key:match("^parking:") then
