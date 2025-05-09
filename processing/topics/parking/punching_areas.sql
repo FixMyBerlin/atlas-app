8bd495b14248950d16c082bf98ec74323ad13e02
Tobias <tobias@fixmycity.de>
2025-05-09 11:36:41 +0200

    Parking: Copy explicit tags for punching_areas

--- processing/topics/parking/punching_areas.sql
+++ processing/topics/parking/punching_areas.sql
@@ -1,17 +1,46 @@
 DROP TABLE IF EXISTS parking_punching_areas;
 
+-- INSERT driveway buffers (rectangles)
+-- "5" is the buffer in meter where no parking is allowed legally
 SELECT
-  ST_Buffer (geom, 5) as geom INTO parking_punching_areas
+  'node/' || intersection_id AS id,
+  ST_Buffer (geom, 5) as geom,
+  jsonb_build_object(
+    /* sql-formatter-disable */
+    'category', 'intersection_corner',
+    'size', 5
+    /* sql-formatter-enable */
+  ) AS tags,
+  '{}'::jsonb AS meta,
+  0 AS minzoom INTO parking_punching_areas
 FROM
   parking_intersection_corners;
 
+-- INSERT driveway buffers (rectangles)
 INSERT INTO
-  parking_punching_areas (geom)
+  parking_punching_areas (id, geom, tags, meta, minzoom)
 SELECT
-  ST_Buffer (geom, 5, 'endcap=flat') as geom
+  id,
+  ST_Buffer (
+    geom,
+    ((tags ->> 'width')::float / 2)::float,
+    'endcap=flat'
+  ) as geom,
+  jsonb_build_object(
+    /* sql-formatter-disable */
+    'size', ((tags ->> 'width')::float / 2)::float,
+    'category', 'driveway',
+    'highway', tags ->> 'highway',
+    'road', tags ->> 'road',
+    'width', tags ->> 'width'
+    /* sql-formatter-enable */
+  ) AS tags,
+  jsonb_build_object('updated_at', meta ->> 'updated_at') AS meta,
+  0 AS minzoom
 FROM
   parking_driveways;
 
+-- MISC
 ALTER TABLE parking_punching_areas
 ALTER COLUMN geom TYPE geometry (Geometry, 5243) USING ST_SetSRID (geom, 5243);
 
