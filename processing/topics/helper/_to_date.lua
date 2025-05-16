da5bc190146cf2c84f73d0f813ccbad9697403c0
Tobias <tobias@fixmycity.de>
2025-05-16 06:42:08 +0200

    Processing: Rename to_date to follow snake case naming

--- processing/topics/helper/_to_date.lua
+++ processing/topics/helper/_to_date.lua
@@ -1,4 +1,4 @@
-function ToDate(tagsObject, toNumberTags)
+function to_date(tagsObject, toNumberTags)
   for key, value in pairs(tagsObject) do
     if toNumberTags[key] then
       tagsObject[key] = os.date('!%Y-%m-%dT%H:%M:%SZ', value)
