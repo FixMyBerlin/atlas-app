fcd9d7dfed8f27245d6d17860b68fc68fa5755d9
Tobias <tobias@fixmycity.de>
2025-05-20 21:41:20 +0200

    Parking: Fix barrier line category

--- processing/topics/parking/obstacles/line/obstacle_line_categories.lua
+++ processing/topics/parking/obstacles/line/obstacle_line_categories.lua
@@ -3,6 +3,7 @@ require('class_obstacle_category')
 require('two_wheel_parking_helper')
 require('amenity_parking_helper')
 require('Log')
+local TAG_HELPER = require('tag_helper')
 
 obstacle_line_categories = {
   class_obstacle_category.new({
@@ -27,23 +28,26 @@ obstacle_line_categories = {
     perform_snap = 'self',
     perform_buffer = function(tags) return nil end,
     conditions = function(tags)
-      return tags.barrier == 'bollard' or -- https://www.openstreetmap.org/way/889059815
-        tags.barrier == "fence" -- https://www.openstreetmap.org/way/777325759
+      return TAG_HELPER.is_obstacle_parking(tags) and (
+        tags.barrier == 'bollard' or -- https://www.openstreetmap.org/way/889059815
+        tags.barrier == 'fence' -- https://www.openstreetmap.org/way/777325759
+      )
     end,
     tags = function(tags) return { barrier = tags.barrier } end,
     tags_cc = { },
   }),
-  class_obstacle_category.new({
-    id = 'path',
-    side_schema = nil,
-    side_key = nil,
-    perform_snap = 'self',
-    perform_buffer = function(tags) return nil end,
-    conditions = function(tags)
-      return tags.barrier == 'bollard' or -- https://www.openstreetmap.org/way/889059815
-        tags.barrier == "fence" -- https://www.openstreetmap.org/way/777325759
-    end,
-    tags = function(tags) return { barrier = tags.barrier } end,
-    tags_cc = { },
-  }),
+  -- class_obstacle_category.new({
+  --   id = 'path',
+  --   side_schema = nil,
+  --   side_key = nil,
+  --   perform_snap = 'self',
+  --   perform_buffer = function(tags) return nil end,
+  --   conditions = function(tags)
+  --     return TAG_HELPER.is_obstacle_parking(tags) and (
+  --       tags.highway
+  --     )
+  --   end,
+  --   tags = function(tags) return { barrier = tags.barrier } end,
+  --   tags_cc = { },
+  -- }),
 }
