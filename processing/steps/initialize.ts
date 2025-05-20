4d2d7ecca99c20d48ebbcf0d200032bda62093bc
Tobias <tobias@fixmycity.de>
2025-05-20 14:46:31 +0200

    Processing: Ensure postgis is initialized
    This needs to be done only once but when changing the docker image it is needed (sometimes). It does not hurt to check it every time, so lets just add it.

--- processing/steps/initialize.ts
+++ processing/steps/initialize.ts
@@ -1,4 +1,4 @@
-import { $ } from 'bun'
+import { $, sql } from 'bun'
 import { OSM_DOWNLOAD_DIR, OSM_FILTERED_DIR, PERSISTENT_DIR } from '../constants/directories.const'
 import { initializeCustomFunctionsDataTables, initializeSchemaData } from '../dataTables/dataTables'
 import { initializeCustomFunctionDiffing, initializeSchemaBackup } from '../diffing/diffing'
@@ -7,6 +7,8 @@ import { initializeMetadataTable } from './metadata'
 /** Initialize Folder, Schema, Custom SQL Functions, Tables */
 export async function initialize() {
   await $`mkdir -p ${OSM_DOWNLOAD_DIR} ${OSM_FILTERED_DIR} ${PERSISTENT_DIR}`
+
+  await sql`CREATE EXTENSION IF NOT EXISTS postgis`
 
   // See ./diffing
   await initializeSchemaBackup()
