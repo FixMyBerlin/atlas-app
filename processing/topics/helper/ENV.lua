4e438f5224c4a812235090cad5cdda6abc82f8ce
Tobias <tobias@fixmycity.de>
2025-05-20 21:40:47 +0200

    Processing: Don't log the env
    This is not a good idea to have active all the time I think

--- processing/topics/helper/ENV.lua
+++ processing/topics/helper/ENV.lua
@@ -1,12 +1,13 @@
 require('init')
 require('Log')
+local inspect = require('inspect')
 
 -- Import as "local ENV = require('ENV')" and use as ENV.is_production.
 -- We use a different pattern here to allow us to overwrite the values easily in tests.
 -- Note: This is always nil during test runs because then env is missing then.
 local function is_production()
   local env = os.getenv("ENVIRONMENT")
-  Log(env == "production", 'ENV is_production:')
+  -- print('ENV is_production: ' .. inspect(env == "production"))
   return env == "production"
 end
 
